300d4ccacfc35c5d466ca1436095d7b9a62101f5
Ari Kobren <akobren@cs.umass.edu>
2016-01-21 10:46:48 -0500

    fixed some regex in script 4

--- node-script/20160114-demo/4-create_iclr_sub_inv.js
+++ node-script/20160114-demo/4-create_iclr_sub_inv.js
@@ -92,8 +92,8 @@ var subInv = {
 		    'content': {
 //			'title': '.{0,500}',
 			'qualEval': '[\\S\\s]{1,5000}',
-			'quantEval': '(10: Top 5% of accepted papers, seminal paper)|(9: Top 15% of accepted papers, strong accept)|(8: Top 50% of accepted papers, clear accept)|(7: Good paper, accept)|(6: Marginally above acceptance threshold)|(5: Marginally below acceptance threshold)|(4: Ok but not good enough - rejection)|(3: Clear rejection)|(2: Strong rejection)|(1: Trivial or wrong)',
-			'confidence': '(5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature\.)|(4: The reviewer is confident but not absolutely certain that the evaluation is correct\.)|(3: The reviewer is fairly confident that the evaluation is correct\.)|(2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper\.)|(1: The reviewer\'s evaluation is an educated guess\.)'
+			'quantEval': '10: Top 5% of accepted papers, seminal paper|9: Top 15% of accepted papers, strong accept|8: Top 50% of accepted papers, clear accept|7: Good paper, accept|6: Marginally above acceptance threshold|5: Marginally below acceptance threshold|4: Ok but not good enough - rejection|3: Clear rejection|2: Strong rejection|1: Trivial or wrong',
+			'confidence': '5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature|4: The reviewer is confident but not absolutely certain that the evaluation is correct|3: The reviewer is fairly confident that the evaluation is correct|2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper|1: The reviewer\'s evaluation is an educated guess'
 		    },
 		    'process': (function (token, invitation, note, count, lib) {
 			var mail = {
@@ -199,7 +199,7 @@ var subInv = {
 //			'title': '.{0,500}',
 			'qualEval': '[\\S\\s]{1,5000}',
 			'quantEval': '(10: Top 5% of accepted papers, seminal paper)|(9: Top 15% of accepted papers, strong accept)|(8: Top 50% of accepted papers, clear accept)|(7: Good paper, accept)|(6: Marginally above acceptance threshold)|(5: Marginally below acceptance threshold)|(4: Ok but not good enough - rejection)|(3: Clear rejection)|(2: Strong rejection)|(1: Trivial or wrong)',
-			'confidence': '(5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature\.)|(4: The reviewer is confident but not absolutely certain that the evaluation is correct\.)|(3: The reviewer is fairly confident that the evaluation is correct\.)|(2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper\.)|(1: The reviewer\'s evaluation is an educated guess\.)'
+			'confidence': '(5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature)|(4: The reviewer is confident but not absolutely certain that the evaluation is correct)|(3: The reviewer is fairly confident that the evaluation is correct)|(2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper)|(1: The reviewer\'s evaluation is an educated guess)'
 		}
 	    }
 	};
