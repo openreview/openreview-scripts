5e0565e7212ca175f7655eb66816af80d15b84a7
Thomas Logan <thomaslilogan@gmail.com>
2016-02-26 13:43:14 -0500

    created script 12 for reviewer assignment from csv

---
+++ node-script/20160114-demo/12-assign_reviewers_from_csv.js
@@ -0,0 +1,246 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var request = require('request');
+var csvparse = require('csv-parse');
+var _ = require('lodash');
+
+var urlPrefix = 'http://localhost:8529/_db/_system/openreview/'; 
+
+var createInvitationData = function(rev_num, note, tpmsId) { return {
+  'id': 'ICLR.cc/2016/workshop/-/paper/' + tpmsId + '/review/' + rev_num,
+  'signatures': ['ICLR.cc/2016/workshop'],  // super user can sign as anyone
+  'writers': ['ICLR.cc/2016/workshop'],
+  'readers': ['ICLR.cc/2016/workshop', 'ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + rev_num],
+  'invitees': ['ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + rev_num],
+  'reply': {
+      'forum': note.forum,
+      'parent': note.id,
+      'signatures': '((~.*)|ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + rev_num + '),',  // author reveals their ~ handle or remains anonymous
+      // This reviewer has not been assigned yet
+      'writers': '((~.*)|ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + rev_num + '),',  // author reveals their ~ handle or remains anonymous
+      'readers': 'everyone,',     // review must be world readable
+      'content': {
+    'title': {
+        'order': 1,
+        'value-regex': '.{0,500}',
+        'description': 'Brief summary of your review.'
+    },
+    'review': {
+        'order': 2,
+        'value-regex': '[\\S\\s]{1,5000}',
+        'description': 'Please provide an evaluation of the quality, clarity, originality and significance of this work, including a list of its pros and cons.'
+    },
+    'rating': {
+        'order': 3,
+        'value-regex': '10: Top 5% of accepted papers, seminal paper|9: Top 15% of accepted papers, strong accept|8: Top 50% of accepted papers, clear accept|7: Good paper, accept|6: Marginally above acceptance threshold|5: Marginally below acceptance threshold|4: Ok but not good enough - rejection|3: Clear rejection|2: Strong rejection|1: Trivial or wrong'
+    },
+    'confidence': {
+        'order': 4,
+        'value-regex': '5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature|4: The reviewer is confident but not absolutely certain that the evaluation is correct|3: The reviewer is fairly confident that the evaluation is correct|2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper|1: The reviewer\'s evaluation is an educated guess'
+    }
+      }
+  },
+  'process': (function (token, invitation, note, tpmsId, lib) {
+      //figure out the signatures of the original note
+      var or3origNote = {
+    'url': 'http://localhost:8529/_db/_system/openreview/notes?id=' + note.forum,
+    'method': 'GET',
+    'json': true,
+    'headers': {
+        'Authorization': 'Bearer ' + token
+    }
+      };
+
+      var origNote = request(or3origNote);
+      console.log("ORIG NOTE SIGNATURES");
+      console.log(origNote.body.notes[0].content.author_emails.trim().split(","));
+
+      var mail = {
+    "groups": origNote.body.notes[0].content.author_emails.trim().split(","),
+    "subject": "Review of your ICLR submission \"" + note.content.title + "\".",
+    "message": "Your submission to ICLR 2016 workshops has received a new review.\n\nTo view the review, click here: http://beta.openreview.net/forum?id=" + note.forum
+      };
+
+      var or3commentMail = {
+    'url': 'http://localhost:8529/_db/_system/openreview/mail',
+    'method': 'POST',
+    'port': 8529,
+    'json': true,
+    'body': mail,
+    'headers': {
+        'Authorization': 'Bearer ' + token
+    }
+      };
+
+      var sendMail = function (o) {
+    var resp = request(o);
+    console.log("MAIL");
+    console.log(resp);
+      };
+      sendMail(or3commentMail);
+
+      // Now submit a new version of the review invitation that has no invitees
+      // This effectively makes it impossible to submit another review
+      var fulfilled_review_invite = invitation;
+      fulfilled_review_invite.invitees = [];
+      fulfilled_review_invite.process = (function (token, invitation, note, tpmsId, lib) {
+    console.log("THIS REVIEW HAS ALREADY BEEN SUBMITTED");
+    return true;
+      }) + "";
+
+      var or3fulfilled_rev = {
+    'url': 'http://localhost:8529/_db/_system/openreview/invitations',
+    'method': 'POST',
+    'port': 8529,
+    'json': true,
+    'body': fulfilled_review_invite,
+    'headers': {
+        'Authorization': 'Bearer ' + token
+    }
+      };
+      var resp = request(or3fulfilled_rev);
+      console.log("CREATING FULFILLED REVIEW");
+      console.log(fulfilled_review_invite);
+      console.log(resp);
+      return true;
+  }) + ""
+};};
+
+var assignReviewer = function(reviewerEmail, count, note, tpmsId, token) {
+  var reviewerData = {
+    'id': 'ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + count,
+    'signatures': ['ICLR.cc/2016'],
+    'writers': ['ICLR.cc/2016'],
+    'members': [reviewerEmail],
+    'readers': ['ICLR.cc/2016', 'ICLR.cc/2016/workshop/paper/' + tpmsId + '/reviewer/' + count],
+    'signatories': [reviewerEmail]
+  };
+  request(
+    {
+      'method': 'POST',
+      'url': urlPrefix + 'groups', 
+      'json': true,
+      'body': reviewerData,
+      'headers': {
+        'Authorization': 'Bearer ' + token 
+      }
+    },
+    function (error, response, body) {
+
+      console.log("creating reviewers");
+      console.log(error);
+      console.log(body);
+      if (!error && response.statusCode == 200) {
+        console.log("created reviewer: " + body.id);
+        console.log(body);
+      }
+    }
+  );
+
+  var invitationData = createInvitationData(count, note, tpmsId);
+  request(
+    {
+      'method': 'POST',
+      'url': urlPrefix + 'invitations', 
+      'json': true,
+      'body': invitationData,
+      'headers': {
+        'Authorization': 'Bearer ' + token 
+      }
+    },
+    function (error, response, body) {
+      console.log("creating invitation");
+      console.log(error);
+      console.log(body);
+      if (!error && response.statusCode == 200) {
+        console.log("created invitation: " + body.id);
+        console.log(body);
+      }
+    }
+  );
+
+
+};  
+
+
+
+console.log("about to login");
+//Login
+request(
+  {
+    method: 'POST',
+    url: urlPrefix + 'login', 
+    json: true, 
+    body: {
+      'id': 'OpenReview.net', 'password': '12345678'
+    }
+  },
+  function (error, response, body) {
+    console.log("logging in: " + response.statusCode);
+    if (!error && response.statusCode == 200) {
+      console.log("logged in");
+      console.log(body);
+
+      var token = body.token; 
+      //GET notes
+      request(
+        {
+          method: 'GET',
+          url: urlPrefix + 'notes', 
+          json: true,
+          body: {},
+          headers: {
+            'Authorization': 'Bearer ' + token 
+          }
+        },
+        function (error, response, body) {
+          console.log("getting notes");
+          if (!error && response.statusCode == 200) {
+            console.log("got notes");
+            console.log(body)
+            var notes = _.filter(body.notes, function(n) {
+              return n.id == n.forum;
+            });
+
+            var dubArr = _.map(notes, function(note) {
+              var commentInvitation = _.find(note.replyInvitations, function(invId) {
+                var regex = new RegExp("ICLR.cc/2016/workshop/-/paper/[0-9]+/comment");
+                var matches = invId.match(regex);
+                return matches && matches[0] == invId;
+              });
+              var count = commentInvitation ? commentInvitation.substring(30).slice(0, -8) : '';
+              return [count, note];
+            });
+            var tpmsPaperId2note = _.fromPairs(dubArr);
+
+            console.log("tpmsPaperId2note: " + JSON.stringify(tpmsPaperId2note));
+
+            var reviewerFile = process.argv[2];
+            fs.createReadStream(reviewerFile).pipe(csvparse({delimiter: ','}, function(err, csvDubArr) {
+
+              var notesWithReviewers = _.map(_.groupBy(csvDubArr, function(row) { 
+                return row[0];
+              }), function(rows, tpmsId) {
+                var note = tpmsPaperId2note[tpmsId + ""];
+                var reviewerEmails = _.map(rows, function(row) {
+                  return row[1].trim();
+                });
+
+                console.log("note: " + note);
+                if (note) {
+                  _.forEach(reviewerEmails, function(reviewerEmail, index) {
+                    console.log("assigning: " + index);
+                    assignReviewer(reviewerEmail, index + 1, note, tpmsId, token);
+                  });
+                }
+              });
+
+            }));
+
+          }
+        }
+      );
+    }
+});
+
