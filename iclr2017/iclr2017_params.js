becec5b90bfc952d1830decaab0022ca6df94995
mspector <michael.l.spector@gmail.com>
2016-06-29 16:30:05 -0400

    Michael ac testing (#19)
    * reorganized
    
    * modified bnmw scripts
    
    * updated iclr scripts
    
    * removed node-script folder and add DS_store to gitignore
    
    * fixed gitignore
    
    * working on iclr2017 setup
    
    * added iclr2017 scripts to test area chair functionality

--- iclr2017/iclr2017_params.js
+++ iclr2017/iclr2017_params.js
@@ -11,126 +11,153 @@ module.exports = {
   'rootUser':rootUser
 };
 
+/*
+
+SCENARIO DETAILS:
+
+*/
 
 
-var hugo = {
-  id:'hugo@openreview.net',
-  password: '12345678'
-}
-var programChair_1 = {
-  'id': 'ICLR.cc/2017/workshop/programChairs/1',
-  'members': [hugo.id],
-  'signatories':['ICLR.cc/2017/workshop/programChairs/1',hugo.id]
-}
-module.exports.hugo = hugo;
-module.exports.programChair_1 = programChair_1;
-
-
-
-var oriol = {
-  id:'oriol@openreview.net',
-  password: '12345678'
-}
-var programChair_2 = {
-  'id': 'ICLR.cc/2017/workshop/programChairs/2',
-  'members': [oriol.id],
-  'signatories':['ICLR.cc/2017/workshop/programChairs/2',oriol.id]
-}
-module.exports.oriol = oriol;
-module.exports.programChair_2 = programChair_2;
-
-
-
-var marcAurelio = {
-  id:'marcAurelio@openreview.net',
-  password: '12345678'
-}
-var programChair_3 = {
-  'id': 'ICLR.cc/2017/workshop/programChairs/3',
-  'members': [marcAurelio.id],
-  'signatories':['ICLR.cc/2017/workshop/programChairs/3',marcAurelio.id]
-}
-module.exports.marcAurelio = marcAurelio;
-module.exports.programChair_3 = programChair_3;
-
-
-
-var tara = {
-  id:'tara@openreview.net',
-  password: '12345678'
-}
-var programChair_4 = {
-  'id': 'ICLR.cc/2017/workshop/programChairs/4',
-  'members': [tara.id],
-  'signatories':['ICLR.cc/2017/workshop/programChairs/4',tara.id]
-}
-module.exports.tara = tara;
-module.exports.programChair_4 = programChair_4;
-
-
-
-var michael = {
-  id: 'spector@cs.umass.edu',
-  password: '12345678'
-};
-var areaChair_1 = {
-  'id':'ICLR.cc/2017/workshop/areaChairs/1',
-  'members':[michael.id],
-  'signatories':['Area_Chair_1',michael.id]
-};
-module.exports.michael = michael;
-module.exports.areaChair_1 = areaChair_1
-
-
-
-//ICLR.cc aready exists; may want to overwrite, though
+//ICLR.cc ALREADY EXISTS! In test, we can post this group, but make sure that you thoroughly test interactions with the old ICLR groups
 var iclr = {
   'id': 'ICLR.cc',
   'signatures': [rootUser.id],
   'writers': [rootUser.id],
-  'members': [],
+  'members': [rootUser.id],
   'readers': ['everyone'],
   'signatories': ['ICLR.cc']
 };
-module.exports.iclr = iclr;
 
-var iclr17 = {
-  'id': 'ICLR.cc/2017',
-  'signatures': ['ICLR.cc'],
-  'writers': ['ICLR.cc'],
+var iclr2017 = {
+  'id': iclr.id+'/2017',
+  'signatures': [iclr.id],
+  'writers': [iclr.id],
   'readers': ['everyone'],
-  'members': programChairs,
-  'signatories': ['ICLR.cc/2017']
+  'members': [],
+  'signatories': [iclr.id+'/2017']
 };
-module.exports.iclr17 = iclr17;
 
-var workshop = {
-  'id': 'ICLR.cc/2017/workshop',
-  'signatures': ['ICLR.cc/2017'],
-  'writers': ['ICLR.cc/2017'],
+var iclr2017workshop = {
+  'id': iclr2017.id+'/workshop',
+  'signatures': [iclr2017.id],
+  'writers': [iclr2017.id],
   'readers': ['everyone'],
-  'members': ['Program_Chair_1', 'Area_Chair_1'],
-  'signatories': ['ICLR.cc/2017', 'ICLR.cc/2017/workshop'],
+  'members': [], //members of the workshop group are set below
+  'signatories': [iclr2017.id+'/workshop'],
   'web': fs.readFileSync('./iclr2017_webfield.html', "utf8")
 };
-module.exports.workshop = workshop
 
-var areaChairs = {
-  'id':'ICLR.cc/2017/workshop/areaChairs',
-  'signatures':['ICLR.cc/2017/workshop'],
-  'writers':['ICLR.cc/2017/workshop'],
+var iclr2017workshopAreaChairs = {
+  'id': iclr2017workshop.id+'/areaChairs',
+  'signatures':[iclr2017workshop.id],
+  'writers':[iclr2017workshop.id],
   'readers':['everyone'],
-  'members':[areaChair_1.id],
-  'signatories':[]
+  'members':[], //members of the area chairs group are set below
+  'signatories':[iclr2017workshop.id+'/areaChairs']
 }
 
-var programChairs = {
-  'id': workshop.id+'/programChairs',
-  'signatures': [workshop.id],
-  'writers': [workshop.id],
+var iclr2017workshopProgramChairs = {
+  'id': iclr2017workshop.id+'/programChairs',
+  'signatures': [iclr2017workshop.id],
+  'writers': [iclr2017workshop.id],
   'readers': ['everyone'],
-  'members': [programChair_1.id, programChair_2.id, programChair_3.id, programChair_4.id],
-  'signatories':[workshop.id]
+  'members': [], //members of program chairs group are set below
+  'signatories':[iclr2017workshop.id+'/programChairs']
 };
 
 
+
+var hugo = {
+  'id': 'hugo@openreview.net',
+  'first':'Hugo',
+  'last':'LaRochelle'
+};
+var programChair1 = {
+  'id': iclr2017workshopProgramChairs.id+'/1',
+  'signatures':[iclr2017workshopProgramChairs.id],
+  'writers':[iclr2017workshopProgramChairs.id],
+  'readers':['everyone'],
+  'members': [hugo.id],
+  'signatories': [iclr2017workshopProgramChairs.id+'/1', hugo.id]
+};
+
+
+
+var oriol = {
+  'id': 'oriol@openreview.net',
+  'first': 'Oriol',
+  'last': 'Vinyals'
+}
+var areaChair1 = {
+  'id': iclr2017workshopAreaChairs.id+'/1',
+  'signatures':[iclr2017workshopAreaChairs.id],
+  'writers':[iclr2017workshopAreaChairs.id],
+  'readers':['everyone'],
+  'members': [oriol.id],
+  'signatories': [iclr2017workshopAreaChairs.id+'/1', oriol.id]
+};
+
+iclr2017workshopProgramChairs.members = [programChair1.id];
+iclr2017workshopAreaChairs.members = [areaChair1.id];
+iclr2017workshop.members = [].concat(iclr2017workshopProgramChairs.members, iclr2017workshopAreaChairs.members);
+
+
+var areaChair1reviewers = {
+  'id': areaChair1.id+'/reviewers',
+  'signatures': [areaChair1.id],
+  'writers': [areaChair1.id],
+  'readers': ['everyone'],
+  'members': [], //added below
+  'signatories':[areaChair1.id+'/reviewers']
+};
+
+var michael = {
+  'id': 'spector@cs.umass.edu',
+  'first':'Michael',
+  'last':'Spector'
+};
+var reviewer1 = {
+  'id': 'reviewer-1',
+  'signatures': [areaChair1.id],
+  'writers': [areaChair1.id],
+  'readers': ['everyone'],
+  'members': [michael.id],
+  'signatories':[areaChair1reviewers.id+'/1',michael.id]
+}
+areaChair1reviewers.members = [reviewer1.id]
+
+
+var note1 = {
+  'content': {
+    'CMT_id':'',
+    'abstract':'This is note 1',
+    'author_emails':"author@gmail.com",
+    'authors':'Author 1',
+    'conflicts':'cs.berkeley.edu',
+    'pdf':'http://arxiv.org/pdf/1407.1808v1.pdf',
+    'title':'Note 1',
+    'keywords':'keyword'
+  },
+  'forum': null,
+  'invitation': iclr2017workshop.id+'/-/submission',
+  'parent': null,
+  'pdfTransfer':"url",
+  'readers':["everyone"],
+  'signatures':["~super_user1"],
+  'writers':["~super_user1"],
+};
+
+
+module.exports.iclr = iclr;
+module.exports.iclr2017 = iclr2017;
+module.exports.iclr2017workshop = iclr2017workshop
+module.exports.iclr2017workshopAreaChairs = iclr2017workshopAreaChairs;
+module.exports.iclr2017workshopProgramChairs = iclr2017workshopProgramChairs;
+module.exports.hugo = hugo;
+module.exports.programChair1 = programChair1;
+module.exports.oriol = oriol;
+module.exports.areaChair1 = areaChair1;
+module.exports.areaChair1reviewers = areaChair1reviewers
+module.exports.michael = michael;
+module.exports.reviewer1 = reviewer1;
+module.exports.note1 = note1;
