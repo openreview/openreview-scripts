7b1711fc465a876cdbe4b7992e3a657814fc256d
Michael Spector <michaelspector@gmail.com>
2016-08-31 13:20:57 -0400

    added MLITS conference

---
+++ nips2016_MLITS/process/MLITSProcess.js
@@ -0,0 +1,172 @@
+function () {
+  var or3client = lib.or3client;
+  
+  var commentProcess = function(){
+    var or3client = lib.or3client;
+
+    var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
+
+    var conference = or3client.prettyConferenceName(note);
+
+    origNote.then(function(result){
+      var mail = {
+        "groups": result.notes[0].content.author_emails.trim().split(","),
+        "subject": "Comment on your submission to " + conference + ": \"" + note.content.title + "\".",
+        "message": "Your submission to "+ conference +" has received a comment.\n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
+      };
+      var mailP = or3client.or3request( or3client.mailUrl, mail, 'POST', token )
+      
+    });
+
+    return true;
+  };
+
+  var reviewProcess = function(){
+    var or3client = lib.or3client;
+    var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
+    
+    var conference = or3client.prettyConferenceName(note);
+
+    origNote.then(function(result){
+      var mail = {
+        "groups": result.notes[0].content.author_emails.trim().split(","),
+        "subject": "Review of your submission to " + conference + ": \"" + note.content.title + "\".",
+        "message": "Your submission to "+ conference +" has received a review.\n\nTitle: "+note.content.title+"\n\nReview: "+note.content.review+"\n\nTo view the review, click here: "+baseUrl+"/forum?id=" + note.forum
+      };
+      var mailP = or3client.or3request( or3client.mailUrl, mail, 'POST', token )
+      
+    });
+    var fulfilledP = or3client.fulfillInvitation(invitation, note, token);
+    return true;
+  };
+
+  var commentInvite = {
+    'id': 'NIPS.cc/2016/workshop/MLITS/-/paper'+note.number+'/comment',
+    'signatures':['NIPS.cc/2016/workshop/MLITS'],
+    'writers':['NIPS.cc/2016/workshop/MLITS'],
+    'invitees': ['~'],
+    'noninvitees': [],
+    'readers': ['everyone'],
+    'process': commentProcess+'',
+    'reply': {
+      'forum': note.forum,      // 'parent' not specified so we can allow comments on comments
+      'signatures': {
+        'values-regex':'~.*',
+        'description': 'Your displayed identity associated with the above content.' 
+        },
+      'writers': {'values-regex':'~.*'},
+      'readers': { 
+        'values': ['everyone'], 
+        'description': 'The users who will be allowed to read the above content.'
+        },
+      'content': {
+        'title': {
+          'order': 1,
+          'value-regex': '.{1,500}',
+          'description': 'Brief summary of your comment.'
+        },
+        'comment': {
+          'order': 2,
+          'value-regex': '[\\S\\s]{1,5000}',
+          'description': 'Your comment or reply.'
+        }
+      }
+    }
+  };
+
+  var openReviewInvitation = { 
+    'id': 'NIPS.cc/2016/workshop/MLITS/-/paper'+note.number+'/open/review',
+    'signatures': ['NIPS.cc/2016/workshop/MLITS'],
+    'writers': ['NIPS.cc/2016/workshop/MLITS'],
+    'invitees': ['~'],
+    'noninvitees':[],
+    'readers':['everyone'],
+    'process': reviewProcess+'',
+    'reply': {
+      'forum': note.id, 
+      'parent': note.id,
+      'signatures': {
+        'values-regex':'~.*|NIPS.cc/2016/workshop/MLITS/paper'+note.number+'/reviewer[0-9]+',
+        'description': 'Your displayed identity associated with the above content.' 
+      },
+      'writers': {'values-regex':'~.*|NIPS.cc/2016/workshop/MLITS/paper'+note.number+'/reviewer[0-9]+'},
+      'readers': { 
+        'values': ['everyone'], 
+        'description': 'The users who will be allowed to read the above content.'
+        },
+      'content': {
+        'title': {
+          'order': 1,
+          'value-regex': '.{0,500}',
+          'description': 'Brief summary of your review.'
+        },
+        'review': {
+          'order': 2,
+          'value-regex': '[\\S\\s]{1,5000}',
+          'description': 'Please provide an evaluation of the quality, clarity, originality and significance of this work, including a list of its pros and cons.'
+        },
+        'rating': {
+          'order': 3,
+          'value-dropdown': [
+            '10: Top 5% of accepted papers, seminal paper', 
+            '9: Top 15% of accepted papers, strong accept', 
+            '8: Top 50% of accepted papers, clear accept', 
+            '7: Good paper, accept',
+            '6: Marginally above acceptance threshold',
+            '5: Marginally below acceptance threshold',
+            '4: Ok but not good enough - rejection',
+            '3: Clear rejection',
+            '2: Strong rejection',
+            '1: Trivial or wrong'
+          ]
+        },
+        'confidence': {
+          'order': 4,
+          'value-radio': [
+            '5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature', 
+            '4: The reviewer is confident but not absolutely certain that the evaluation is correct', 
+            '3: The reviewer is fairly confident that the evaluation is correct',
+            '2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper',
+            '1: The reviewer\'s evaluation is an educated guess'
+          ]
+        }
+      } 
+    }
+  };
+
+  // Create an empty group for this paper, e.g. "ICLR.cc/2017/conference/paper444"
+  var paperGroup = {
+    'id': 'NIPS.cc/2016/workshop/MLITS/paper'+note.number,
+    'signatures': ['NIPS.cc/2016/workshop/MLITS'],
+    'writers': ['NIPS.cc/2016/workshop/MLITS'],
+    'members': [],
+    'readers': ['everyone'],
+    'signatories': ['NIPS.cc/2016/workshop/MLITS', 'NIPS.cc/2016/workshop/MLITS/paper'+note.number]
+  };
+
+  var reviewerGroup = {
+    'id': paperGroup.id+'/reviewers',
+    'signatures':['NIPS.cc/2016/workshop/MLITS'],
+    'writers':['NIPS.cc/2016/workshop/MLITS'],
+    'members':[],
+    'readers':['everyone'],
+    'signatories':['NIPS.cc/2016/workshop/MLITS',paperGroup.id+'/reviewers']
+  };
+
+  //Send an email to the author of the submitted note, confirming its receipt
+  var conference = or3client.prettyConferenceName(note);
+
+  var mail = {
+    "groups": note.content.author_emails.trim().split(","),
+    "subject": "Submission to " + conference + " received: \"" + note.content.title + "\".",
+    "message": "Your submission to "+ conference +" has been posted.\n\nTo view the note, click here: "+baseUrl+"/forum?id=" + note.forum
+  };
+
+
+  or3client.or3request(or3client.inviteUrl, commentInvite, 'POST',token).catch(error=>console.log(error))
+  .then(result => or3client.or3request(or3client.inviteUrl, openReviewInvitation, 'POST', token).catch(error=>console.log(error)))
+  .then(result => or3client.or3request( or3client.mailUrl, mail, 'POST', token ).catch(error=>console.log(error)))
+
+
+  return true;
+};
