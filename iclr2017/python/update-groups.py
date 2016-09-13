55848ddb1952e90c32c823b887c0f2233adee563
mspector <michael.l.spector@gmail.com>
2016-09-13 16:33:44 -0400

    Fix/workflow refactor (#81)
    * removed setup-groups, setup invitations; added superuser-init, updated webfield
    
    * updated superuser-init to require conflicts information"
    
    * added a check in superuser init to see if the groups exist
    
    * added email to update-groups: now when the reviewers-invited group is updated, an email is sent to the newly added members
    
    * removed invitation due date

---
+++ iclr2017/python/update-groups.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+"""
+Setup python script takes as input the CSV files above and adds them as
+members to the groups ICLR.cc/2017/pc, areachairs, and reviewers-invited.
+
+"""
+
+## Import statements
+import argparse
+import csv
+import sys
+from openreview import *
+
+## Handle the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--programchairs', help="csv file containing the email addresses of the program chair(s)")
+parser.add_argument('-a','--areachairs', help="csv file containing the email addresses of the area chairs")
+parser.add_argument('-r','--reviewers', help="csv file containing the email addresses of candidate reviewers. Will send invitation email to the addresses on this list.")
+parser.add_argument('-u','--baseurl', help="base URL for the server to connect to")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+## Initialize the client library with username and password
+if args.username!=None and args.password!=None:
+    openreview = Client(baseurl=args.baseurl, username=args.username, password=args.password)
+else:
+    openreview = Client(baseurl=args.baseurl)
+
+groups = [];
+
+
+def sendMail(reviewers_invited):
+    ## For each candidate reviewer, send an email asking them to confirm or reject the request to review
+    for count, reviewer in enumerate(reviewers_invited):
+        print "Sending message to "+reviewer
+        hashkey = openreview.get_hash(reviewer, "4813408173804203984")
+        url = openreview.baseurl+"/invitation?id=ICLR.cc/2017/conference/-/reviewer_invitation&email=" + reviewer + "&key=" + hashkey + "&response="
+        message = "You have been invited to serve as a reviewer for the International Conference on Learning Representations (ICLR) 2017 Conference.\n\n"
+        message = message+ "To ACCEPT the invitation, please click on the following link: \n\n"
+        message = message+ url + "Yes\n\n"
+        message = message+ "To DECLINE the invitation, please click on the following link: \n\n"
+        message = message+ url + "No\n\n" + "Thank you"
+
+        openreview.send_mail("OpenReview invitation response", [reviewer], message)
+
+
+def update_group_members(groupid,csvfile):
+    new_members = []
+    group = openreview.get_group(groupid)
+    if type(group)==Group:
+        with open(csvfile, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in reader:
+                for email in row:
+                    new_members.append(email)
+        print "updating group ",g
+        group.members += new_members
+    else:
+        print "could not find group ",g
+    
+    if groupid=='ICLR.cc/2017/conference/reviewers-invited':
+        sendMail(new_members)
+
+    return group
+
+
+for g,c in [('ICLR.cc/2017/pcs',args.programchairs),('ICLR.cc/2017/areachairs',args.areachairs),('ICLR.cc/2017/conference/reviewers-invited',args.reviewers)]:
+    if c!=None:
+        group = update_group_members(g,c)
+        if type(group)==Group:
+            openreview.post_group(group)
+
+    
