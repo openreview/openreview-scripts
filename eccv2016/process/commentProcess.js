67ed5d3d3b669cabf1706c14877f72e9ec24d2a4
Melisa Bok <melisabok@gmail.com>
2016-09-21 10:23:08 -0400

    Feature/eccv template functions (#93)
    * change the name of the invitation
    add a duedate to the pre-review invitation
    add error handling to the officail review process function
    
    * template for eccv process functions

---
+++ eccv2016/process/commentProcess.js
@@ -0,0 +1,21 @@
+function(){
+    var or3client = lib.or3client;
+
+    var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
+
+    var conference = or3client.prettyConferenceName(note);
+
+    origNote.then(function(result){
+      var mail = {
+        "groups": result.notes[0].content.author_emails.trim().split(","),
+        "subject": "Comment on your submission to " + conference + ": \"" + note.content.title + "\".",
+        "message": "Your submission to "+ conference +" has received a comment.\n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
+      };
+      return or3client.or3request( or3client.mailUrl, mail, 'POST', token );
+      
+    })
+    .then(result => done())
+    .catch(error => done(error));
+
+    return true;
+  };
