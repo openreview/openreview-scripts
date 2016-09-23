d59d5796f3979e4762dad440bce274109d69571f
Melisa Bok <melisabok@gmail.com>
2016-09-23 15:43:59 -0400

    Change landing page

--- iclr2017/python/update-invitations.py
+++ iclr2017/python/update-invitations.py
@@ -9,7 +9,8 @@ from openreview import *
 ## Handle the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('invitation', help="invitation to update")
-parser.add_argument('process', help="process file")
+parser.add_argument('--process', help="process file")
+parser.add_argument('--webfield', help="html file that will replace the current ICLR 2017 homepage")
 parser.add_argument('--baseurl', help="base URL")
 parser.add_argument('--username')
 parser.add_argument('--password')
@@ -26,10 +27,17 @@ else:
 invitation = openreview.get_invitation(args.invitation)
 
 if type(invitation) == Invitation: 
-    with open(args.process) as f: 
-        invitation.process = f.read()
-        updated_invitation = openreview.post_invitation(invitation)
-        print "Updated invitation",updated_invitation.to_json().get('id')
+	
+	if(args.process):
+		with open(args.process) as f:
+			invitation.process = f.read()
+
+	if(args.webfield):
+		with open(args.webfield) as f:
+			invitation.web = f.read()
+
+	updated_invitation = openreview.post_invitation(invitation)
+	print "Updated invitation",updated_invitation.to_json().get('id')
 else:
     print "Invitation not found"
     
