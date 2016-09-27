eba0da563cc7d48b6268c18223273f0b05b5b0b9
Michael Spector <michaelspector@gmail.com>
2016-09-27 10:22:46 -0400

    cleaned up update-groups function

--- iclr2017/python/update-groups.py
+++ iclr2017/python/update-groups.py
@@ -34,23 +34,7 @@ groups = [];
 def SingleAssignmentValid(s):
     if not '@' in s:
         return False
-
     return True
-
-def sendMail(reviewers_invited):
-    ## For each candidate reviewer, send an email asking them to confirm or reject the request to review
-    for count, reviewer in enumerate(reviewers_invited):
-        print "Sending message to "+reviewer
-        hashkey = openreview.get_hash(reviewer, "4813408173804203984")
-        url = openreview.baseurl+"/invitation?id=ICLR.cc/2017/conference/-/reviewer_invitation&email=" + reviewer + "&key=" + hashkey + "&response="
-        message = "You have been invited to serve as a reviewer for the International Conference on Learning Representations (ICLR) 2017 Conference.\n\n"
-        message = message+ "To ACCEPT the invitation, please click on the following link: \n\n"
-        message = message+ url + "Yes\n\n"
-        message = message+ "To DECLINE the invitation, please click on the following link: \n\n"
-        message = message+ url + "No\n\n" + "Thank you"
-
-        openreview.send_mail("OpenReview invitation response", [reviewer], message)
-
 
 def update_group_members(groupid,assignment):
     new_members = []
@@ -76,9 +60,6 @@ def update_group_members(groupid,assignment):
         group.members += new_members
     else:
         print "could not find group ",g
-    
-    # if groupid=='ICLR.cc/2017/conference/reviewers-invited':
-    #     sendMail(new_members)
 
     return group
 
