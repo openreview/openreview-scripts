00c18d4ada7eb09886e42541f68333446db5906e
Michael Spector <michaelspector@gmail.com>
2016-09-28 08:42:34 -0400

    de-linted acceptanceProcess

--- nips2016_NAMPI/process/acceptanceProcess.js
+++ nips2016_NAMPI/process/acceptanceProcess.js
@@ -1,4 +1,5 @@
-function(){
+var acceptanceProcess = function(){
+    "use strict";
     var or3client = lib.or3client;
     or3client.addInvitationNoninvitee(note.invitation, note.signatures[0],token)
     .then(result => done())
