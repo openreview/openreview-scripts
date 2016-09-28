b443c67a35c91cb05072296d05a1fe0e077fb305
Michael Spector <michaelspector@gmail.com>
2016-09-28 10:07:17 -0400

    de-linted response invitation process

--- nips2016_NAMPI/process/responseInvitationProcess.js
+++ nips2016_NAMPI/process/responseInvitationProcess.js
@@ -1,33 +1,23 @@
 function() {
-
+  "use strict";
   var or3client = lib.or3client;
   var hashKey = or3client.createHash(note.content.email, "4813408173804203984");
   if(hashKey == note.content.key) {
     if (note.content.response == 'Yes') {
-      console.log("Invitation replied Yes")
-      //if a user is in the declined group, remove them from that group and add them to the reviewers group
       or3client.removeGroupMember(invitation.signatures[0]+'/reviewers-declined', note.content.email, token)
-      or3client.addGroupMember(invitation.signatures[0]+'/reviewers', note.content.email, token)
-      .then(function(result) {
-        var message = "The user " + note.content.email + " has accepted the invitation to serve as a reviewer."
-      })
+      .then(result => or3client.addGroupMember(invitation.signatures[0]+'/reviewers', note.content.email, token))
       .then(result => done())
       .catch(error => console.log(error));
     } else if (note.content.response == 'No'){
-      console.log("Invitation replied No")
-      //if a user is in the reviewers group, remove them from that group and add them to the reviewers-declined group
       or3client.removeGroupMember(invitation.signatures[0]+'/reviewers', note.content.email, token)
-      or3client.addGroupMember(invitation.signatures[0] + '/reviewers-declined', note.content.email, token)
-      .then(function(result) {
-        var message = "The user " + note.content.email + " has rejected the invitation to serve as a reviewer."
-      })  
+      .then(result=> or3client.addGroupMember(invitation.signatures[0] + '/reviewers-declined', note.content.email, token))
       .then(result => done())
       .catch(error => console.log(error));
     }
     return true;
   } else {
     console.log('Invalid key', note.content.key);
-    done()
+    done();
     return false;
   }
 }
