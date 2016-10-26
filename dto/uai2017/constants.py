e518d3798f1abdb55c67d80e32b9abce54200ded
Ankur Aggarwal <agankur21@gmail.com>
2016-10-25 23:01:14 -0400

    Scripts Added * Script to create Sample Meta data for reviewers* * Script to create Sample Meta data for submitted papers* *Data Transfer Objects for creating random samples of Meta Data Content*

---
+++ dto/uai2017/constants.py
@@ -0,0 +1,12 @@
+"""
+UAI Conference constants
+"""
+CONFERENCE_REVIEWERS = "UAI.org/2017/conference/reviewers"
+CONFERENCE_SUBMISSION = "UAI.org/2017/conference/-/submission"
+CONFERENCE ="UAI.org/2017/conference"
+CONFERENCE_PCS="UAI.org/2017/pcs"
+CONFERENCE_ACS="UAI.org/2017/acs"
+
+INVITATION_REPLY = {
+    'content': {}
+}
