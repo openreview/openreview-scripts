16720fe029c0c3d0af2b295df1f220f311b877a3
Melisa Bok <melisabok@gmail.com>
2016-11-15 09:45:31 -0500

    two scripts to remove the assignments of areachairs and reviewers. (#142)

---
+++ iclr2017/python/unassign-reviewers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import argparse
+import csv
+from openreview import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('assignments', help="either (1) a csv file containing reviewer unassignments or (2) a string of the format '<paper#>, <email_address>' e.g. '23,reviewer@cs.umass.edu'")
+parser.add_argument('--baseurl', help="base url")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+if args.username!=None and args.password!=None:
+    openreview = Client(baseurl=args.baseurl, username=args.username, password=args.password)
+else:
+    openreview = Client(baseurl=args.baseurl)
+baseurl = openreview.baseurl
+
+
+def unassign_reviewer(member, paper_number):
+
+	reviewers_id = 'ICLR.cc/2017/conference/paper' + str(paper_number)+ '/reviewers'
+	reviewers = openreview.get_group(reviewers_id)
+	if reviewers:
+		result = openreview.get_groups(member= member, regex = 'ICLR.cc/2017/conference/paper' + str(paper_number) + '/AnonReviewer[1-9]*')
+		if result and len(result) > 0:
+			reviewer = result[0]
+			res = openreview.remove_members_from_group(reviewers, [reviewer.id])
+			print "Member", member, "removed from", reviewers.id
+		else:
+			print "Member not found in the assingment", member
+	else:
+		print "Reviewers group not found", reviewers_id
+
+if args.assignments.endswith('.csv'):   
+    with open(args.assignments, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            paper_number = row[0]
+            member = row[1]
+            unassign_reviewer(member, paper_number)
+else:
+    paper_number = args.assignments.split(',')[0]
+    reviewer = args.assignments.split(',')[1]
+    unassign_reviewer(reviewer,paper_number)
+
