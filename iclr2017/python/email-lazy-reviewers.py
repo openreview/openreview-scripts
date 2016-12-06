2149f65fad98bd698942aac7657d83f592029ce7
mspector <michael.l.spector@gmail.com>
2016-12-06 10:21:19 -0500

    Feature/update lazy reviewers script (#158)
    * updated lazy reviewers email to collect all email addresses first, then send them off all at once
    
    * removed testing code
    
    * corrected verbose line

--- iclr2017/python/email-lazy-reviewers.py
+++ iclr2017/python/email-lazy-reviewers.py
@@ -14,6 +14,7 @@ import openreview
 ## Handle the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--invitation', help="choose either 'question' to email users that have not submitted their pre-review question, or 'review' to do the same for official reviews")
+parser.add_argument('-v','--verbose', help="set to true if you want late users listed per-paper")
 parser.add_argument('--baseurl', help="base URL")
 parser.add_argument('--username')
 parser.add_argument('--password')
@@ -56,22 +57,39 @@ if args.invitation == 'question':
     invitation = 'pre-review/question'
 elif args.invitation == 'review':
     invitation = 'official/review'
+
+verbose = True if args.verbose.lower()=='true' else False
+
+
 total_missing = 0;
 total_complete = 0;
+
 if invitation:
-    print "Sending emails to users that did not submit their %s" % invitation
-    print "Emailing the following users:"
+
+    late_users = set()
+    print "Collecting users that did not submit their %s" % invitation
+
     for n in iclrsubs:
         revs = client.get_group('ICLR.cc/2017/conference/paper%s/reviewers' % n.number)
 
         notes = client.get_notes(invitation='ICLR.cc/2017/conference/-/paper%s/%s' % (n.number,invitation))
 
         ontime_reviewers = [r.signatures[0] for r in notes]
-        late_reviewers = [r for r in revs.members if r not in ontime_reviewers]
-        total_missing += len(late_reviewers)
+        late_AnonReviewers = [r for r in revs.members if r not in ontime_reviewers]
+
+        for l in late_AnonReviewers:
+            late_anonRev = client.get_group(l)
+            late_user = late_anonRev.members
+            if verbose:
+                print "late users on paper %s: %s" % (n.number,late_user)
+            late_users.update(late_user)
+
+        total_missing += len(late_AnonReviewers)
         total_complete += len(ontime_reviewers)
-        response = client.send_mail(subjectline, late_reviewers, message)
-        print "Paper %s: %s" % (n.number, response.json()['groups'])
+
+    response = client.send_mail(subjectline, list(late_users), message)
+    print "Emailing the following users:"
+    print response.json()['groups']
 
     print "%s: %s %ss missing" % (invitation,total_missing,args.invitation)
     print "%s: %s %ss complete" % (invitation,total_complete,args.invitation)
