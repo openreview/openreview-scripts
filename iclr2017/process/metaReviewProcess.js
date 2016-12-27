7e820b2ca12cc4640dbde9b6d70d723d42848386
Melisa Bok <melisabok@gmail.com>
2016-12-27 13:22:57 -0500

    update metareview invitation and process function

--- iclr2017/process/metaReviewProcess.js
+++ iclr2017/process/metaReviewProcess.js
@@ -1,6 +1,6 @@
 function(){
     var or3client = lib.or3client;
-    
+
     var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
     var list = note.invitation.replace(/_/g,' ').split('/');
     list.splice(list.indexOf('-',1));
@@ -10,27 +10,17 @@ function(){
     var programchairs = ['ICLR.cc/2017/pcs'];
 
     origNote.then(function(result){
-      var authors = result.notes[0].content.authorids;
-      var author_mail = {
-        "groups": authors,
-        "subject": "Meta-review of your submission to " + conference + ": \"" + note.content.title + "\".",
-        "message": "Your submission to "+ conference +" has received a meta-review by an area chair.\n\nTitle: "+note.content.title+"\n\nReview: "+note.content.review+"\n\nTo view the meta-review, click here: "+baseUrl+"/forum?id=" + note.forum
-      };
+      var forum = result.notes[0];
 
       var pc_mail = {
         "groups": programchairs,
-        "subject": "["+conference+"] Meta-review by area chair posted: "+ "\"" + note.content.title + "\".",
-        "message": "A paper submission to "+ conference +" has received a meta-review by an area chair.\n\nTitle: "+note.content.title+"\n\nReview: "+note.content.review+"\n\nTo view the meta-review, click here: "+baseUrl+"/forum?id=" + note.forum
+        "subject": "Meta-review by area chair posted: "+ "\"" + forum.content.title + "\".",
+        "message": "A paper submission to "+ conference +" has received a meta-review by an area chair.\n\nTitle: "+note.content.title+"\n\nMeta-review: "+note.content.metareview+"\n\nTo view the meta-review, click here: "+baseUrl+"/forum?id=" + note.forum
       };
 
-      var promises = [
-        or3client.or3request( or3client.mailUrl, author_mail, 'POST', token ),
-        or3client.or3request( or3client.mailUrl, pc_mail, 'POST', token )
-      ];
-      return Promise.all(promises)
-
+      return or3client.or3request( or3client.mailUrl, pc_mail, 'POST', token );
     })
-    .then(or3client.addInvitationNoninvitee(note.invitation, note.signatures[0],token))   
+    .then(or3client.addInvitationNoninvitee(note.invitation, note.signatures[0],token))
     .then(result => done())
     .catch(error=>done(error));
 
