abac31e4f9e0027b3fad9491b37e4bb40a18f3df
Melisa Bok <melisabok@gmail.com>
2017-01-09 17:23:03 -0500

    fix email message

--- uai2017/process/commentProcess.js
+++ uai2017/process/commentProcess.js
@@ -29,7 +29,7 @@ function(){
         var reviewer_mail = {
           "groups": reviewers,
           "subject": "Comment posted to your assigned paper: \"" + forumNote.content.title + "\"",
-          "message": "A submission to UAI 2017, for which you are a Program Committee, has received a comment. \n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
+          "message": "A submission to UAI 2017, for which you are a Program Committee member, has received a comment. \n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
         };
         return or3client.or3request( or3client.mailUrl, reviewer_mail, 'POST', token );
       })
@@ -49,7 +49,7 @@ function(){
         var areachair_mail = {
           "groups": areachairs,
           "subject": "Comment posted to your assigned paper: \"" + forumNote.content.title + "\"",
-          "message": "A submission to UAI 2017, for which you are an Senior Program Committee, has received a comment. \n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
+          "message": "A submission to UAI 2017, for which you are a Senior Program Committee member, has received a comment. \n\nTitle: "+note.content.title+"\n\nComment: "+note.content.comment+"\n\nTo view the comment, click here: "+baseUrl+"/forum?id=" + note.forum
         };
         return or3client.or3request( or3client.mailUrl, areachair_mail, 'POST', token );
       })
