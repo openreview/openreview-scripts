f983fb16d058098e2d6857dc877e598213ec0f06
Michael Spector <michaelspector@gmail.com>
2017-01-10 11:43:28 -0500

    removed author notifications on reviews

--- uai2017/process/reviewProcess.js
+++ uai2017/process/reviewProcess.js
@@ -11,24 +11,15 @@ function(){
       var origNoteTitle = result.notes[0].content.title;
       var reviewers = ['auai.org/UAI/2017/Paper'+note_number+'/Reviewers'];
       var areachairs = ['auai.org/UAI/2017/Paper'+note_number+'/Area_Chair'];
-      var authors = result.notes[0].content.authorids;
-
-      var author_mail = {
-        "groups": authors,
-        "subject": "Review of your submission to UAI 2017: \"" + origNoteTitle + "\"",
-        "message": "Your submission to UAI 2017 has received an official review.\n\nTitle: "+note.content.title+"\n\nReview: "+note.content.review+"\n\nTo view the review, click here: "+baseUrl+"/forum?id=" + note.forum
-      };
 
       var areachair_mail = {
         "groups": areachairs,
         "subject": "Review posted to your assigned paper: \"" + origNoteTitle + "\"",
         "message": "A submission to UAI 2017, for which you are an official area chair, has received an official review. \n\nTitle: "+note.content.title+"\n\nComment: "+note.content.review+"\n\nTo view the review, click here: "+baseUrl+"/forum?id=" + note.forum
       };
-      var authorMailP = or3client.or3request( or3client.mailUrl, author_mail, 'POST', token );
       var areachairMailP = or3client.or3request( or3client.mailUrl, areachair_mail, 'POST', token );
 
       return Promise.all([
-        authorMailP,
         areachairMailP
       ])
     })
