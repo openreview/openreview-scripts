2b32aa5319e2a1cbe31a390161e1ee05927ed9e1
Michael Spector <michaelspector@gmail.com>
2017-01-12 10:58:57 -0500

    updates

--- iclr2017/python/invite-workshop-reviewers.py
+++ iclr2017/python/invite-workshop-reviewers.py
@@ -8,7 +8,7 @@
 import argparse
 import csv
 import sys
-from openreview import *
+import openreview
 
 ## Handle the arguments
 parser = argparse.ArgumentParser()
@@ -20,33 +20,34 @@ args = parser.parse_args()
 
 ## Initialize the client library with username and password
 if args.username!=None and args.password!=None:
-    openreview = Client(baseurl=args.baseurl, username=args.username, password=args.password)
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 else:
-    openreview = Client(baseurl=args.baseurl)
+    client = openreview.Client(baseurl=args.baseurl)
 
 
 message = """
-WORKSHOP INVITATION TEXT
+Dear Reviewer,
 
-To ACCEPT the invitation, please click on the following link:
+Thank you for your help thus far in reviewing for the main ICLR conference. We are writing to inform you that the Workshop Track review process for ICLR will begin on February 17th. Workshop papers include late-breaking developments, very novel ideas and position papers. The focus of the Workshop Track is to stimulate discussion of new ideas and directions.
+
+The review process for Workshop Track abstracts will be much lighter than for regular Conference Track submissions. Submissions are shorter (2-3 page abstract) compared to regular ICLR submissions. The review process consists of just a short review submitted by the reviewer, with optional discussion at the reviewer's and authors' discretion. We are planning to keep the load low and assign at most 4 abstracts per reviewer.
+
+Here's a tentative timeline for the ICLR Workshop Track review process:
+
+February 17: workshop author submission deadline
+March 10: full review deadline
+
+The success of ICLR depends on the quality of the reviewing process and ultimately on the quality and dedication of the reviewers.
+
+If you are *NOT* able to review for the Workshop Track, please click on the link below:
 
 %s
 
-To DECLINE the invitation, please click on the following link:
-
-%s
-
-We'd appreciate an answer within 10 days.
-
-If you accept, please make sure to either update your Toronto Paper Matching System (TPMS) account, or create one if you do not have one already. We will be using TPMS to assign reviewers to papers, and having an account that reflects your expertise will be crucial for you to receive papers for which you are suited. Also please make sure your OpenReview account lists the email you are using for your TPMS account.
-
-Here's a tentative timeline for the ICLR reviewing process:
-
-TIMELINE
+Otherwise, a few Workshop Track reviews will be assigned to you in your OpenReview account a few days after the February 17th deadline.
 
 If you have any question, please contact the program chairs at iclr2017.programchairs@gmail.com .
 
-We are looking forward to your reply, and are grateful if you accept this invitation and help make ICLR 2017 a success!
+Thank you for helping us make ICLR 2017 a success!
 
 Cheers!
 
@@ -63,20 +64,19 @@ def sendMail(reviewers_invited):
     ## For each candidate reviewer, send an email asking them to confirm or reject the request to review
     for count, reviewer in enumerate(reviewers_invited):
         print "Sending message to "+reviewer
-        hashkey = openreview.get_hash(reviewer, "4813408173804203984")
-        url = openreview.baseurl+"/invitation?id=ICLR.cc/2017/workshop/-/reviewer_invitation&email=" + reviewer + "&key=" + hashkey + "&response="
+        hashkey = client.get_hash(reviewer, "4813408173804203984")
+        url = client.baseurl+"/invitation?id=ICLR.cc/2017/workshop/-/reviewer_invitation&email=" + reviewer + "&key=" + hashkey + "&response="
 
-        openreview.send_mail("Reviewer Invitation for ICLR 2017", [reviewer], message %(url + "Yes", url + "No"))
+        client.send_mail("Reviewer Invitation for ICLR 2017: Workshop Track", [reviewer], message % (url+"No"))
 
 
-if openreview.exists("ICLR.cc/2017/conference/reviewers") and openreview.exists("ICLR.cc/2017/workshop/reviewers-emailed"):
-    reviewers = openreview.get_group("ICLR.cc/2017/conference/reviewers")
-    reviewers_emailed = openreview.get_group("ICLR.cc/2017/workshop/reviewers-emailed")
-    recipients = [reviewer for reviewer in reviewers.members if reviewer not in reviewers_emailed.members]
+if client.exists("ICLR.cc/2017/conference/reviewers") and client.exists("ICLR.cc/2017/workshop/reviewers-emailed"):
+    conference_reviewers = client.get_group("ICLR.cc/2017/conference/reviewers")
+    workshop_reviewers_emailed = client.get_group("ICLR.cc/2017/workshop/reviewers-emailed")
+    recipients = [reviewer for reviewer in conference_reviewers.members if reviewer not in workshop_reviewers_emailed.members]
     sendMail(recipients)
-    reviewers_emailed.members = reviewers_emailed.members+recipients
-    openreview.post_group(reviewers_emailed)
+    client.add_members_to_group(workshop_reviewers_emailed,recipients)
 else:
-    print "Error while retrieving ICLR.cc/2017/workshop/reviewers-invited; group may not exist"
+    print "Error while retrieving the groups. \nICLR.cc/2017/conference/reviewers exists? %s\nICLR.cc/2017/workshop/reviewers-emailed exists? %s" % (client.exists("ICLR.cc/2017/conference/reviewers"), client.exists("ICLR.cc/2017/workshop/reviewers-emailed"))
 
 
