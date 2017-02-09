e8f948dba66849b043f3b25e0d68773a2a3b79c5
Michael Spector <michaelspector@gmail.com>
2017-02-09 15:06:49 -0500

    small fixes

--- uai2017/process/pc_responseInvitationProcess_uai2017.js
+++ uai2017/process/pc_responseInvitationProcess_uai2017.js
@@ -4,31 +4,33 @@ function() {
   var hashKey = or3client.createHash(note.content.email, "2810398440804348173");
   if(hashKey == note.content.key) {
     if (note.content.response == 'Yes') {
-      console.log("Invitation replied Yes")
+      console.log("Invitation replied Yes");
       //if a user is in the declined group, remove them from that group and add them to the reviewers group
       or3client.removeGroupMember('auai.org/UAI/2017/Program_Committee/declined', note.content.email, token)
-      or3client.addGroupMember('auai.org/UAI/2017/Program_Committee', note.content.email, token)
-      .then(function(result) {
-        var message = "The user " + note.content.email + " has accepted the invitation to serve as a reviewer."
-        //return or3client.or3request(or3client.mailUrl, { groups: [invitation.signatures[0]], subject: "OpenReview invitation accepted" , message: message}, 'POST', token);
+      .then(function(result){
+        or3client.addGroupMember('auai.org/UAI/2017/Program_Committee', note.content.email, token);
       })
-      .then(result => done())
-      .catch(error => console.log(error));
+      .then(function(result) {
+        return "The user " + note.content.email + " has accepted the invitation to serve as a reviewer.";
+      })
+      .then(result => done(result))
+      .catch(error => done(error));
     } else if (note.content.response == 'No'){
-      console.log("Invitation replied No")
+      console.log("Invitation replied No");
       //if a user is in the reviewers group, remove them from that group and add them to the reviewers-declined group
       or3client.removeGroupMember('auai.org/UAI/2017/Program_Committee', note.content.email, token)
-      or3client.addGroupMember('auai.org/UAI/2017/Program_Committee/declined', note.content.email, token)
-      .then(function(result) {
-        var message = "The user " + note.content.email + " has rejected the invitation to serve as a reviewer."
-        //return or3client.or3request(or3client.mailUrl, { groups: [invitation.signatures[0]], subject: "OpenReview invitation rejected" , message: message}, 'POST', token);
+      .then(function(result){
+        or3client.addGroupMember('auai.org/UAI/2017/Program_Committee/declined', note.content.email, token);
       })
-      .then(result => done())
-      .catch(error => console.log(error));
+      .then(function(result) {
+        return "The user " + note.content.email + " has rejected the invitation to serve as a reviewer.";
+      })
+      .then(result => done(result))
+      .catch(error => done(error));
     }
     return true;
   } else {
-    console.log('Invalid key', note.content.key);
+    done('Invalid key: ',note.content.key);
     return false;
   }
 }
