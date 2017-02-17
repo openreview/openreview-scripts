57a484c202f70f8c5470fa93cc4e0f83907eedb7
pmandler <mandler@cs.umass.edu>
2017-02-17 15:50:34 -0500

    anonymize note authors (#237)
    * anonymize note authors
    
    * fix pdf url and use client.get_profile()

--- iclr2017/python/get-submission-notes.py
+++ iclr2017/python/get-submission-notes.py
@@ -1,5 +1,6 @@
 import argparse
 import openreview
+import requests
 import csv
 
 #####################################################################################
@@ -28,6 +29,12 @@ if args.ifile!=None:
 submissions = client.get_notes(invitation='ICLR.cc/2017/conference/-/submission')
 allnotes = client.get_notes(invitation='ICLR.cc/2017/conference/-/paper.*/*')
 
+headers = {'User-Agent': 'test-create-script', 'Content-Type': 'application/json',
+           'Authorization': 'Bearer ' + client.token}
+anon_reviewers = requests.get(client.baseurl + '/groups?id=ICLR.cc/2017/conference/paper.*/AnonReviewer.*',
+                              headers=headers)
+area_chairs = requests.get(client.baseurl+'/groups?id=ICLR.cc/2017/conference/paper.*/areachair.*', headers = headers)
+
 # paper_status[paper_num]['pdf'] = url to the submission pdf
 # paper_status[paper_num][note_id] dictionary including author, timestamp, type and content
 paper_status = {}
@@ -38,10 +45,8 @@ for paper in submissions:
 
 # add each note to the associated paper in paper_status
 for note in allnotes:
-    paper_num0 = note.invitation.split('paper')[1].split('/')[0]
-    note_type = note.invitation.split('paper' + str(paper_num0))[1]
-    # WHY does paper_num work and paper_num0 not?
-    paper_num = int(note.invitation.split('paper')[1].split(note_type)[0])
+    paper_num = int(note.invitation.split('paper')[1].split('/')[0])
+    note_type = note.invitation.split('paper'+str(paper_num))[1]
     if paper_num in paper_status:
         author = note.signatures[0]
         id_str = note.id
@@ -52,9 +57,71 @@ for note in allnotes:
             paper_status[paper_num][id_str]['type'] = note_type
             paper_status[paper_num][id_str]['content'] = note.content
         else:
-            note_type = note.invitation.split('paper' + str(paper_num))[1]
-            print("%s %s MISSED %s for %s" % (paper_num, author, note_type,
-                                              paper_status[paper_num][id_str]['type']))
+            note_type = note.invitation.split('paper'+str(paper_num))[1]
+            print("%s %s MISSED %s for %s" %(paper_num,author, note_type,
+                                                           paper_status[paper_num][id_str]['type']))
+
+# in order to come up with one set of anonymous names even thought the same person might be
+# reviewer1 on paper 324 and reviewer2 on paper 444, we find all of the real names,
+# then convert them each into a number
+
+# profile_by_email[email_id] = ~name
+profile_by_email = {}
+def check_profile(member):
+
+    if member.startswith('~'):
+        return member
+
+    if '@' in member:
+        if member in profile_by_email:
+            return profile_by_email[member]
+
+        response = requests.get(client.baseurl+'/user/profile?email=' + member, headers = headers)
+        if 'profile' not in response.json():
+            profile_by_email[member] = member
+            return member
+        profile = response.json()['profile']
+        profile_by_email[member] = profile['id']
+        return profile_by_email[member]
+
+    return member
+
+# attach real name to the anonymized name for reviewers
+reviewers = {}
+for r in anon_reviewers.json():
+    reviewer_id = r['id']
+    members = r['members']
+    if members:
+        reviewers[reviewer_id] = check_profile(members[0])
+
+# attach real name to the anonymized name for area chairs
+# and convert email ids to names
+for r in area_chairs.json():
+    reviewer_id = r['id']
+    # only look at areachair1,2... not "areachairs"
+    if not reviewer_id.endswith('areachairs'):
+        members = r['members']
+        if members:
+            reviewers[reviewer_id] = check_profile(members[0])
+
+# make sure ALL names are in
+for paper_num in paper_status:
+    for note_id in paper_status[paper_num]:
+        if note_id != 'paper_id':
+            name = paper_status[paper_num][note_id]['author']
+            if name not in reviewers:
+                reviewers[name] = name
+
+# now that we know the real ID of everyone, we hide all of the names....
+new_val = 0
+for r in reviewers:
+    name = reviewers[r]
+    if type(name) != int:
+        new_val += 1
+        # it would be nice to start from 'r' and move forward
+        for y in reviewers:
+            if reviewers[y] == name:
+                reviewers[y] = new_val
 
 # print csv file
 with open(file_name, 'wb') as outfile:
@@ -71,12 +138,14 @@ with open(file_name, 'wb') as outfile:
     for paper_num in paper_status:
         for note_id in paper_status[paper_num]:
             if note_id != 'paper_id':
+                pdf_url = 'https://openreview.net/pdf?id='+paper_status[paper_num]['paper_id'].encode('utf-8')
+                author_name = 'reviewer'+str(reviewers[paper_status[paper_num][note_id]['author']])
                 row = []
                 row.append(paper_num)
                 row.append(paper_status[paper_num][note_id]['timestamp'])
                 row.append(paper_status[paper_num][note_id]['type'].encode('utf-8'))
-                row.append(paper_status[paper_num][note_id]['author'].encode('utf-8'))
+                row.append(author_name)
                 row.append(paper_status[paper_num][note_id]['content'])
-                row.append('https://openreview.net/pdf?id='+paper_status[paper_num]['paper_id'].encode('utf-8'))
+                row.append(pdf_url)
                 csvwriter.writerow(row)
 
