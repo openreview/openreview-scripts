195ce356ee46f5165fa5e7efae8b1810fbd368e1
Pam Mandler <mandler@cs.umass.edu>
2017-03-30 17:57:36 -0400

    change duedate to 5:00pm, add 30min buffer to webfield

--- rss2017/python/rssdata.py
+++ rss2017/python/rssdata.py
@@ -12,6 +12,6 @@ POSTER = CONFERENCE+"/Poster"
 PROCEEDINGS = CONFERENCE+"/Proceedings"
 POSTER_REVIEWERS = POSTER+"/Reviewers"
 PROCEEDINGS_REVIEWERS = PROCEEDINGS+"/Reviewers"
-# Due date (guess) June 30, 2017 at 5:15pm (here)
-DATE_DUE = datetime.datetime(2017, 6, 30, 17, 15)
+# Due date (guess) June 30, 2017 at 5:00pm (here)
+DATE_DUE = datetime.datetime(2017, 6, 30, 17, 0)
 TIMESTAMP_DUE = int(time.mktime(DATE_DUE.timetuple()))*1000
