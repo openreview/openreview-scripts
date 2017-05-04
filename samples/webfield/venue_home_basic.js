2e6dfee61983e8698c8f8c0e8d8b1713d555c7a7
Zach Bialecki <zach.bialecki@gmail.com>
2017-05-04 19:37:31 -0400

    Get api for note creation working

--- samples/webfield/venue_home_basic.js
+++ samples/webfield/venue_home_basic.js
@@ -13,7 +13,7 @@ var BUFFER = 1000 * 60 * 30;  // 30 minutes
 
 // Main is the entry point to the webfield code and runs everything
 function main() {
-  Webfield.ui.setup('#group-container');
+  Webfield.ui.setup('#group-container');  // required
 
   renderConferenceHeader();
 
@@ -29,38 +29,42 @@ function renderConferenceHeader() {
     location: 'Toulon, France',
     date: 'April 24 - 26, 2017',
     website: 'http://www.iclr.cc',
+    instructions: null,  // Add any custom instructions here. Accepts HTML
     deadline: 'Submission Deadline Extended: Saturday, November 5th, 2016, at 5:00pm Eastern Daylight Time (EDT)'
   });
+
+  Webfield.ui.spinner('#notes');
 }
 
 // Load makes all the API calls needed to get the data to render the page
 // It returns a jQuery deferred object: https://api.jquery.com/category/deferred-object/
 function load() {
-  var invitationP = Webfield.getSubmissionInvitation(INVITATION, {deadlineBuffer: BUFFER});
-  var notesP = Webfield.getSubmissions(INVITATION);
+  var invitationP = Webfield.api.getSubmissionInvitation(INVITATION, {deadlineBuffer: BUFFER});
+  var notesP = Webfield.api.getSubmissions(INVITATION);
 
   return $.when(invitationP, notesP);
 }
 
 // Render is called when all the data is finished being loaded from the server
-// It also can be called when the page needs to be refreshed, for example when a user
+// It should also be called when the page needs to be refreshed, for example after a user
 // submits a new paper.
 function render(invitation, notes) {
   // Display submission button and form
   $('#invitation').empty();
-  var onInvitationButtonClicked = function(e) {};
-  Webfield.ui.invitationButton(invitation, onInvitationButtonClicked, { largeLabel: true });
-  Webfield.ui.noteEditor(invitation, function() {
-    // Callback funtion to be run when a paper has successfully been submitted
-    load().then(render);
+  Webfield.ui.submissionButton(invitation, user, {
+    onNoteCreated: function() {
+      // Callback funtion to be run when a paper has successfully been submitted (required)
+      load().then(render);
+    }
   });
 
   // Display the list of all submitted papers
-  $('#notes').empty().append(
-    '<h2>Submitted Papers</h2>',
-    '<hr>'
-  );
-  Webfield.ui.notesList(notes, { titleLink: true, replyCount: true });
+  $('#notes').empty();
+  Webfield.ui.notesList(notes, {
+    heading: 'Submitted Papers',
+    pdfLink: true,
+    replyCount: true
+  });
 }
 
 // Go!
