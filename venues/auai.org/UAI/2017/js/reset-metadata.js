6d0a6a5f025ff95a15b506b1f302738f07a3d5c1
Michael Spector <michaelspector@gmail.com>
2017-05-08 10:24:23 -0400

    add comment

--- venues/auai.org/UAI/2017/js/reset-metadata.js
+++ venues/auai.org/UAI/2017/js/reset-metadata.js
@@ -1,3 +1,5 @@
+// Run this script when the UAI paper metadata needs to be reset
+
 var notes = db._query("FOR note IN openreview_notes FILTER CONTAINS(note.invitation, 'auai.org/UAI/2017/-/Paper/Metadata') RETURN note._id").toArray()
 for(i=0;i<notes.length;i++){
 	db.openreview_notes.remove(notes[i])
