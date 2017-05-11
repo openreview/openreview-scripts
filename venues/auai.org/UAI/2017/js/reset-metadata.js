3bfeb1993502349a5838501e54971ee466ff34eb
Michael Spector <michaelspector@gmail.com>
2017-05-11 11:20:06 -0400

    add reset-metadata script

--- venues/auai.org/UAI/2017/js/reset-metadata.js
+++ venues/auai.org/UAI/2017/js/reset-metadata.js
@@ -4,3 +4,8 @@ var notes = db._query("FOR note IN openreview_notes FILTER CONTAINS(note.invitat
 for(i=0;i<notes.length;i++){
 	db.openreview_notes.remove(notes[i])
 }
+
+var refs = db._query("FOR note IN openreview_references FILTER CONTAINS(note.invitation, 'auai.org/UAI/2017/-/Paper/Metadata') RETURN note._id").toArray()
+for(i=0;i<refs.length;i++){
+	db.openreview_references.remove(refs[i])
+}
