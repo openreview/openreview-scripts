6a878e90de0238680cdde64cbcdf8aea24e1d80f
Melisa Bok <melisabok@gmail.com>
2017-05-18 13:46:59 -0400

    use variable

--- venues/auai.org/UAI/2017/python/email-lazy-reviewers.py
+++ venues/auai.org/UAI/2017/python/email-lazy-reviewers.py
@@ -62,12 +62,12 @@ def get_reviews():
     offset = 0
     limit = 1000
 
-    notes = client.get_notes(invitation='auai.org/UAI/2017/-/Paper.*/' + invitation, limit = limit, offset = offset)
+    notes = client.get_notes(invitation = invitation_id, limit = limit, offset = offset)
     all_notes = notes
 
     while len(notes) == limit:
         offset = offset + limit
-        notes = client.get_notes(invitation='auai.org/UAI/2017/-/Paper.*/' + invitation, limit = limit, offset = offset)
+        notes = client.get_notes(invitation = invitation_id, limit = limit, offset = offset)
         all_notes.extend(notes)
 
 
