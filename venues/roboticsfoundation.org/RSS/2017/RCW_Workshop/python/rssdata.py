bb424f721cf989a5d3b05f4f17d7da63f5ed5716
pmandler <mandler@cs.umass.edu>
2017-05-19 15:15:09 -0400

    change submission due date (#379)

--- venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
+++ venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
@@ -13,5 +13,5 @@ PROCEEDINGS = CONFERENCE+"/-_Proceedings"
 POSTER_REVIEWERS = POSTER+"/Reviewers"
 PROCEEDINGS_REVIEWERS = PROCEEDINGS+"/Reviewers"
 # Due date May 21, 2017 at 5:00pm (here)
-DATE_DUE = datetime.datetime(2017, 5, 21, 17, 0)
+DATE_DUE = datetime.datetime(2017, 5, 22, 7, 59)
 TIMESTAMP_DUE = int(time.mktime(DATE_DUE.timetuple()))*1000
