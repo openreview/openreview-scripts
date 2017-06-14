966b87b278db044a12df20d9b9ab0f57cdb1fd61
pmandler <mandler@cs.umass.edu>
2017-06-14 13:41:25 -0400

    minor fixes in RSS and Rostock (#414)

--- venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
+++ venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
@@ -13,5 +13,6 @@ PROCEEDINGS = CONFERENCE+"/-_Proceedings"
 POSTER_REVIEWERS = POSTER+"/Reviewers"
 PROCEEDINGS_REVIEWERS = PROCEEDINGS+"/Reviewers"
 # Due date May 21, 2017 at 5:00pm (here)
-DATE_DUE = datetime.datetime(2017, 7, 16, 7, 59)
+# June 17th 11:59:59pm AoE time = June 18 7:59:59 here
+DATE_DUE = datetime.datetime(2017, 7, 18, 7, 59, 59)
 TIMESTAMP_DUE = int(time.mktime(DATE_DUE.timetuple()))*1000
