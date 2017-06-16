e781807319cda605f64dc99a9146cb41550f6d51
Michael Spector <michaelspector@gmail.com>
2017-06-16 17:26:27 -0400

    new schema for matching data
    bug fixes in superuser init script
    
    moved matching script into the admin directory, for corresponding changes in openreview-matching
    
    rework matching API
    
    remove search-based feature from uai_features.py
    
    add standard metadata file, which will eventually become more generic
    
    update some variable names

--- venues/auai.org/UAI/2017/python/config.py
+++ venues/auai.org/UAI/2017/python/config.py
@@ -8,6 +8,7 @@ SPC = "auai.org/UAI/2017/Senior_Program_Committee"
 PC = "auai.org/UAI/2017/Program_Committee"
 CONFERENCE = "auai.org/UAI/2017"
 SUBMISSION = "auai.org/UAI/2017/-/blind-submission"
+METADATA = "auai.org/UAI/2017/-/Paper_Metadata"
 SUBJECT_AREAS = ["Algorithms: Approximate Inference",
 			    'Algorithms: Belief Propagation',
 			    'Algorithms: Distributed and Parallel',
