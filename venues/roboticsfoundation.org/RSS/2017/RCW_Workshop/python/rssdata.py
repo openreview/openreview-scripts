55cc7178429951673c426585380b82ab9ca56c2a
Pam Mandler <mandler@cs.umass.edu>
2017-06-19 17:09:04 -0400

    Fix RSS duedate from July to June

--- venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
+++ venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/python/rssdata.py
@@ -14,5 +14,4 @@ POSTER_REVIEWERS = POSTER+"/Reviewers"
 PROCEEDINGS_REVIEWERS = PROCEEDINGS+"/Reviewers"
 # Due date May 21, 2017 at 5:00pm (here)
 # June 17th 11:59:59pm AoE time = June 18 7:59:59 here
-DATE_DUE = datetime.datetime(2017, 7, 18, 7, 59, 59)
-TIMESTAMP_DUE = int(time.mktime(DATE_DUE.timetuple()))*1000
+TIMESTAMP_DUE = 1497787200000
