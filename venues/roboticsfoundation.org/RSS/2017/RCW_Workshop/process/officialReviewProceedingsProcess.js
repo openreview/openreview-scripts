f18ab6c87c81d94b3d04ad54188818c21307934d
pmandler <mandler@cs.umass.edu>
2017-06-22 10:58:25 -0400

    Fix RSS duedate from July to June (#422)
    * Fix RSS duedate from July to June
    
    * RSS code to go with adding reviewers
    
    * change readers to everyone because still only invitees can see

--- venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/process/officialReviewProceedingsProcess.js
+++ venues/roboticsfoundation.org/RSS/2017/RCW_Workshop/process/officialReviewProceedingsProcess.js
@@ -1,32 +1,25 @@
 function(){
-    const TRACK_NAME = <<param0>>;
-    const CONFERENCE = 'roboticsfoundation.org/RSS/2017/RCW_Workshop';
-    const TRACK = CONFERENCE+'/-_'+TRACK_NAME;
-    const PAPERGRP = TRACK+'/Paper' + note.number;
+    const TRACK_NAME = "Proceedings"
+    const CONFERENCE = 'roboticsfoundation.org/RSS/2017/RCW_Workshop/-_'+TRACK_NAME;
     const CONFERENCE_NAME = 'RSS 2017 Workshop '+TRACK_NAME+' Track';
     var or3client = lib.or3client;
 
-    console.log('PAM - the official Review Process is here Review'+note.number);
-
-    // send email to author of paper submission
     var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
     origNote.then(function(result) {
       var forum = result.notes[0];
-      var note_number = forum.number;
-
-      var reviewers = [PAPERGRP + '/Reviewers'];
+      // send email to author of paper submission
       var authors = forum.content.authorids;
-
       var author_mail = {
         "groups": authors,
-        "subject": "Review of your submission to "+TRACK+": \"" + forum.content.title + "\"",
+        "subject": "Review of your submission to "+CONFERENCE_NAME+": \"" + forum.content.title + "\"",
         "message": "Your submission to " + CONFERENCE_NAME + " has received an official review.\n\nTitle: " + note.content.title + "\n\nReview: " + note.content.review + "\n\nTo view the review, click here: " + baseUrl+"/forum?id=" + note.forum
       };
 
       var authorMailP = or3client.or3request( or3client.mailUrl, author_mail, 'POST', token );
 
       // allow this reviewer to see other reviews
-      var non_reviewer_group = TRACK+'/Paper'+note_number+'/Reviewers/NonReaders';
+      var note_number = forum.number;
+      var non_reviewer_group = CONFERENCE+'/Paper'+note_number+'/Reviewers/NonReaders';
       var reviewReader = or3client.removeGroupMember(non_reviewer_group, note.signatures[0], token);
 
       return Promise.all([
