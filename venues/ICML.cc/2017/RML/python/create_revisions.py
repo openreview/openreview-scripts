7a4aad3bfabf7bcc0caf92807219db150c425ac0
Michael Spector <michael.l.spector@gmail.com>
2017-07-28 15:16:54 -0400

    remove extraneous variables

--- venues/ICML.cc/2017/RML/python/create_revisions.py
+++ venues/ICML.cc/2017/RML/python/create_revisions.py
@@ -16,49 +16,6 @@ print client.baseurl
 submissions = client.get_notes(invitation = config.SUBMISSION)
 
 def revision_invitation(n):
-    revision_content = {
-        'title': {
-            'description': 'Title of paper.',
-            'order': 1,
-            'value-regex': '.{1,250}',
-            'required':True
-        },
-        'authors': {
-            'description': 'Comma separated list of author names.',
-            'order': 2,
-            'values-regex': "[^;,\\n]+(,[^,\\n]+)*",
-            'required':True
-        },
-        'authorids': {
-            'description': 'Comma separated list of author email addresses, lowercased, in the same order as above. For authors with existing OpenReview accounts, please make sure that the provided email address(es) match those listed in the author\'s profile.',
-            'order': 3,
-            'values-regex': "([a-z0-9_\-\.]{2,}@[a-z0-9_\-\.]{2,}\.[a-z]{2,},){0,}([a-z0-9_\-\.]{2,}@[a-z0-9_\-\.]{2,}\.[a-z]{2,})",
-            'required':True
-        },
-        'keywords': {
-            'description': 'Comma separated list of keywords.',
-            'order': 6,
-            'values-regex': "(^$)|[^;,\\n]+(,[^,\\n]+)*"
-        },
-        'TL;DR': {
-            'description': '\"Too Long; Didn\'t Read\": a short sentence describing your paper',
-            'order': 7,
-            'value-regex': '[^\\n]{0,250}',
-            'required':False
-        },
-        'abstract': {
-            'description': 'Abstract of paper.',
-            'order': 8,
-            'value-regex': '[\\S\\s]{1,5000}',
-            'required':True
-        },
-        'pdf': {
-            'description': 'Upload a PDF file that ends with .pdf',
-            'order': 9,
-            'value-regex': 'upload',
-            'required':True
-        }
-    }
 
     revision_reply = {
         'content': config.submission_content,
