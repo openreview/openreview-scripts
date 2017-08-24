605552ca1c3b03314b923a4d36b64c1cee6c1c06
Pam Mandler <mandler@cs.umass.edu>
2017-08-24 14:47:19 -0400

    Add comments, revision invitation, and add reviews to config

--- admin/superuser-init.py
+++ admin/superuser-init.py
@@ -16,11 +16,17 @@ REQUIRED ARGUMENTS
 OPTIONAL ARGUMENTS
 
     overwrite - if present, overwrites the conference directory.
+    data - a .json file containing parameters that are otherwise prompted for in the script
     baseurl -  the URL of the OpenReview server to connect to (live site:
         https://openreview.net)
     username - the email address of the logging in user
     password - the user's password
 
+Gathers information to fill in config.py and the webfield/conf.html.
+Creates directories and groups for the entire CONF directory structure.
+Creates sub-directores python/, process/, data/ and webfield/.
+Creates config.py, admin-init.py, submissionProcess.js, commentProcess.js and conf.html.
+Optionally creates admin user profile
 """
 
 parser = argparse.ArgumentParser()
@@ -34,11 +40,13 @@ parser.add_argument('--password')
 args = parser.parse_args()
 
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+if client.username.lower() != "openreview.net": raise(Exception('This script may only be run by the superuser'))
 
 data = {'conference': args.conf}
 if args.data:
     datafile = open(os.path.join(os.path.dirname(__file__), args.data), 'r')
     data.update(json.loads(datafile.read()))
+    print data
 
 prompts = {
     'conference': "Enter the full path of the conference group you would like to create (e.g. my-conference.org/MYCONF/2017): ",
@@ -54,6 +62,8 @@ prompts = {
     "url": "Enter the URL of the conference: "
 }
 
+# if 'name' is already in data[] return the value,
+#  otherwise use prompts[] to prompt user for the value and store it in data
 def get_input_or_data(name):
     if name not in data.keys():
         data[name] = raw_input(prompts[name])
@@ -63,6 +73,7 @@ def get_input_or_data(name):
 conference = get_input_or_data("conference")
 directory = utils.get_path('../venues/%s' % conference, __file__)
 
+## if set to overwrite, or running for the first time for this conference
 if args.overwrite or not os.path.exists(directory):
     conference_title = get_input_or_data("conference_title")
     conference_subtitle = get_input_or_data("conference_subtitle")
@@ -70,18 +81,21 @@ if args.overwrite or not os.path.exists(directory):
     conference_date = get_input_or_data("conference_date")
     url = get_input_or_data("url")
     conference_phrase = get_input_or_data("conference_phrase")
-    submission_name = get_input_or_data("submission_name")
 
+    submission_name = get_input_or_data("submission_name")
+    # create a default submission name if not provided
     if submission_name.strip() == '':
         submission_name = "Submission"
         data['submission_name'] = submission_name
 
     human_duedate = get_input_or_data("human_duedate")
     # check if due date/time is valid and after current time
-
     duedate_valid = False
-    while not duedate_valid and "duedate_input" not in data.keys():
+    if "duedate_input" not in data.keys():
         duedate_input = raw_input(prompts["duedate_input"])
+    else:
+        duedate_input = data["duedate_input"]
+    while not duedate_valid:
         try:
             day, month, year = duedate_input.split('/')
             day = int(day)
@@ -104,12 +118,13 @@ if args.overwrite or not os.path.exists(directory):
             data["duedate_input"] = duedate_input
         except Exception, e:
             print "Duedate invalid: ", e
+            duedate_input = raw_input(prompts["duedate_input"])
 
     duedate_milliseconds = utils.date_to_timestamp(duedate)
 
 
     print "Creating conference directory at %s" % directory
-
+    ## create directory structure
     makedir = lambda d: os.makedirs(d) if not os.path.exists(d) else None
 
     makedir(directory)
@@ -118,6 +133,7 @@ if args.overwrite or not os.path.exists(directory):
     makedir(directory + '/process')
     makedir(directory + '/data')
 
+    ## add data inputs to config file
     print "writing %s/python/config.py" % directory
     with open(directory + '/python/config.py', 'w') as new_configfile, open(utils.get_path('./conference-template/python/config.template', __file__)) as template_configfile:
         templatestring = template_configfile.read().replace('<<CONF>>', "\"%s\"" % conference)
@@ -125,6 +141,7 @@ if args.overwrite or not os.path.exists(directory):
         templatestring = templatestring.replace('<<TIMESTAMP>>',str(duedate_milliseconds))
         new_configfile.write(templatestring)
 
+    ## add data inputs to webfield file
     print "writing %s/webfield/conf.html" % directory
     with open(directory + '/webfield/conf.html', 'w') as new_webfile, open(utils.get_path('./conference-template/webfield/conf.template',__file__)) as template_webfile:
         templatestring = template_webfile.read().replace('<<TITLE>>',"\"%s\"" % conference_title)
@@ -137,36 +154,43 @@ if args.overwrite or not os.path.exists(directory):
         templatestring = templatestring.replace('<<URL>>',"\"%s\"" % url)
         new_webfile.write(templatestring)
 
+    ## add data inputs to admin-init script
     print "writing %s/python/admin-init.py" % directory
     with open(directory + '/python/admin-init.py', 'w') as new_initfile, open(utils.get_path('./conference-template/python/admin-init.template', __file__)) as template_initfile:
         templatestring = template_initfile.read().replace('<<SUBMISSION_DUEDATE>>', "%s" % duedate_milliseconds)
         new_initfile.write(templatestring)
 
-    print "writing %s/process/submissionProcess.template" % directory
-    with open(directory + '/process/submissionProcess.template', 'w') as new_submissionprocess, open(utils.get_path('./conference-template/process/submissionProcess.template', __file__)) as template_submissionprocess:
+    ## add data inputs to submission process function
+    print "writing %s/process/submissionProcess.js" % directory
+    with open(directory + '/process/submissionProcess.js', 'w') as new_submissionprocess, open(utils.get_path('./conference-template/process/submissionProcess.template', __file__)) as template_submissionprocess:
         templatestring = template_submissionprocess.read().replace('<<CONF>>', "\"%s\"" % conference)
         templatestring = templatestring.replace('<<PHRASE>>', "\"%s\"" % conference_phrase)
         new_submissionprocess.write(templatestring)
 
+    ## add data inputs to comment process function
     print "writing %s/process/commentProcess.js" % directory
     with open(directory + '/process/commentProcess.js', 'w') as new_commentprocess, open(utils.get_path('./conference-template/process/commentProcess.template', __file__)) as template_commentprocess:
         templatestring = template_commentprocess.read().replace('<<PHRASE>>', "\"%s\"" % conference_phrase)
         new_commentprocess.write(templatestring)
+
+    ## add data inputs to submission process function
+    print "writing %s/process/officialReviewProcess.js" % directory
+    with open(directory + '/process/officialReviewProcess.js', 'w') as new_reviewprocess, open(utils.get_path('./conference-template/process/officialReviewProcess.template', __file__)) as template_reviewprocess:
+        templatestring = template_reviewprocess.read().replace('<<CONF>>', "\"%s\"" % conference)
+        templatestring = templatestring.replace('<<PHRASE>>', "\"%s\"" % conference_phrase)
+        new_reviewprocess.write(templatestring)
 
     print "writing %s/params.json" % directory
     with open(directory + '/params.json', 'w') as paramsfile:
         json.dump(data, paramsfile, indent=4)
 
 
-admin = args.conf + '/Admin'
-
-if client.username.lower() != "openreview.net": raise(Exception('This script may only be run by the superuser'))
-
+# create list of subpaths (ex. Test.com, Test.com/TestConference, Test.com/TestConference/2018)
 path_components = args.conf.split('/')
 paths = ['/'.join(path_components[0:index+1]) for index, path in enumerate(path_components)]
 
-# We need to check if ancestors of the conference exist. If they don't, they
-# must be created before continuing.
+# We need to check if ancestor groups of the conference exist.
+# If they don't, they must be created before continuing.
 for p in paths:
     if not client.exists(p) and p != args.conf:
         client.post_group(openreview.Group(
@@ -179,6 +203,7 @@ for p in paths:
         ))
         print "Posting group: ", p
 
+# create conference group
 if not client.exists(args.conf):
     print "Posting group: ", args.conf
     conf_group = client.post_group(openreview.Group(
@@ -191,6 +216,8 @@ else:
     print "Group %s already exists" % args.conf
     conf_group = client.get_group(args.conf)
 
+# create admin group
+admin = args.conf + '/Admin'
 if not client.exists(admin):
     admin_group = client.post_group(openreview.Group(
         admin,
@@ -202,15 +229,11 @@ else:
     print "Group %s already exists" % admin
     admin_group = client.get_group(admin)
 
+# add admin group to the conference members
 client.add_members_to_group(conf_group, [admin])
 
-utils.process_to_file(
-    os.path.join(os.path.dirname(__file__), "../venues/%s/process/submissionProcess.template" % args.conf),
-    os.path.join(os.path.dirname(__file__), "../venues/%s/process" % args.conf)
-    )
-
+# create admin user
 create_admin = raw_input("Create administrator login? (y/[n]): ").lower()
-
 if create_admin == 'y' or create_admin == 'yes':
     default_username = conference.split('/')[-1].lower()+'_admin'
     username = raw_input("Please provide administrator login, in lowercase, with no spaces (default: {0}): ".format(default_username))
