55d61ddea810dc7b4badaa23082d37fd33fe532d
Michael Spector <michaelspector@gmail.com>
2017-10-17 22:06:17 -0400

    add args

--- superuser/scripts/create-conference-group.py
+++ superuser/scripts/create-conference-group.py
@@ -1,3 +1,6 @@
+import argparse
+import openreview
+
 def build_groups(conference_group_id):
     '''
     Generates the conference group and its ancestors.
@@ -24,10 +27,22 @@ def build_groups(conference_group_id):
 
     return groups
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--venue', required=True, help = "the full path of the conference group to create.")
+parser.add_argument('--overwrite', action='store_true', help="if true, overwrites the conference directory.")
+parser.add_argument('--baseurl')
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+conference_group_id = args.venue
+
 groups = build_groups(conference_group_id)
 
 for g in sorted([g for g in groups]):
     print "posting group {0}".format(g)
     client.post_group(groups[g])
+
 # add admin group to the conference members
 client.add_members_to_group(groups[conference_group_id], conference_group_id + '/Admin')
