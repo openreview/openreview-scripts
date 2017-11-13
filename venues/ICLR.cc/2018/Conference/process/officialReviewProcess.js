7c18adc8db4c4339c9340c48f8554ad85dcbcad3
Michael Spector <michaelspector@gmail.com>
2017-11-13 10:41:44 -0500

    remove author email from official review process function

--- venues/ICLR.cc/2018/Conference/process/officialReviewProcess.js
+++ venues/ICLR.cc/2018/Conference/process/officialReviewProcess.js
@@ -16,24 +16,14 @@ function(){
       var areachairs = ['ICLR.cc/2018/Conference/Paper' + note_number + '/Area_Chair'];
       var authors = forum.content.authorids;
 
-      var author_mail = {
-        "groups": authors,
-        "subject": "Review of your submission to ICLR 2018: \"" + forum.content.title + "\"",
-        "message": "Your submission to " + conference + " has received an official review.\n\nTitle: " + note.content.title + "\n\nReview: " + note.content.review + "\n\nTo view the review, click here: " + baseUrl+"/forum?id=" + note.forum
-      };
-
       var areachair_mail = {
         "groups": areachairs,
         "subject": "Review posted to your assigned paper: \"" + forum.content.title + "\"",
         "message": "A submission to " + conference + ", for which you are an official area chair, has received an official review. \n\nTitle: " + note.content.title + "\n\nComment: " + note.content.review + "\n\nTo view the review, click here: " + baseUrl + "/forum?id=" + note.forum
       };
-      var authorMailP = or3client.or3request( or3client.mailUrl, author_mail, 'POST', token );
       var areachairMailP = or3client.or3request( or3client.mailUrl, areachair_mail, 'POST', token );
 
-      return Promise.all([
-        authorMailP,
-        areachairMailP
-      ])
+      return areachairMailP;
     })
     .then(result => or3client.addInvitationNoninvitee(note.invitation, note.signatures[0], token))
     .then(result => done())
