45c332aac9b01cd7bdc20c5aa652285361763a4b
Michael Spector <michaelspector@gmail.com>
2017-12-01 17:16:19 -0500

    change /Reviewers to /Authors

--- venues/ICLR.cc/2018/Conference/process/reviewRevisionProcess.js
+++ venues/ICLR.cc/2018/Conference/process/reviewRevisionProcess.js
@@ -8,7 +8,7 @@ function() {
     console.log('number: '+ number);
 
     var reviewerMail = {
-      "groups": ['ICLR.cc/2018/Conference/Paper' + number + '/Reviewers'],
+      "groups": ['ICLR.cc/2018/Conference/Paper' + number + '/Authors'],
       "subject": "Revision posted to a review of your paper",
       "message": "A review of your paper has been revised. \n\nTo view the review, click here: " + baseUrl + "/forum?id=" + note.forum + "&noteId=" + note.id + "\n\nYou can see the changes made at " + baseUrl + "/revisions?id=" + note.forum
     };
