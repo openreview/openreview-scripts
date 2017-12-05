cb23a8eee601b3cc6e397544361d3d4701f9fa9e
Melisa Bok <melisabok@gmail.com>
2017-12-05 16:16:41 -0500

    use program chairs group

--- venues/NIPS.cc/2017/Workshop/MLITS/python/create-decision-tags.py
+++ venues/NIPS.cc/2017/Workshop/MLITS/python/create-decision-tags.py
@@ -46,45 +46,45 @@ invitation = Invitation(id = 'NIPS.cc/2017/Workshop/MLITS/-/Decision', readers =
 client.post_invitation(invitation)
 
 
-tag = Tag(forum = 'H1gwPOLAW', replyto = 'H1gwPOLAW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'H1gwPOLAW', replyto = 'H1gwPOLAW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
-tag = Tag(forum = 'BJ4tluLAZ', replyto = 'BJ4tluLAZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Reject')
+tag = Tag(forum = 'BJ4tluLAZ', replyto = 'BJ4tluLAZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Reject')
 client.post_tag(tag)
 
-tag = Tag(forum = 'rys_hvLRW', replyto = 'rys_hvLRW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Oral')
+tag = Tag(forum = 'rys_hvLRW', replyto = 'rys_hvLRW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Oral')
 client.post_tag(tag)
 
-tag = Tag(forum = 'HyHG8LURb', replyto = 'HyHG8LURb', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Reject')
+tag = Tag(forum = 'HyHG8LURb', replyto = 'HyHG8LURb', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Reject')
 client.post_tag(tag)
 
-tag = Tag(forum = 'HkW01LLA-', replyto = 'HkW01LLA-', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Reject')
+tag = Tag(forum = 'HkW01LLA-', replyto = 'HkW01LLA-', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Reject')
 client.post_tag(tag)
 
-tag = Tag(forum = 'Hycq9SLCW', replyto = 'Hycq9SLCW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Oral')
+tag = Tag(forum = 'Hycq9SLCW', replyto = 'Hycq9SLCW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Oral')
 client.post_tag(tag)
 
-tag = Tag(forum = 'rJ26HSLRb', replyto = 'rJ26HSLRb', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Oral')
+tag = Tag(forum = 'rJ26HSLRb', replyto = 'rJ26HSLRb', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Oral')
 client.post_tag(tag)
 
-tag = Tag(forum = 'Bk4BBBLRZ', replyto = 'Bk4BBBLRZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'Bk4BBBLRZ', replyto = 'Bk4BBBLRZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
-tag = Tag(forum = 'BJ3-mrURW', replyto = 'BJ3-mrURW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'BJ3-mrURW', replyto = 'BJ3-mrURW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
-tag = Tag(forum = 'BkQszBIAW', replyto = 'BkQszBIAW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Oral')
+tag = Tag(forum = 'BkQszBIAW', replyto = 'BkQszBIAW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Oral')
 client.post_tag(tag)
 
-tag = Tag(forum = 'HylddmUAZ', replyto = 'HylddmUAZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'HylddmUAZ', replyto = 'HylddmUAZ', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
-tag = Tag(forum = 'BypTNXUCW', replyto = 'BypTNXUCW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Reject')
+tag = Tag(forum = 'BypTNXUCW', replyto = 'BypTNXUCW', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Reject')
 client.post_tag(tag)
 
-tag = Tag(forum = 'Hy4q48h3Z', replyto = 'Hy4q48h3Z', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'Hy4q48h3Z', replyto = 'Hy4q48h3Z', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
-tag = Tag(forum = 'SyiF5-23Z', replyto = 'SyiF5-23Z', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['~Li_Erran_Li1'], tag = 'Poster')
+tag = Tag(forum = 'SyiF5-23Z', replyto = 'SyiF5-23Z', invitation ='NIPS.cc/2017/Workshop/MLITS/-/Decision', readers = ['everyone'], signatures = ['NIPS.cc/2017/Workshop/MLITS/Program_Chairs'], tag = 'Poster')
 client.post_tag(tag)
 
