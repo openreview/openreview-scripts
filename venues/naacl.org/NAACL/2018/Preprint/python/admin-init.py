89758b48fd6a2647db969f5b16e439b59e181508
Michael Spector <michaelspector@gmail.com>
2017-12-11 17:25:20 -0500

    naacl preprint infrastructure

---
+++ venues/naacl.org/NAACL/2018/Preprint/python/admin-init.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys, os
+import argparse
+import openreview
+import config
+
+"""
+
+OPTIONAL SCRIPT ARGUMENTS
+
+    baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+    username - the email address of the logging in user
+    password - the user's password
+
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+groups = {}
+
+groups[config.AUTHORS] = openreview.Group(config.AUTHORS, **config.group_params)
+
+groups[config.CONF] = client.get_group(config.CONF)
+groups[config.CONF].signatures = [client.signature]
+groups[config.CONF].add_webfield(config.WEBPATH)
+
+invitations = {}
+invitations[config.SUBMISSION] = openreview.Invitation(
+    config.SUBMISSION, duedate=config.DUE_TIMESTAMP, **config.submission_params)
+
+invitations[config.BLIND_SUBMISSION] = openreview.Invitation(
+    config.BLIND_SUBMISSION, **config.blind_submission_params)
+
+# public comments are disabled for now
+
+for g in groups.values():
+    print "Posting group: ", g.id
+    client.post_group(g)
+
+for i in invitations.values():
+    print "Posting invitation: ", i.id
+    client.post_invitation(i)
