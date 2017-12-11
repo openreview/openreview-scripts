79767ad6175ce7fc560661d5277361c3e2226873
mspector <michael.l.spector@gmail.com>
2017-12-11 15:51:38 -0500

    fix process function so that the correct url gets sent in the notific… (#551)
    * fix process function so that the correct url gets sent in the notification email
    
    * remove unnecessary call

--- venues/ICLR.cc/2018/Conference/process/addRevisionProcess.js
+++ venues/ICLR.cc/2018/Conference/process/addRevisionProcess.js
@@ -5,15 +5,17 @@ function() {
 
     var CONF = 'ICLR.cc/2018/Conference';
 
-    console.log('number: '+ number);
-
-    var reviewerMail = {
-      "groups": ['ICLR.cc/2018/Conference/Paper' + number + '/Reviewers'],
-      "subject": "Revision posted to a paper that you reviewed",
-      "message": "A paper that you reviewed has been revised. \n\nTo view the paper, click here: " + baseUrl + "/forum?id=" + note.forum + "\n\nYou can see the changes made at " + baseUrl + "/revisions?id=" + note.forum
-    };
-
-    or3client.or3request(or3client.mailUrl, reviewerMail, 'POST', token)
+    or3client.or3request(or3client.notesUrl + '?forum=' + note.forum, {}, 'GET', token)
+    .then(result => result.notes.filter(n => n.forum === n.id)[0])
+    .then(originalNote => originalNote.overwriting[0])
+    .then(overwritingId =>
+      var reviewerMail = {
+        "groups": ['ICLR.cc/2018/Conference/Paper' + number + '/Reviewers'],
+        "subject": "Revision posted to a paper that you reviewed",
+        "message": "A paper that you reviewed has been revised. \n\nTo view the paper, click here: " + baseUrl + "/forum?id=" + overwritingId + "\n\nYou can see the changes made at " + baseUrl + "/revisions?id=" + overwritingId
+      };
+      return or3client.or3request(or3client.mailUrl, reviewerMail, 'POST', token)
+    })
     .then(result => done())
     .catch(error => done(error));
     return true;
