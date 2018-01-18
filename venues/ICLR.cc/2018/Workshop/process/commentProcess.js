128735a46ebb973b56f0b35f0804a4b683ef6bda
Michael Spector <michaelspector@gmail.com>
2018-01-18 13:11:55 -0800

    add submission and (some) commenting infrastructure

---
+++ venues/ICLR.cc/2018/Workshop/process/commentProcess.js
@@ -0,0 +1,79 @@
+function(){
+    var or3client = lib.or3client;
+
+    var CONFERENCEPHRASE = "ICLR 2018"
+
+    var origNoteP = or3client.or3request(or3client.notesUrl + '?id=' + note.forum, {}, 'GET', token);
+    var replytoNoteP = note.replyto ? or3client.or3request(or3client.notesUrl + '?id=' + note.replyto, {}, 'GET', token) : null;
+
+    Promise.all([
+      origNoteP,
+      replytoNoteP
+    ]).then(function(result) {
+
+      var origNote = result[0].notes[0];
+      var replytoNote = note.replyto ? result[1].notes[0] : null;
+      var replytoNoteSignatures = replytoNote ? replytoNote.signatures : [];
+      var author_mail;
+
+      var selfComment = replytoNoteSignatures.indexOf(note.signatures[0]) > -1;
+      if(selfComment){
+        console.log('self comment detected');
+      }
+
+      var readableComment = true;
+
+      //make sure that all readers in note.readers is also in replytoNotes.readers
+
+      for(var i=0; i<note.readers.length; i++){
+        if(!replytoNote.readers.includes(note.readers[i])) {
+          readableComment = false;
+        }
+      };
+
+      var reviewer_mail = {
+        'groups': ['ICLR.cc/2018/Workshop/Paper' + origNote.number + '/Reviewers'],
+        'subject': 'Comment posted to a paper you are reviewing. Title: ' + origNote.content.title,
+        'message': 'A comment was posted to a paper for which you are serving as reviewer.\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      };
+
+      var pc_mail = {
+        'groups': ['ICLR.cc/2018/Conference/Program_Chairs'],
+        'subject': 'A Program Chair-only comment was posted',
+        'message': 'A comment was posted to a paper with readership restricted to only the Program Chairs.\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      };
+
+      var promises = [];
+
+      if(note.readers.indexOf('ICLR.cc/2018/Conference/Reviewers_and_Higher') > -1 ||
+        note.readers.indexOf('ICLR.cc/2018/Conference/Authors_and_Higher') > -1 ||
+        note.readers.indexOf('everyone') > -1){
+        promises.push(or3client.or3request(or3client.mailUrl, reviewer_mail, 'POST', token));
+      } else if(note.readers.indexOf('ICLR.cc/2018/Conference/Program_Chairs') > -1){
+        promises.push(or3client.or3request(or3client.mailUrl, pc_mail, 'POST', token));
+      }
+
+      if(!selfComment && readableComment){
+        if(replytoNote.id == origNote.id){
+          author_mail = {
+            "groups": origNote.content.authorids,
+            "subject": "Your submission to " + CONFERENCEPHRASE + " has received a comment",
+            "message": "Your submission to " + CONFERENCEPHRASE + " has received a comment.\n\nComment title: " + note.content.title + "\n\nComment: " + note.content.comment + "\n\nTo view the comment, click here: " + baseUrl + "/forum?id=" + note.forum + '&noteId=' + note.id
+          };
+        } else {
+          author_mail = {
+            "groups": replytoNote.signatures == '(anonymous)' ? [] : replytoNote.signatures,
+            "subject": "Your comment has received a response",
+            "message": "Your comment titled \"" + replytoNote.content.title + "\" has received a response.\n\nComment title: " + note.content.title + "\n\nComment: " + note.content.comment + "\n\nTo view the comment, click here: " + baseUrl + "/forum?id=" + note.forum + '&noteId=' + note.id
+          };
+        }
+        promises.push(or3client.or3request(or3client.mailUrl, author_mail, 'POST', token));
+      }
+
+      return Promise.all(promises);
+    })
+    .then(result => done())
+    .catch(error => done(error));
+
+    return true;
+  };
---
+++ venues/ICLR.cc/2018/Workshop/python/superuser-init.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+
+import sys, os
+import argparse
+import openreview
+import config
+
+"""
+
+OPTIONAL SCRIPT ARGUMENTS
+
+	baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+ 	username - the email address of the logging in user
+	password - the user's password
+
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+conference = client.post_group(
+	openreview.Group(
+		config.CONF,
+		web = config.WEBPATH,
+		signatures = [client.signature],
+		readers = ['everyone'],
+		writers = [config.CONF],
+		signatories = [config.CONF]
+	))
+
+program_chairs = client.post_group(
+	openreview.Group(**config.program_chairs_params))
+
+authors = client.post_group(
+	openreview.Group(config.AUTHORS, **config.group_params))
+
+reviewers = client.post_group(
+	openreview.Group(config.REVIEWERS, **config.reviewer_group_params))
+
+reviewers_invited = client.post_group(
+	openreview.Group(config.REVIEWERS_INVITED, **config.reviewer_group_params))
+
+reviewers_declined = client.post_group(
+	openreview.Group(config.REVIEWERS_DECLINED, **config.reviewer_group_params))
+
+reviewers_emailed = client.post_group(
+	openreview.Group(config.REVIEWERS_EMAILED, **config.reviewer_group_params))
+
+authors_plus = client.post_group(
+	openreview.Group(
+		config.AUTHORS_PLUS,
+		members = [config.AUTHORS, config.REVIEWERS_PLUS],
+		**config.public_group_params))
+
+reviewers_plus = client.post_group(
+	openreview.Group(
+		config.REVIEWERS_PLUS,
+		members = [config.CONF, config.REVIEWERS, config.PROGRAM_CHAIRS],
+		**config.public_group_params))
+
+submission_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.SUBMISSION,
+		duedate=config.DUE_TIMESTAMP,
+		**config.submission_params))
+
+withdrawn_submission = client.post_invitation(
+	openreview.Invitation(
+		config.WITHDRAWN_SUBMISSION,
+		**config.withdrawn_submission_params))
+
+public_comment_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.PUBLIC_COMMENT,
+		**config.public_comment_params))
+
+add_bid_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.ADD_BID,
+		**config.add_bid_params))
+
+metadata_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.METADATA,
+		**config.metadata_params))
+
+assignments_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.ASSIGNMENTS,
+		**config.assignments_params))
+
+recruit_reviewers_invitation = client.post_invitation(
+	openreview.Invitation(
+		config.RECRUIT_REVIEWERS,
+		**config.recruit_reviewers_params))
