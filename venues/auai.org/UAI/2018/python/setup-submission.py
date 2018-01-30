7beb0bd890c5c91a969aad43d56240ddf74de995
Michael Spector <michaelspector@gmail.com>
2018-01-30 12:50:56 -0500

    some modifications in progress

--- venues/auai.org/UAI/2018/python/setup-submission.py
+++ venues/auai.org/UAI/2018/python/setup-submission.py
@@ -1,11 +1,102 @@
 import openreview
 import argparse
-from openreview import conference
+from openreview import invitations
 from openreview import process
 from openreview import webfield
 
 client = openreview.Client()
 print 'connecting to {0}'.format(client.baseurl)
+
+subject_areas = [
+    "Algorithms: Approximate Inference",
+    "Algorithms: Belief Propagation",
+    "Algorithms: Distributed and Parallel",
+    "Algorithms: Exact Inference",
+    "Algorithms: Graph Theory",
+    "Algorithms: Heuristics",
+    "Algorithms: Lifted Inference",
+    "Algorithms: MCMC methods",
+    "Algorithms: Optimization",
+    "Algorithms: Other",
+    "Algorithms: Software and Tools",
+    "Applications: Biology",
+    "Applications: Databases",
+    "Applications: Decision Support",
+    "Applications: Diagnosis and Reliability",
+    "Applications: Economics",
+    "Applications: Education",
+    "Applications: General",
+    "Applications: Medicine",
+    "Applications: Planning and Control",
+    "Applications: Privacy and Security",
+    "Applications: Robotics",
+    "Applications: Sensor Data",
+    "Applications: Social Network Analysis",
+    "Applications: Speech",
+    "Applications: Sustainability and Climate",
+    "Applications: Text and Web Data",
+    "Applications: User Models",
+    "Applications: Vision",
+    "Data: Big Data",
+    "Data: Multivariate",
+    "Data: Other",
+    "Data: Relational",
+    "Data: Spatial",
+    "Data: Temporal or Sequential",
+    "Learning: Active Learning",
+    "Learning: Classification",
+    "Learning: Clustering",
+    "Learning: Deep Learning",
+    "Learning: General",
+    "Learning: Nonparametric Bayes",
+    "Learning: Online and Anytime Learning",
+    "Learning: Other",
+    "Learning: Parameter Estimation",
+    "Learning: Probabilistic Generative Models",
+    "Learning: Ranking",
+    "Learning: Recommender Systems",
+    "Learning: Regression",
+    "Learning: Reinforcement Learning",
+    "Learning: Relational Learning",
+    "Learning: Relational Models",
+    "Learning: Scalability",
+    "Learning: Semi-Supervised Learning",
+    "Learning: Structure Learning",
+    "Learning: Structured Prediction",
+    "Learning: Theory",
+    "Learning: Unsupervised",
+    "Methodology: Bayesian Methods",
+    "Methodology: Calibration",
+    "Methodology: Elicitation",
+    "Methodology: Evaluation",
+    "Methodology: Human Expertise and Judgement",
+    "Methodology: Other",
+    "Methodology: Probabilistic Programming",
+    "Models: Bayesian Networks",
+    "Models: Directed Graphical Models",
+    "Models: Dynamic Bayesian Networks",
+    "Models: Markov Decision Processes",
+    "Models: Mixed Graphical Models",
+    "Models: Other",
+    "Models: Relational Models",
+    "Models: Topic Models",
+    "Models: Undirected Graphical Models",
+    "None of the above",
+    "Principles: Causality",
+    "Principles: Cognitive Models",
+    "Principles: Decision Theory",
+    "Principles: Game Theory",
+    "Principles: Information Theory",
+    "Principles: Other",
+    "Principles: Probability Theory",
+    "Principles: Statistical Theory",
+    "Representation: Constraints",
+    "Representation: Dempster-Shafer",
+    "Representation: Fuzzy Logic",
+    "Representation: Influence Diagrams",
+    "Representation: Non-Probabilistic Frameworks",
+    "Representation: Probabilistic"
+]
 
 '''
 Set the variable names that will be used in various pieces of executable javascript.
@@ -38,7 +129,7 @@ this copy is defined by the blind submission invitation.
 
 '''
 
-submission_inv = conference.Submission(
+submission_inv = invitations.Submission(
     name = 'Submission',
     conference_id = 'auai.org/UAI/2018',
     duedate = 1527757200000, # 17:00:00 EST on May 1, 2018
@@ -47,10 +138,13 @@ submission_inv = conference.Submission(
                 'auai.org/UAI/2018', '{content.authorids}', '{signatures}']},
         'signatures': {'values-regex': '~.*|auai.org/UAI/2018'},
         'writers': {'values': ['auai.org/UAI/2018']}
+    },
+    content_params = {
+        'subject areas': {'required': True, 'values-dropdown': subject_areas}
     }
 )
 
-blind_inv = conference.Submission(
+blind_inv = invitations.Submission(
     name = 'Blind_Submission',
     conference_id = 'auai.org/UAI/2018',
     duedate = 1527757200000, # 17:00:00 EST on May 1, 2018
@@ -59,19 +153,47 @@ blind_inv = conference.Submission(
         'authorids': {'values-regex': '.*'}
     },
     reply_params = {
-        'signatures': {'values': ['auai.org/UAI/2018']}
+        'signatures': {'values': ['auai.org/UAI/2018']},
+        'readers': {'values-regex':'|'.join([
+            'auai.org/UAI/2018',
+            'auai.org/UAI/2018/Program_Committee',
+            'auai.org/UAI/2018/Senior_Program_Committee',
+            'auai.org/UAI/2018/Program_Chairs',
+            'auai.org/UAI/2018/Paper.*/Authors'
+            ])}
     }
-
 )
 
 submission_process = process.MaskSubmissionProcess(js_constants, mask = blind_inv)
-
 submission_inv.add_process(submission_process)
 
+# post both the submissions
 submission_inv = client.post_invitation(submission_inv)
 blind_inv = client.post_invitation(blind_inv)
+print "posted invitation", submission_inv.id
+print "posted invitation", blind_inv.id
 
+'''
+Create a bid invitation.
+'''
 
+bid_invitation = invitations.AddBid(
+    name = 'Add_Bid',
+    conference_id = 'auai.org/UAI/2018',
+    duedate = 1527757200000, # 17:00:00 EST on May 1, 2018
+    completion_count = 50,
+    inv_params = {
+        'readers': ['auai.org/UAI/2018','auai.org/UAI/2018/Program_Committee'],
+         'invitees': ['auai.org/UAI/2018/Program_Committee']}
+    )
+
+bid_webfield = webfield.BidWebfield(
+    js_constants, group_id = 'auai.org/UAI/2018', subject_areas = subject_areas)
+
+bid_invitation.web = bid_webfield.render()
+
+bid_invitation = client.post_invitation(bid_invitation)
+print "posted invitation", bid_invitation.id
 '''
 Create the homepage and add it to the conference group.
 
@@ -95,6 +217,12 @@ instructions = ' '.join([
     'and click on the "Add Revision" button.',
     'Revisions on originals propagate all changes to anonymous copies,',
     'while maintaining anonymity.</p>',
+    ''
+    '<p><strong>Bidding on Papers (for reviewers)</strong><br>',
+    'If you are serving as a member of the Program Committee (as a reviewer), you can',
+    'bid on papers in the list below. You can also use the ',
+    '<a href="invitation?id=auai.org/UAI/2018/-/Add_Bid">Bidding Console</a> for better',
+    'navigational features. </p>',
     '',
     '<p><strong>Questions or Concerns:</strong><br>',
     'Please contact the OpenReview support team at',
@@ -106,9 +234,11 @@ js_constants['INSTRUCTIONS'] = instructions
 
 homepage = webfield.TabbedHomepage(
     group_id = 'auai.org/UAI/2018',
-    js_constants = js_constants
+    js_constants = js_constants,
+    subject_areas = subject_areas
 )
 
 uai2018_conference = client.get_group('auai.org/UAI/2018')
 uai2018_conference.web = homepage.render()
 uai2018_conference = client.post_group(uai2018_conference)
+print "adding webfield to", uai2018_conference.id
