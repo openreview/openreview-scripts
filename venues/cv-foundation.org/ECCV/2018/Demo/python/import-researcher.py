5f2a8c7a2571ab74a06359499ac26d45b0901d81
Michael Spector <michaelspector@gmail.com>
2018-02-01 11:56:45 -0500

    add argument handling

--- venues/cv-foundation.org/ECCV/2018/Demo/python/import-researcher.py
+++ venues/cv-foundation.org/ECCV/2018/Demo/python/import-researcher.py
@@ -5,17 +5,26 @@ import json
 import re
 import csv
 from import_user import *
-client = openreview.Client()
-print client.baseurl
+
+## Handle the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('directory')
+parser.add_argument('--baseurl', help="openreview base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+## Initialize the client library with username and password
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print "connecting to", client.baseurl
 
 def Files(directory):
     for filename in os.listdir(directory):
         if filename.endswith('.json'):
             yield os.path.join(directory, filename)
 
-directory = '../data/researcher/processed'
-
-files = Files(directory)
+files = Files(args.directory)
 
 for filename in files:
     resolved_filename = filename.replace('/processed/','/resolved/')
