90acfe180f098de036d18e9da0fecbeff8f5845d
Michael Spector <michaelspector@gmail.com>
2018-02-14 12:20:07 -0500

    move directory creation outside the loop

--- venues/ICLR.cc/2018/Workshop/python/programchairs/get-pdfs.py
+++ venues/ICLR.cc/2018/Workshop/python/programchairs/get-pdfs.py
@@ -3,13 +3,14 @@ import os
 
 submissions_response = requests.get('https://openreview.net/notes?invitation=ICLR.cc/2018/Workshop/-/Submission', params={})
 
+if not os.path.exists('./pdfs'):
+    os.makedirs('./pdfs')
+
 for n in submissions_response.json()['notes']:
     pdf_url = 'https://openreview.net{0}'.format(n['content']['pdf'])
     paper_id = n['number']
     print "retrieving Paper{0} at {1}".format(paper_id, pdf_url)
     pdf_response = requests.get(pdf_url, stream=True)
-    if not os.path.exists('./pdfs'):
-    	os.makedirs('./pdfs')
 
     with open('./pdfs/Paper{0}.pdf'.format(paper_id), 'wb') as f:
         f.write(pdf_response.content)
