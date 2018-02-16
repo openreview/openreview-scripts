64765abf1b5778d30c88505111117cbd3ce90310
Michael Spector <michaelspector@gmail.com>
2018-02-16 18:23:51 -0500

    second refactor

--- venues/auai.org/UAI/2018/python/match.py
+++ venues/auai.org/UAI/2018/python/match.py
@@ -3,7 +3,7 @@
 """
 Initializes the structures used for paper/user metadata
 """
-
+from __future__ import print_function
 import argparse
 import openreview
 import openreview_matcher
@@ -20,12 +20,6 @@ client = openreview.Client(username=args.username, password=args.password, baseu
 # get the already-posted configuration note
 
 reviewer_configuration = {
-    'label': 'reviewers',
-    'group': 'auai.org/UAI/2018/Program_Committee',
-    'submission': 'auai.org/UAI/2018/-/Blind_Submission',
-    'exclude': [],
-    'metadata': 'auai.org/UAI/2018/-/Paper_Metadata',
-    'assignment': 'auai.org/UAI/2018/-/Paper_Assignment',
     'minusers': 3,
     'maxusers': 3,
     'minpapers': 0,
@@ -35,17 +29,36 @@ reviewer_configuration = {
     }
 }
 
-matcher = openreview_matcher.Matcher(client=client)
+papers = client.get_notes(invitation = 'auai.org/UAI/2018/-/Blind_Submission')
+paper_metadata = client.get_notes(invitation = 'auai.org/UAI/2018/-/Paper_Metadata')
+match_group = client.get_group('auai.org/UAI/2018/Program_Committee')
 
-# solving the matcher returns a configuration note object with the content.assignments field filled in.
-assignment_notes = matcher.solve(reviewer_configuration)
+new_assignments_by_forum = openreview_matcher.match(reviewer_configuration,
+    papers = papers, metadata = paper_metadata, group = match_group)
 
+def create_assignment_note(forum, label):
+    return openreview.Note(**{
+        'forum': forum,
+        'invitation': 'auai.org/UAI/2018/-/Paper_Assignment',
+        'readers': [
+            'auai.org/UAI/2018',
+            'auai.org/UAI/2018/Program_Chairs',
+            'auai.org/UAI/2018/Senior_Program_Committee'
+        ],
+        'writers': ['auai.org/UAI/2018'],
+        'signatures': ['auai.org/UAI/2018'],
+        'content': {
+            'label': label
+        }
+    })
 
-existing_assignment_notes_by_forum = {n.forum: n for n in client.get_notes(invitation='auai.org/UAI/2018/-/Paper_Assignment')}
-# Post a note with the configuration and assignments for later use
-for assignment in assignment_notes:
-	if assignment.forum in existing_assignment_notes_by_forum:
-		assignment.content = existing_assignment_notes_by_forum[assignment.forum].content
-	client.post_note(assignment)
-	print "posted assignment note {0}".format(assignment.forum)
+existing_assignments = client.get_notes(invitation='auai.org/UAI/2018/-/Paper_Assignment')
+existing_reviewer_assignments = {n.forum: n for n in existing_assignments if n.content['label'] == 'reviewers'}
+
+for forum, assignment in new_assignments_by_forum.iteritems():
+    assignment_note = existing_reviewer_assignments.get(forum, create_assignment_note(forum, 'reviewers'))
+    assignment_note.content['assignment'] = assignment
+    assignment_note = client.post_note(assignment_note)
+    print('Paper{0: <6}'.format(assignment_note.number), ', '.join(assignment))
+
 
