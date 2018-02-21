7748809099ae1d8b7c7abb9790ee7bf02b93cf8b
mspector <michael.l.spector@gmail.com>
2018-02-21 13:50:40 -0500

    factor out conflict code into openreview.tools (#614)

--- venues/ICLR.cc/2018/Workshop/python/programchairs/assign-reviewer.py
+++ venues/ICLR.cc/2018/Workshop/python/programchairs/assign-reviewer.py
@@ -42,61 +42,6 @@ Paper123/Reviewers = {
 
 '''
 
-def get_domains(email):
-    full_domain = email.split('@')[1]
-    domain_components = full_domain.split('.')
-    domains = ['.'.join(domain_components[index:len(domain_components)]) for index, path in enumerate(domain_components)]
-    valid_domains = [d for d in domains if '.' in d]
-    return valid_domains
-
-def get_user_conflicts(client, profile_or_email):
-    domain_conflicts = set()
-    relation_conflicts = set()
-    relation_conflicts.update([profile_or_email])
-    try:
-        profile = client.get_profile(profile_or_email)
-
-        profile_domains = []
-        for e in profile.content['emails']:
-            profile_domains += get_domains(e)
-
-        domain_conflicts.update(profile_domains)
-
-        institution_domains = [h['institution']['domain'] for h in profile.content['history']]
-        domain_conflicts.update(institution_domains)
-
-        if 'relations' in profile.content:
-            relation_conflicts.update([r['email'] for r in profile.content['relations']])
-
-        if 'gmail.com' in domain_conflicts:
-            domain_conflicts.remove('gmail.com')
-
-        return (domain_conflicts, relation_conflicts)
-
-    except openreview.OpenReviewException as e:
-        return (set(), set())
-
-def get_paper_conflicts(client, n):
-    authorids = n.content['authorids']
-    domain_conflicts = set()
-    relation_conflicts = set()
-    for e in authorids:
-        author_domain_conflicts, author_relation_conflicts = get_user_conflicts(client, e)
-        if author_domain_conflicts:
-            domain_conflicts.update(author_domain_conflicts)
-        if author_relation_conflicts:
-            relation_conflicts.update(author_relation_conflicts)
-        if '@' in e:
-            domain_conflicts.update(get_domains(e))
-
-    relation_conflicts = set([e for e in authorids if '@' in e])
-
-    # remove the domain "gmail.com"
-    if 'gmail.com' in domain_conflicts:
-        domain_conflicts.remove('gmail.com')
-
-    return (domain_conflicts, relation_conflicts)
-
 def next_anonreviewer_id(empty_anonreviewer_groups, full_anonreviewer_groups, paper_number):
     if len(empty_anonreviewer_groups) > 0:
         anonreviewer_group = empty_anonreviewer_groups[0]
@@ -130,6 +75,7 @@ def assign(client, paper_number, reviewer_to_add=None, reviewer_to_remove=None):
             }))
         else:
             raise e
+
     anonreviewer_groups = client.get_groups(id = 'ICLR.cc/2018/Workshop/Paper{0}/AnonReviewer.*'.format(paper_number))
     empty_anonreviewer_groups = sorted([ a for a in anonreviewer_groups if a.members == [] ], key=lambda x: x.id)
 
@@ -145,8 +91,9 @@ def assign(client, paper_number, reviewer_to_add=None, reviewer_to_remove=None):
 
     if reviewer_to_add:
         paper = client.get_notes(invitation='ICLR.cc/2018/Workshop/-/Submission', number=paper_number)[0]
-        user_domain_conflicts, user_relation_conflicts = get_user_conflicts(client, reviewer_to_add)
-        paper_domain_conflicts, paper_relation_conflicts = get_paper_conflicts(client, paper)
+
+        user_domain_conflicts, user_relation_conflicts = openreview.tools.get_profile_conflicts(client, reviewer_to_add)
+        paper_domain_conflicts, paper_relation_conflicts = openreview.tools.get_paper_conflicts(client, paper)
 
         domain_conflicts = paper_domain_conflicts.intersection(user_domain_conflicts)
         relation_conflicts = paper_relation_conflicts.intersection(user_relation_conflicts)
