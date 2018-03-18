61c5f4371286175695747cd4b96538c7c8bdab89
Melisa Bok <melisabok@gmail.com>
2018-03-18 09:00:44 -0400

    create area chair group

--- venues/cv-foundation.org/ECCV/2018/Conference/python/setup-conference.py
+++ venues/cv-foundation.org/ECCV/2018/Conference/python/setup-conference.py
@@ -22,6 +22,15 @@ reviewers_group = client.post_group(openreview.Group(**{
     'members': []
 }))
 
+areachairs_group = client.post_group(openreview.Group(**{
+    'id': 'cv-foundation.org/ECCV/2018/Conference/Area_Chairs',
+    'readers': ['everyone'],
+    'writers': [],
+    'signatures': ['~Super_User1'],
+    'signatories': [],
+    'members': []
+}))
+
 submission_invitation = client.post_invitation(openreview.Invitation(id='cv-foundation.org/ECCV/2018/Conference/-/Submission',
     **{
     'duedate': 2515811930000,
