35996cc99eb29fc3ab12a1a7c921045527f45dd0
Melisa Bok <melisabok@gmail.com>
2018-03-19 12:45:58 -0400

    update ac profiles with instituion domains

--- venues/cv-foundation.org/ECCV/2018/Conference/python/upload-areachairs.py
+++ venues/cv-foundation.org/ECCV/2018/Conference/python/upload-areachairs.py
@@ -11,6 +11,12 @@ args = parser.parse_args()
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
 
+def search_institution(history, domain):
+    for h in history:
+        if domain == h['institution']['domain']:
+            return True
+    return False
+
 with open(os.path.join(os.path.dirname(__file__),'../data/areachairs.csv')) as f:
     reader = csv.reader(f)
     reader.next()
@@ -20,11 +26,29 @@ with open(os.path.join(os.path.dirname(__file__),'../data/areachairs.csv')) as f
         middle = line[1].strip()
         last = line[2].strip()
         email = line[3].lower().strip()
+        conflicts = [c.strip() for c in line[5].lower().strip().split(';')]
         try:
-            profile = openreview.tools.create_profile(client, first = first, middle = middle, last = last, email = email)
-            members.append(profile.id)
+            profile = client.get_profile(email)
+            history = profile.content.get('history', [])
+
+            for c in conflicts:
+                exists = search_institution(history, c)
+                if not exists:
+                    history.append({
+                        'end': None,
+                        'start': None,
+                        'position': None,
+                        'institution': {
+                            'name': c,
+                            'domain': c
+                        }
+                    })
+            profile.content['history'] = history
+
+            saved_profile = client.update_profile(profile.id, profile.content)
+            members.append(saved_profile.id)
         except Exception as error:
-            print('Error get profile', email, error)
+            print(error, email)
 
 areachairs_group = client.post_group(openreview.Group(**{
     'id': 'cv-foundation.org/ECCV/2018/Conference/Area_Chairs',
