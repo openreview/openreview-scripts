34f849bbd3b4135b2b3ab1915d02264189097463
Lee Campbell <lee@salk.edu>
2018-03-21 22:52:46 -0700

    adding get-workshops

--- venues/ICLR.cc/2018/Conference/python/programchairs/get-workshops.py
+++ venues/ICLR.cc/2018/Conference/python/programchairs/get-workshops.py
@@ -1,173 +1,91 @@
 #!/usr/bin/env python
 
 """
-Dump csv file of meta-review information to make final decision.
+Dump xlsx file of accepted workshop papers
 """
+
 import argparse
 import openreview
-import xlsxwriter
+import openpyxl
 import requests
+import datetime
 from pdb import set_trace as debug
+
+year = datetime.datetime.now().year
 
 ## Argument handling
 parser = argparse.ArgumentParser()
 parser.add_argument('--baseurl', help="base url")
 parser.add_argument('--username')
 parser.add_argument('--password')
-parser.add_argument('--ofile', help="output file name - default to status.csv")
 args = parser.parse_args()
-
 
 # load in all acceptance decisions
 def load_decisions(client):
-    decisions = client.get_notes(invitation='ICLR.cc/2018/Workshop/-/Acceptance_Decision')
-    dec_info = {}
-    for decision in decisions:
-        if decision.content['decision'].startswith('Accept'):
-            dec_info[decision.forum] = decision.content['decision']
+    decisions = client.get_notes(invitation='ICLR.cc/{0}/Workshop/-/Acceptance_Decision'.format(year))
+    dec_info = {decision.forum if decision.content['decision'].startswith('Accept') else None: decision.content['decision'] for decision in decisions}
     return dec_info
-
-def load_profile(profile_info, author, profile):
-    profile_info[author] = {}
-
-    profile_info[author]['first'] = profile['content']['names'][0]['first']
-    if len(profile['content']['names'][0]['middle']) > 0:
-        profile_info[author]['mi'] = profile['content']['names'][0]['middle'][0]
-    else:
-        profile_info[author]['mi'] = " "
-
-    profile_info[author]['last'] = profile['content']['names'][0]['last']
-    # if preferred email isn't set, use email from form
-    if profile['content']['preferred_email'] != "":
-        profile_info[author]['email'] = profile['content']['preferred_email']
-    else:
-        profile_info[author]['email'] = author
-    # check for most recent entry in history
-    end_date = 0
-    profile_info[author]['institute'] = ""
-    for entry in profile['content']['history']:
-        if entry['end'] > end_date:
-            end_date = entry['end']
-            profile_info[author]['institute'] = entry['institution']['name']
-    return profile_info
 
 def main():
     ## Initialize the client library with username and password
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
-    submissions = client.get_notes(invitation='ICLR.cc/2018/Workshop/-/Acceptance_Decision')
+    submissions = client.get_notes(invitation='ICLR.cc/{0}/Workshop/-/Submission'.format(year))
     decision_info = load_decisions(client)
-    profile_info = {}
 
     # get all profile information ahead by first listing all authors needed
-    authors = []
-    for note in submissions:
-        if note.forum in decision_info:
-            authors.extend(note.forumContent.get("authorids"))
-    # remove duplicates
-    authors = list(set(authors))
+    authors = set()
+    [authors.update(note.content.get("authorids")) for note in submissions if note.forum in decision_info]
 
-    # get all associated profiles
-
-    profiles = []
+    profile_info = {}
     for email in authors:
         print email,
         try:
             pf = client.get_profile(email)
-            print
+            if type(pf) == type([]):
+                debug()
+            info = {}
+            name = pf.content.get("names")[0]
+            info['lastname'] = name.get("last")
+            info['firstname'] = name.get("first")
+            info['middleinitial'] = name.get("middle")
+            preferred_email = pf.content.get("preferred_email")
+            info['email'] = preferred_email if preferred_email else email
+            history = sorted(pf.content['history'], key = lambda x:x['end'], reverse=True)
+            info['institution'] = history[0].get("institution").get("name") if history else u""
+            profile_info[email] = info
         except Exception as e:
-            pf = email
-            print "*"
-
-        profiles.append(pf)
-    debug()
-    for item in profiles:
-        author = item['email']
-        profile = item['profile']
-        if author not in profile_info:
-            profile_info = load_profile(profile_info, author, profile)
-
-
-
-    ## Initialize output file name
-    file_name = 'ICLR_workshops.xlsx'
-    if args.ofile!=None:
-        file_name = args.ofile
+            print e,
+            profile_info[email] = {}
+        print
 
     # Create a workbook and add a worksheet.
-    workbook = xlsxwriter.Workbook(file_name)
-    worksheet = workbook.add_worksheet()
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
 
-    # Start from the first cell. Rows and columns are zero indexed.
-    row = 0
-    col = 0
-
-    # write the header
-    header = ['Unique Id', 'Paper Number', 'Title', 'Keywords', 'Type', 'Date', 'Start Time', 'End Time', 'Abstract',
-              'External URL', 'Poster ID', 'Location', 'Author Count', 'Last Name', 'Middle Initial', 'First Name',
-              'Email', 'Institution', 'Department', 'Last Name', 'Middle Initial', 'First Name', 'Email', 'Institution',
-              'Department']
-    for item in header:
-        worksheet.write(row, col, item)
-        col += 1
-    row +=1
+    worksheet.append(['Unique Id', 'Paper Number', 'Title', 'Keywords', 'Type', 'Date', 'Start Time', 'End Time', 'Abstract',
+        'External URL', 'Poster ID', 'Location', 'Author Count', 'Last Name', 'Middle Initial', 'First Name',
+        'Email', 'Institution', 'Department', 'Last Name', 'Middle Initial', 'First Name', 'Email', 'Institution',
+        'Department'])
 
     for note in submissions:
         if note.forum in decision_info:
-            print note.number
-            # paper data
-            col = 0
-            worksheet.write(row, col, note.forum)
-            col += 2 # Paper Number
-            worksheet.write(row, col, note.content['title'])
-            col += 2 #Keywords
-            worksheet.write(row, col, decision_info[note.forum])
-            # skipping Date, StartTime, EndTime
-            col += 4
-            worksheet.write(row, col, note.content['abstract'])
-            col += 1
-            worksheet.write(row, col, note.content['pdf'])
-            # skipping PosterID, Location
-            col += 3
-            # author data
-            worksheet.write(row, col, len(note.content['authorids']))
-            col += 1
+            row = [note.forum, note.number, note.content['title'], None, "Accept(Workshop)", None, None, None, note.content['abstract'],
+                note.content['pdf'], None, None, len(note.content['authorids'])]
+    
             for author in note.content['authorids']:
-                if author not in profile_info:
-                    ## A hack to get profiles that are missed by get_profiles
-                    try:
-                        note = client.get_profile(author)
-                        profile = {'content': note.content}
-                        profile_info = load_profile(profile_info, author, profile)
-                    except openreview.OpenReviewException as e:
-                        # cannot find author_id in profile notes
-                        e =1
-                if author in profile_info:
-                    worksheet.write(row, col, profile_info[author]['last'])
-                    col += 1
-                    worksheet.write(row, col, profile_info[author]['mi'])
-                    col += 1
-                    worksheet.write(row, col, profile_info[author]['first'])
-                    col += 1
-                    worksheet.write(row, col, profile_info[author]['email'])
-                    col += 1
-                    worksheet.write(row, col, profile_info[author]['institute'])
-                    # skipping department
-                    col += 2
+                profile = profile_info.get(author)
+                if profile:
+                    row.extend([profile.get('lastname'), profile.get('middleinitial'), profile.get('firstname'), profile.get('email'), 
+                        profile.get('institution'), None])
                 else:
                     # skip names
-                    col += 3
-                    worksheet.write(row, col, author)
-                    # skipping institute and department
-                    col += 3
+                    try:
+                        row.extend([None, None, None, author, None, None])
+                    except:
+                        debug()
+            worksheet.append(row)
 
-            row += 1
-
-    workbook.close()
-
-
-
-
-
+    workbook.save('ICLR_workshops.xlsx')
 
 if __name__ == "__main__":
     main()
