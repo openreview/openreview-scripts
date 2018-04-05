7b130512663d3563520a3a0f72574f015f9e8b39
Michael Spector <michaelspector@gmail.com>
2018-04-05 12:34:43 -0400

    reviewer match functional

---
+++ venues/cv-foundation.org/ECCV/2018/Conference/python/match-reviewers.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import argparse
+import openreview
+import openreview_matcher
+
+# Argument handling
+parser = argparse.ArgumentParser()
+parser.add_argument('--label', required=True)
+parser.add_argument('--username')
+parser.add_argument('--password')
+parser.add_argument('--baseurl', help="base URL")
+args = parser.parse_args()
+
+client = openreview.Client(username=args.username, password=args.password, baseurl=args.baseurl)
+
+print("connecting to {}".format(client.baseurl))
+
+config_note = openreview.Note(**{
+    'invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Assignment_Configuration',
+    'readers': [
+        'cv-foundation.org/ECCV/2018/Conference',
+        'cv-foundation.org/ECCV/2018/Conference/Program_Chairs'
+    ],
+    'writers': ['cv-foundation.org/ECCV/2018/Conference'],
+    'signatures': ['cv-foundation.org/ECCV/2018/Conference'],
+    'content': {
+        'label': args.label,
+        'configuration': {
+            'minusers': 3,
+            'maxusers': 3,
+            'minpapers': 0,
+            'maxpapers': 8,
+            'alternates': 5,
+            'weights': {
+                'tpms_score': 1,
+                'conflict_score': 1
+            }
+        },
+        #'constraints': {}, # manually-added constraints are not supported yet
+        'paper_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Submission',
+        'metadata_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Paper_Metadata/Reviewers',
+        'assignment_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Paper_Assignment',
+        'match_group': 'cv-foundation.org/ECCV/2018/Conference/Reviewers',
+        'status': 'complete' # 'queued', 'processing', 'complete' or 'error'
+    }
+})
+
+assignments = openreview_matcher.match(client, config_note)
---
+++ venues/cv-foundation.org/ECCV/2018/Conference/python/setup-metadata-reviewers.py
@@ -0,0 +1,176 @@
+import openreview
+import random
+import argparse
+import requests
+from collections import defaultdict
+import csv
+import os
+import json
+import pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print 'connecting to {0}'.format(client.baseurl)
+
+group_ids = [
+    'cv-foundation.org/ECCV/2018/Conference/Reviewers'
+]
+
+metadata_invitation = 'cv-foundation.org/ECCV/2018/Conference/-/Paper_Metadata/Reviewers'
+
+#Load Papers
+print "getting all submissions..."
+papers = openreview.tools.get_all_notes(client, 'cv-foundation.org/ECCV/2018/Conference/-/Submission')
+papers_by_forum = {}
+forum_by_paperId = {}
+for p in papers:
+    papers_by_forum[p.forum] = p
+    forum_by_paperId[p.content['paperId']] = p.forum
+
+#Load group profiles
+print "getting groups..."
+groups = [client.get_group(g) for g in group_ids]
+profiles_by_id = {}
+for g in groups:
+    profiles_by_id.update({profile.id: profile for profile in client.get_profiles(g.members)})
+
+#Load TPMS scores
+print "loading tpms scores from file..."
+tpms_scores = defaultdict(list)
+scores_by_email = {}
+file = '../data/reviewers_scores.csv'
+with open(os.path.join(os.path.dirname(__file__), file)) as f:
+    reader = csv.reader(f)
+    reader.next()
+    for line in reader:
+        paperId = line[0].strip()
+        email = line[1].strip().lower()
+        score = line[2].strip()
+
+        if email not in scores_by_email:
+            scores_by_email[email] = {}
+
+        forum = forum_by_paperId.get(paperId, 0)
+
+        scores_by_email[email][forum] = float(score)
+
+    #translate emails to ids
+    for k,v in scores_by_email.iteritems():
+        profiles = client.get_profiles([k])
+        if profiles:
+            tpms_scores[profiles[k].id] = v
+        else:
+            'Profile not found', k
+
+#Load author profiles
+print "loading author profiles from file..."
+author_profiles_by_email = {}
+with open('../data/profiles.pkl', 'rb') as f:
+    author_profiles_by_email = pickle.load(f)
+
+#Load CMT conflicts
+cmt_conflicts = defaultdict(list)
+with open('../data/reviewer-conflicts.csv') as f:
+    reader = csv.reader(f)
+    reader.next()
+    for line in reader:
+        email = line[2].strip().lower()
+        paperid = line[3].strip().lower()
+        if paperid in forum_by_paperId:
+            conflicted_forum = forum_by_paperId[paperid]
+            profiles = client.get_profiles([email])
+            if profiles:
+                userid = profiles[email].id
+            else:
+                print "no profile found while updating CMT conflicts: ", email
+            if userid:
+                cmt_conflicts[conflicted_forum].append(userid)
+        else:
+            print "couldn't find paper with id ", paperid
+
+def conflict(forum, user_id):
+    try:
+        paper = papers_by_forum[forum]
+        profile = profiles_by_id[user_id]
+        author_profiles = {}
+        for authorid in paper.content['authorids']:
+            author_profiles[authorid] = author_profiles_by_email.get(authorid, None)
+        return openreview.matching.get_conflicts(author_profiles, profile)
+    except KeyError as e:
+        print "conflict error!"
+        print 'forum: ', forum
+        return []
+
+def tpms(forum, user_id):
+    if forum in tpms_scores[user_id]:
+        return tpms_scores[user_id][forum]
+    else:
+        print 'Score not found', forum, user_id
+        return 0.0
+
+
+def metadata(forum, groups):
+    metadata_params = {
+        'forum': forum,
+        'invitation': metadata_invitation,
+        'readers': [
+            'cv-foundation.org/ECCV/2018/Conference',
+            'cv-foundation.org/ECCV/2018/Conference/Program_Chairs'
+        ],
+        'writers': ['cv-foundation.org/ECCV/2018/Conference'],
+        'signatures': ['cv-foundation.org/ECCV/2018/Conference'],
+        'content': {
+            'groups': {}
+        }
+    }
+
+    forum_cmt_conflicts = cmt_conflicts[forum]
+
+    for g in groups:
+
+        group_entry = []
+        for user_id in g.members:
+            if '~' in user_id:
+                user_entry = {'userId': user_id, 'scores': {}}
+                tpms_score = tpms(forum, user_id)
+                conflicts = conflict(forum, user_id)
+
+
+                if conflicts:
+                    user_entry['scores']['conflict_score'] = '-inf'
+                    user_entry['conflicts'] = conflicts
+                if tpms_score > 0:
+                    user_entry['scores']['tpms_score'] = tpms_score
+
+                if user_id in forum_cmt_conflicts:
+                    user_entry['scores']['conflict_score'] = '-inf'
+                    if 'conflicts' not in user_entry:
+                        user_entry['conflicts'] = ['CMT_conflict']
+                    else:
+                        user_entry['conflicts'].append('CMT_conflict')
+
+                group_entry.append(user_entry)
+
+        metadata_params['content']['groups'][g.id] = group_entry
+
+    return metadata_params
+
+print "getting existing metadata notes..."
+existing_notes_by_forum = {n.forum: n for n in openreview.tools.get_all_notes(client, metadata_invitation)}
+
+print "updating paper metadata..."
+for p in papers:
+    if p.forum in existing_notes_by_forum:
+        metadata_params = existing_notes_by_forum[p.forum].to_json()
+    else:
+        metadata_params = {}
+
+    metadata_params.update(metadata(p.forum, groups))
+    metadata_note = client.post_note(openreview.Note(**metadata_params))
+    print metadata_note.id
