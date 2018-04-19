061d49a8bc76d1b30c30caf337ed0117566f18f2
Michael Spector <michaelspector@gmail.com>
2018-04-19 11:02:48 -0400

    Revert "move process reviewer file script to common directory, and add argument handling"
    This reverts commit dee1026e7e8ea3d1b495f615a17bff3cb99ed874.

--- venues/MIDL.amsterdam/2018/Conference/python/process-reviewer-assignments.py
+++ venues/MIDL.amsterdam/2018/Conference/python/process-reviewer-assignments.py
@@ -4,19 +4,10 @@ processes the file "midl_assigned_reviews.csv", sent to us by Geert on April 16,
 This script reformats the information in the above file in a format that
 the assign-reviewers.py script can accept.
 '''
-## Import statements
-import argparse
+
 import csv
-
-
-## Argument handling
-parser = argparse.ArgumentParser()
-parser.add_argument('file')
-parser.add_argument('outfile')
-args = parser.parse_args()
-
 new_rows = []
-with open(args.file) as f:
+with open('../data/midl_assigned_reviews.csv') as f:
     reader = csv.reader(f)
     reader.next()
     for row in reader:
@@ -28,6 +19,6 @@ with open(args.file) as f:
         new_rows.append([rev2, papernum])
         new_rows.append([rev3, papernum])
 
-with open(args.outfile, 'w') as f:
+with open('../data/2018-04-17-midl-reviewer-assignments.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerows(new_rows)
