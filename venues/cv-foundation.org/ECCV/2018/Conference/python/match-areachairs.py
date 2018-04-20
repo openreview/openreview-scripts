621e75945797bc95a4e1472754a6185b3741f074
Michael Spector <michaelspector@gmail.com>
2018-04-20 14:08:39 -0400

    generalize match-areachairs script

--- venues/cv-foundation.org/ECCV/2018/Conference/python/match-areachairs.py
+++ venues/cv-foundation.org/ECCV/2018/Conference/python/match-areachairs.py
@@ -11,7 +11,7 @@ import openreview_matcher
 # Argument handling
 parser = argparse.ArgumentParser()
 parser.add_argument('--label', required=True)
-parser.add_argument('--number')
+parser.add_argument('--metadata_inv', default='cv-foundation.org/ECCV/2018/Conference/-/Paper_Metadata')
 parser.add_argument('-p', '--paperlimits', nargs=2, required=True, help='Enter two integers: the first should be the minimum number of papers per user, and the second should be the maximum.')
 parser.add_argument('-u', '--userlimits', nargs=2, required=True, help='Enter two integers: the first should be the minimum number of users per paper, and the second should be the maximum.')
 parser.add_argument('--username')
@@ -31,19 +31,7 @@ print("matching with the following parameters: ")
 print("Min/Max users: {}/{}".format(minusers, maxusers))
 print("Min/Max papers: {}/{}".format(minpapers, maxpapers))
 
-if args.number:
-    config_notes = client.get_notes(
-        invitation='cv-foundation.org/ECCV/2018/Conference/-/Assignment_Configuration',
-        number=args.number)
-else:
-    config_notes = client.get_notes(
-        invitation='cv-foundation.org/ECCV/2018/Conference/-/Assignment_Configuration')
-
-config_by_label = {n.content['label']: n.to_json() for n in config_notes}
-
-# post the configuration note. in the future, this note will be posted through the UI.
-
-configuration_note_params = {
+config_note = openreview.Note(**{
     'invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Assignment_Configuration',
     'readers': [
         'cv-foundation.org/ECCV/2018/Conference',
@@ -52,7 +40,7 @@ configuration_note_params = {
     'writers': ['cv-foundation.org/ECCV/2018/Conference'],
     'signatures': ['cv-foundation.org/ECCV/2018/Conference'],
     'content': {
-        'label': label,
+        'label': args.label,
         'configuration': {
             'minusers': minusers,
             'maxusers': maxusers,
@@ -60,33 +48,17 @@ configuration_note_params = {
             'maxpapers': maxpapers,
             'alternates': 5,
             'weights': {
-                'tpms_score': 1,
-                'conflict_score': 1
+                'tpmsScore': 1.0
             }
         },
         'constraints': {},
         'paper_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Submission',
-        'metadata_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Paper_Metadata',
+        'metadata_invitation': args.metadata_inv,
         'assignment_invitation': 'cv-foundation.org/ECCV/2018/Conference/-/Paper_Assignment',
         'match_group': 'cv-foundation.org/ECCV/2018/Conference/Area_Chairs',
         'status': 'complete' # 'queued', 'processing', 'complete' or 'error'
     }
-}
+})
 
-if args.number:
-    config_note = openreview.matching.create_or_update_config(client, label, configuration_note_params, number=args.number)
-else:
-    config_note = openreview.matching.create_or_update_config(client, label, configuration_note_params, number=args.number)
+assignments = openreview_matcher.match(client, config_note)
 
-print(config_note.id)
-print("config note constraints")
-for k,v in config_note.content['constraints'].iteritems():
-    print(k,v)
-
-posted_config = client.post_note(config_note)
-
-assignments = openreview.matching.match(client, posted_config, openreview_matcher.Solver)
-
-for n in assignments:
-    print("posting assignment for ", n.forum, label)
-    client.post_note(n)
