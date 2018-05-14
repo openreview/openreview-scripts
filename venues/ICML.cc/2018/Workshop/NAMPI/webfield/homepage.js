f49da08a765e5dac7d555249044046b4865c998f
mspector <michael.l.spector@gmail.com>
2018-05-14 14:18:11 -0400

    Feature/nampi recruitment (#710)
    * refactor homepage.js
    
    * add recruitment process and webfield
    
    * add functions and config file
    
    * add setup ipynb file
    
    * update comment
    
    * add setup script as a python file
    
    * link promises
    
    * lowercase group and invitation object variables
    
    * rename config to nampi

--- venues/ICML.cc/2018/Workshop/NAMPI/webfield/homepage.js
+++ venues/ICML.cc/2018/Workshop/NAMPI/webfield/homepage.js
@@ -6,11 +6,19 @@
 // and a list of all submitted papers (#notes).
 // ------------------------------------
 
+var HEADER = {
+  title: 'NAMPI: Neural Abstract Machines & Program Induction',
+  subtitle: 'A Federated Artificial Intelligence Meeting (FAIM) workshop (ICML, IJCAI/ECAI, AAMAS)',
+  location: 'Stockholm, Sweden',
+  date: '2018',
+  website: 'https://uclmr.github.io/nampi/',
+  instructions: 'Check back later for details',  // Add any custom instructions here. Accepts HTML
+  deadline: 'TBD'
+}
 
 // Main is the entry point to the webfield code and runs everything
 function main() {
   Webfield.ui.setup('#group-container', 'ICML.cc/2018/Workshop/NAMPI');  // required
-
   renderConferenceHeader();
 
 }
@@ -18,15 +26,7 @@ function main() {
 // RenderConferenceHeader renders the static info at the top of the page. Since that content
 // never changes, put it in its own function
 function renderConferenceHeader() {
-  Webfield.ui.venueHeader({
-    title: 'NAMPI: Neural Abstract Machines & Program Induction',
-    subtitle: 'A Federated Artificial Intelligence Meeting (FAIM) workshop (ICML, IJCAI/ECAI, AAMAS)',
-    location: 'Stockholm, Sweden',
-    date: '2018',
-    website: 'https://uclmr.github.io/nampi/',
-    instructions: 'Check back later for details',  // Add any custom instructions here. Accepts HTML
-    deadline: 'TBD'
-  });
+  Webfield.ui.venueHeader(HEADER);
 }
 
 // Go!
