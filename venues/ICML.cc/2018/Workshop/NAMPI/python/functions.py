9b299c574a419ba6c2dc472d018f0df5e61560fd
Michael Spector <michaelspector@gmail.com>
2018-05-16 15:57:01 -0400

    update functions to match new config

--- venues/ICML.cc/2018/Workshop/NAMPI/python/functions.py
+++ venues/ICML.cc/2018/Workshop/NAMPI/python/functions.py
@@ -1,5 +1,5 @@
 import openreview
-import config
+import nampi
 
 def recruit_reviewer(client, email, first, verbose=True):
     '''
@@ -15,23 +15,23 @@ def recruit_reviewer(client, email, first, verbose=True):
     # build the URL to send in the message
     url = '{baseurl}/invitation?id={recruitment_inv}&email={email}&key={hashkey}&response='.format(
         baseurl = client.baseurl,
-        recruitment_inv = config.recruit_reviewers.id,
+        recruitment_inv = nampi.recruit_reviewers.id,
         email = email,
         hashkey = hashkey
     )
 
     # format the message defined above
-    personalized_message = config.RECRUIT_MESSAGE.format(
+    personalized_message = nampi.RECRUIT_MESSAGE.format(
         name = first,
         accept_url = url + "Yes",
         decline_url = url + "No"
     )
 
     # send the email through openreview
-    response = client.send_mail(config.RECRUIT_MESSAGE_SUBJ, [email], personalized_message)
+    response = client.send_mail(nampi.RECRUIT_MESSAGE_SUBJ, [email], personalized_message)
 
     if 'groups' in response and response['groups']:
-        reviewers_invited = client.get_group(config.REVIEWERS_INVITED_ID)
+        reviewers_invited = client.get_group(nampi.REVIEWERS_INVITED_ID)
         client.add_members_to_group(reviewers_invited, response['groups'])
 
     if verbose:
