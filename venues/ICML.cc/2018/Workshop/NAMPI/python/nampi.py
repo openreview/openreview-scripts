74305c8804572ed362588567977c7d9f9c506113
mspector <michael.l.spector@gmail.com>
2018-05-17 13:11:52 -0400

    Fix/update nampi (#714)
    * update configuration and recruitment message
    
    * add submission invitations to nampi config
    
    * update functions to match new config
    
    * update nampi homepage
    
    * allow nampi admin to remove originals
    
    * add real nampi contact email address
    
    * update instructions
    
    * update nampi recruitment message
    
    * add submission process

--- venues/ICML.cc/2018/Workshop/NAMPI/python/nampi.py
+++ venues/ICML.cc/2018/Workshop/NAMPI/python/nampi.py
@@ -16,6 +16,10 @@ REVIEWERS_ID = CONFERENCE_ID + '/Reviewers'
 REVIEWERS_INVITED_ID = REVIEWERS_ID + '/Invited'
 REVIEWERS_DECLINED_ID = REVIEWERS_ID + '/Declined'
 
+SUBMISSION_ID = CONFERENCE_ID + '/-/Submission'
+BLIND_SUBMISSION_ID = CONFERENCE_ID + '/-/Blind_Submission'
+
+SUBMISSION_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=6, day=1)
 
 '''
 Group definitions.
@@ -23,8 +27,8 @@ Group definitions.
 conference = openreview.Group(**{
     'id': CONFERENCE_ID,
     'readers':['everyone'],
-    'writers': [],
-    'signatures': [],
+    'writers': [CONFERENCE_ID],
+    'signatures': [CONFERENCE_ID],
     'signatories': [CONFERENCE_ID],
     'members': [],
     'web': '../webfield/homepage.js'
@@ -70,20 +74,76 @@ reviewers_declined = openreview.Group(**{
 Invitation definitions.
 '''
 
-recruit_reviewers = invitations.Recruitment(
+# Recruitment
+recruit_reviewers = invitations.RecruitReviewers(
     id = CONFERENCE_ID + '/-/Recruit_Reviewers',
     conference_id = CONFERENCE_ID,
     process = os.path.abspath('../process/recruitReviewerProcess.js'),
     web = os.path.abspath('../webfield/recruitReviewerWebfield.js')
 )
 
+
+# Configure paper submissions
+submission_inv = invitations.Submission(
+    id = SUBMISSION_ID,
+    conference_id = CONFERENCE_ID,
+    duedate = SUBMISSION_DEADLINE,
+    process = os.path.abspath('../process/submissionProcess.js'),
+    reply_params = {
+        'readers': {
+            'values-copied': [
+                CONFERENCE_ID,
+                '{content.authorids}',
+                '{signatures}'
+            ]
+        },
+        'signatures': {
+            'values-regex': '|'.join(['~.*', CONFERENCE_ID])
+        },
+        'writers': {
+            'values-copied': [
+                CONFERENCE_ID,
+                '{signatures}'
+            ]
+        }
+    }
+)
+
+blind_submission_inv = invitations.Submission(
+    id = BLIND_SUBMISSION_ID,
+    conference_id = CONFERENCE_ID,
+    duedate = SUBMISSION_DEADLINE,
+    mask = {
+        'authors': {
+            'values': ['Anonymous']
+        },
+        'authorids': {
+            'values-regex': '.*'
+        }
+    },
+    reply_params = {
+        'signatures': {
+            'values': [CONFERENCE_ID]},
+        'readers': {
+            'values-regex': CONFERENCE_ID + '.*'
+        }
+    }
+)
+
 '''
 Email templates.
 '''
-RECRUIT_MESSAGE_SUBJ = 'NAMPI 2018: Invitation to Review'
+RECRUIT_MESSAGE_SUBJ = 'Invitation to review for Neural Abstract Machines & Program Induction v2 (NAMPI_v2) workshop @ ICML 2018'
 RECRUIT_MESSAGE = '''Dear {name},
 
-You have been invited to serve as a reviewer for the ICML 2018 NAMPI workshop.
+We would like to invite you to serve on the Program Committee for the Neural Abstract Machines & Program Induction v2 (NAMPI_v2) workshop at ICML 2018. The workshop will broadly focus on neural approaches to computation, program induction, algorithm learning, abstract machines and data structures, and their relation to established learning and non-learning methods in the field. More details can be found here: https://uclmr.github.io/nampi/
+
+The NAMPI_v2 workshop will be held in Stockholm, Sweden on July 15th, 2018.
+
+The Program Committee will review up to 4-page (not including references) submissions, and will consist of experts in various fields of interest for the workshop. Realising your busy schedule, we will try to keep the load as low as possible. Our current estimate is 1-2 papers a person max.
+
+We would be grateful if you could confirm your acceptance or refusal of the participation in the NAMPI_v2 committee by clicking on one of the links below..
+
 
 To ACCEPT the invitation, please click on the following link:
 
@@ -93,9 +153,22 @@ To DECLINE the invitation, please click on the following link:
 
 {decline_url}
 
-We really hope you will be able to accept our invitation and help us select a high quality program for NAMPI 2018!
+Workshop deadlines:
+Paper submission: June 1st (There will be no extensions)
+Notification of acceptance: June 15th
+Final Papers Due: June 22nd
+Deadlines are at 11:59pm PDT.
 
-Best regards,
-The NAMPI 2018 Program Chairs
+Peer reviewing will be conducted on a double-blind basis, with open comments.
+
+In case you accept the invitation, we will soon send you further instructions on how to proceed with the reviewing duties.
+
+Please confirm your participation as soon as possible. We know that we will make NAMPI_v2 a big success with your help.
+
+
+
+Kind regards,
+Dawn, Matko, Pushmeet, Rob, Sebastian, Tejas, Tim
+
 
 '''
--- venues/ICML.cc/2018/Workshop/NAMPI/webfield/homepage.js
+++ venues/ICML.cc/2018/Workshop/NAMPI/webfield/homepage.js
@@ -1,9 +1,8 @@
-
 // ------------------------------------
-// Basic venue homepage template
+// Advanced venue homepage template
 //
 // This webfield displays the conference header (#header), the submit button (#invitation),
-// and a list of all submitted papers (#notes).
+// and a tabbed interface for viewing various types of notes.
 // ------------------------------------
 
 var HEADER = {
@@ -12,23 +11,407 @@ var HEADER = {
   location: 'Stockholm, Sweden',
   date: '2018',
   website: 'https://uclmr.github.io/nampi/',
-  instructions: 'Check back later for details',  // Add any custom instructions here. Accepts HTML
-  deadline: 'TBD'
+  instructions: '<p><strong>Important Information about Anonymity:</strong><br>\
+    When you post a submission to NAMPI 2018, please provide the real names and email addresses of authors in the submission form below.\
+    An anonymous record of your paper will appear in the "All Papers Under Review" and "My Papers Under Review" tabs. \
+    All discussion will take place on the anonymous versions of your paper(s).\
+    The <em>original</em> record of your submission will be private, and will contain your real name(s); \
+    originals can be found in the "My Submitted Papers" tab.\
+    Please do not include the author names in the PDF for your submission. </p>\
+    <p><strong>Editing or Revising your submission:</strong><br>\
+    Anonymous versions of papers are not directly editable. To make a change to your paper, go to the \
+    original, non-anonymous version and edit it (if editing is enabled). Changes will propagate to the \
+    anonymous version. Edit and revision policy is determined by the specific policy of this conference. \
+    <p><strong>Questions or Concerns:</strong><br> \
+    Please contact the OpenReview support team at \
+    <a href="mailto:info@openreview.net">info@openreview.net</a> with any questions or concerns about the OpenReview platform. \</br> \
+    Please contact the NAMPI 2018 Program Chairs at \
+    <a href="mailto:nampi@googlegroups.com">nampi@googlegroups.com</a> with any questions or concerns about conference administration or policy. \</p>',
+  deadline: 'June 1, 2018'
 }
+
+// Constants
+var CONFERENCE_ID = 'ICML.cc/2018/Workshop/NAMPI';
+var SUBMISSION_ID = CONFERENCE_ID + '/-/Submission';
+var BLIND_SUBMISSION_ID = CONFERENCE_ID + '/-/Blind_Submission';
+var RECRUIT_REVIEWERS = CONFERENCE_ID + '/-/Recruit_Reviewers';
+var WILDCARD_INVITATION = CONFERENCE_ID + '/-/.*';
+
+var ANON_SIGNATORY_REGEX = /^ICML\.cc\/2018\/Workshop\/NAMPI\/Paper(\d+)\/(AnonReviewer\d+|Area_Chair\d+)/;
+var AUTHORS_SIGNATORY_REGEX = /^ICML\.cc\/2018\/Workshop\/NAMPI\/Paper(\d+)\/Authors/;
+
+var AREA_CHAIRS_ID = CONFERENCE_ID + '/Area_Chairs';
+var PROGRAM_CHAIRS_ID = CONFERENCE_ID + '/Program_Chairs';
+
+
+var COMMENT_EXCLUSION = [
+  SUBMISSION_ID,
+  RECRUIT_REVIEWERS
+];
+
+var BUFFER = 1000 * 60 * 30;  // 30 minutes
+var PAGE_SIZE = 50;
+
+var paperDisplayOptions = {
+  pdfLink: true,
+  replyCount: true,
+  showContents: true
+};
+var commentDisplayOptions = {
+  pdfLink: false,
+  replyCount: true,
+  showContents: false,
+  showParent: true
+};
+var initialPageLoad = true;
 
 // Main is the entry point to the webfield code and runs everything
 function main() {
-  Webfield.ui.setup('#group-container', 'ICML.cc/2018/Workshop/NAMPI');  // required
+  Webfield.ui.setup('#group-container', CONFERENCE_ID);  // required
+
   renderConferenceHeader();
+
+  renderSubmissionButton();
+
+  renderConferenceTabs();
+
+  load().then(renderContent);
+}
+
+// Load makes all the API calls needed to get the data to render the page
+// It returns a jQuery deferred object: https://api.jquery.com/category/deferred-object/
+function load() {
+  var notesP = Webfield.api.getSubmissions(BLIND_SUBMISSION_ID, {
+    pageSize: PAGE_SIZE
+  });
+
+  var submittedNotesP = Webfield.api.getSubmissions(WILDCARD_INVITATION, {
+    pageSize: PAGE_SIZE,
+    tauthor: true
+  });
+
+  var assignedNotePairsP = Webfield.api.getSubmissions(WILDCARD_INVITATION, {
+    pageSize: 100,
+    invitee: true,
+    duedate: true
+  });
+
+  var userGroupsP;
+  var authorNotesP;
+  if (!user || _.startsWith(user.id, 'guest_')) {
+    userGroupsP = $.Deferred().resolve([]);
+    authorNotesP = $.Deferred().resolve([]);
+
+  } else {
+    userGroupsP = Webfield.get('/groups', {member: user.id}).then(function(result) {
+      return _.filter(
+        _.map(result.groups, function(g) { return g.id; }),
+        function(id) { return _.startsWith(id, CONFERENCE_ID); }
+      );
+    });
+
+    authorNotesP = Webfield.get('/notes/search', {
+      term: user.profile.id,
+      group: CONFERENCE_ID,
+      content: 'authors',
+      source: 'forum'
+    }).then(function(result) {
+      return result.notes;
+    });
+  }
+
+  var tagInvitationsP = Webfield.api.getTagInvitations(BLIND_SUBMISSION_ID);
+
+  return userGroupsP
+  .then(function(userGroups) {
+
+    var assignedPaperNumbers = getPaperNumbersfromGroups(userGroups);
+    var assignedNotesP = $.Deferred().resolve([]);
+
+    if (assignedPaperNumbers.length) {
+        assignedNotesP = Webfield.api.getSubmissions(BLIND_SUBMISSION_ID, {
+        pageSize: PAGE_SIZE,
+        number: assignedPaperNumbers.join()
+      });
+    }
+
+
+    return $.when(
+      notesP, submittedNotesP, assignedNotePairsP, assignedNotesP, userGroups,
+      authorNotesP, tagInvitationsP
+    );
+  });
 
 }
 
-// RenderConferenceHeader renders the static info at the top of the page. Since that content
-// never changes, put it in its own function
+
+// Render functions
 function renderConferenceHeader() {
   Webfield.ui.venueHeader(HEADER);
+
+  Webfield.ui.spinner('#notes');
+}
+
+function renderSubmissionButton() {
+  Webfield.api.getSubmissionInvitation(SUBMISSION_ID, {deadlineBuffer: BUFFER})
+    .then(function(invitation) {
+      Webfield.ui.submissionButton(invitation, user, {
+        onNoteCreated: function() {
+          // Callback funtion to be run when a paper has successfully been submitted (required)
+          promptMessage('Your submission is complete. The list of all current submissions is shown below.');
+
+          load().then(renderContent).then(function() {
+            $('.tabs-container a[href="#all-papers-under-review"]').click();
+          });
+        }
+      });
+    });
+}
+
+function renderConferenceTabs() {
+  var sections = [
+    {
+      heading: 'All Papers Under Review',
+      id: 'all-papers-under-review',
+    },
+    {
+      heading: 'My Papers Under Review',
+      id: 'my-papers-under-review',
+    },
+    {
+      heading: 'My Submitted Papers',
+      id: 'my-submitted-papers',
+    },
+    {
+      heading: 'My Tasks',
+      id: 'my-tasks',
+    },
+    {
+      heading: 'My Assigned Papers',
+      id: 'my-assigned-papers',
+    },
+    {
+      heading: 'My Comments & Reviews',
+      id: 'my-comments-reviews',
+    }
+  ];
+
+  Webfield.ui.tabPanel(sections, {
+    container: '#notes',
+    hidden: true
+  });
+}
+
+function renderContent(notes, submittedNotes, assignedNotePairs, assignedNotes, userGroups, authorNotes, tagInvitations) {
+  var data, commentNotes;
+
+  // if (_.isEmpty(userGroups)) {
+  //   // If the user isn't part of the conference don't render tabs
+  //   $('.tabs-container').hide();
+  //   return;
+  // }
+
+  commentNotes = [];
+
+  _.forEach(submittedNotes, function(note) {
+    if (!_.isNil(note.ddate)) {
+      return;
+    }
+    if (!_.includes([SUBMISSION_ID, RECRUIT_REVIEWERS], note.invitation)) {
+      commentNotes.push(note);
+    }
+  });
+
+  // Filter out all tags that belong to other users (important for bid tags)
+  notes = _.map(notes, function(n) {
+    n.tags = _.filter(n.tags, function(t) {
+      return !_.includes(t.signatures, user.id);
+    });
+    return n;
+  });
+
+  var assignedPaperNumbers = getPaperNumbersfromGroups(userGroups);
+  if (assignedPaperNumbers.length !== assignedNotes.length) {
+    console.warn('WARNING: The number of assigned notes returned by API does not ' +
+      'match the number of assigned note groups the user is a member of.');
+  }
+
+  var authorPaperNumbers = getAuthorPaperNumbersfromGroups(userGroups);
+  if (authorPaperNumbers.length !== authorNotes.length) {
+    console.warn('WARNING: The number of submitted notes returned by API does not ' +
+      'match the number of submitted note groups the user is a member of.');
+  }
+
+  // My Tasks tab
+  if (userGroups.length) {
+    var tasksOptions = {
+      container: '#my-tasks',
+      emptyMessage: 'No outstanding tasks for this conference'
+    }
+    Webfield.ui.taskList(assignedNotePairs, tagInvitations, tasksOptions)
+
+    if (_.includes(userGroups, AREA_CHAIRS_ID)) {
+      $('#my-tasks .submissions-list').prepend([
+        '<li class="note invitation-link">',
+          '<a href="/group?id=' + AREA_CHAIRS_ID + '">Area Chair Console</a>',
+        '</li>'
+      ].join(''));
+    }
+
+    if (_.includes(userGroups, PROGRAM_CHAIRS_ID)) {
+      $('#my-tasks .submissions-list').prepend([
+        '<li class="note invitation-link">',
+          '<a href="/assignments?venue=' + CONFERENCE_ID,
+            'Assignments Browser',
+          '</a>',
+        '</li>'
+      ].join(''));
+
+      $('#my-tasks .submissions-list').prepend([
+        '<li class="note invitation-link">',
+          '<a href="/group?id=' + PROGRAM_CHAIRS_ID + '">Program Chair Console</a>',
+        '</li>'
+      ].join(''));
+    }
+  } else {
+    $('.tabs-container a[href="#my-tasks"]').parent().hide();
+  }
+
+  // All Submitted Papers tab
+  var submissionListOptions = _.assign({}, paperDisplayOptions, {
+    showTags: true,
+    tagInvitations: tagInvitations,
+    container: '#all-papers-under-review'
+  });
+
+  Webfield.ui.submissionList(notes, {
+    heading: null,
+    container: '#all-papers-under-review',
+    search: {
+      enabled: true,
+      onResults: function(searchResults) {
+        var blindedSearchResults = searchResults.filter(function(note) {
+          return note.invitation === BLIND_SUBMISSION_ID;
+        });
+        Webfield.ui.searchResults(blindedSearchResults, submissionListOptions);
+        Webfield.disableAutoLoading();
+      },
+      onReset: function() {
+        Webfield.ui.searchResults(notes, submissionListOptions);
+        if (notes.length === PAGE_SIZE) {
+          Webfield.setupAutoLoading(BLIND_SUBMISSION_ID, PAGE_SIZE, submissionListOptions);
+        }
+      }
+    },
+    displayOptions: submissionListOptions,
+    fadeIn: false
+  });
+
+  if (notes.length === PAGE_SIZE) {
+    Webfield.setupAutoLoading(BLIND_SUBMISSION_ID, PAGE_SIZE, submissionListOptions);
+  }
+
+
+  // My Submitted Papers tab
+  if (authorNotes.length) {
+    Webfield.ui.searchResults(
+      authorNotes,
+      _.assign({}, paperDisplayOptions, {container: '#my-submitted-papers'})
+    );
+    console.log('authorNotes', authorNotes);
+    var authorNoteIds = _.map(authorNotes, function(original){
+      return original.id;
+    });
+
+    console.log('authorNoteIds', authorNoteIds);
+    // get blind papers that are authored by this user
+    var myPapersUnderReview = _.filter(notes, function(note){
+      return _.includes(authorNoteIds, note.original);
+    });
+
+    console.log('myPapersUnderReview',myPapersUnderReview);
+
+    // My Papers Under Review tab
+    Webfield.ui.searchResults(
+      myPapersUnderReview,
+      _.assign({}, paperDisplayOptions, {
+        container: '#my-papers-under-review',
+        emptyMessage: 'You have no papers currently under review.'
+      })
+    );
+
+  } else {
+    $('.tabs-container a[href="#my-submitted-papers"]').parent().hide();
+    $('.tabs-container a[href="#my-papers-under-review"]').parent().hide();
+  }
+
+
+
+  // My Assigned Papers tab (only show if not empty)
+  if (assignedNotes.length) {
+    Webfield.ui.searchResults(
+      assignedNotes,
+      _.assign({}, paperDisplayOptions, {container: '#my-assigned-papers'})
+    );
+  } else {
+    $('.tabs-container a[href="#my-assigned-papers"]').parent().hide();
+  }
+
+  // My Comments & Reviews tab (only show if not empty)
+  if (commentNotes.length) {
+    Webfield.ui.searchResults(
+      commentNotes,
+      _.assign({}, commentDisplayOptions, {
+        container: '#my-comments-reviews',
+        emptyMessage: 'No comments or reviews to display'
+      })
+    );
+  } else {
+    $('.tabs-container a[href="#my-comments-reviews"]').parent().hide();
+  }
+
+  $('#notes .spinner-container').remove();
+  $('.tabs-container').show();
+
+  // Show first available tab
+  if (initialPageLoad) {
+    $('.tabs-container ul.nav-tabs li a:visible').eq(0).click();
+    initialPageLoad = false;
+  }
+}
+
+// Helper functions
+function getPaperNumbersfromGroups(groups) {
+  return _.map(
+    _.filter(groups, function(gid) { return ANON_SIGNATORY_REGEX.test(gid); }),
+    function(fgid) { return parseInt(fgid.match(ANON_SIGNATORY_REGEX)[1], 10); }
+  );
+}
+
+function getAuthorPaperNumbersfromGroups(groups) {
+  return _.map(
+    _.filter(groups, function(gid) { return AUTHORS_SIGNATORY_REGEX.test(gid); }),
+    function(fgid) { return parseInt(fgid.match(AUTHORS_SIGNATORY_REGEX)[1], 10); }
+  );
+}
+
+function getDueDateStatus(date) {
+  var day = 24 * 60 * 60 * 1000;
+  var diff = Date.now() - date.getTime();
+
+  if (diff > 0) {
+    return 'expired';
+  }
+  if (diff > -3 * day) {
+    return 'warning';
+  }
+  return '';
 }
 
 // Go!
 main();
+
+
+
+
 
