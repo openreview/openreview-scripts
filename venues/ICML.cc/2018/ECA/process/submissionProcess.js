1627a555a20234cf3275e4d2b1cb47c3a433c0c9
Pam Mandler <mandler@cs.umass.edu>
2018-06-11 14:31:25 -0400

    simplify process function

--- venues/ICML.cc/2018/ECA/process/submissionProcess.js
+++ venues/ICML.cc/2018/ECA/process/submissionProcess.js
@@ -3,7 +3,6 @@ function () {
 
   var SHORT_PHRASE = 'ICML ECA 2018';
   var CONFERENCE_ID = 'ICML.cc/2018/ECA';
-  var PROGRAM_CHAIRS = CONFERENCE_ID+'/Program_Chairs'
 
     // send author a confirmation email
   var author_mail = {
@@ -30,49 +29,8 @@ function () {
         }
    };
 
-  // create paper group, authors group and Authors_and_Committee group for this paper
-  var paperGroup = {
-    id: CONFERENCE_ID + '/Paper' + note.number,
-    signatures: [CONFERENCE_ID],
-    writers: [CONFERENCE_ID],
-    members: [],
-    readers: [CONFERENCE_ID],
-    signatories: []
-  };
-  return or3client.or3request(or3client.grpUrl, paperGroup, 'POST', token)
-  .then(savedPaperGroup => {
-    var reviewerGroupId = savedPaperGroup.id + '/Reviewers';
-    var authorGroupId = savedPaperGroup.id + '/Authors';
-    var authorGroup = {
-      id: authorGroupId,
-      signatures: [CONFERENCE_ID],
-      writers: [CONFERENCE_ID],
-      members: note.content.authorids.concat(note.signatures),
-      readers: [CONFERENCE_ID, PROGRAM_CHAIRS, authorGroupId],
-      signatories: [authorGroupId]
-    };
-    var authorPlusGroupId = authorGroupId+'_and_Committee';
-    var authorPlusGroup = {
-      id: authorPlusGroupId,
-      signatures: [CONFERENCE_ID],
-      writers: [CONFERENCE_ID],
-      members: [CONFERENCE_ID, PROGRAM_CHAIRS, authorGroupId, reviewerGroupId],
-      readers: [CONFERENCE_ID, PROGRAM_CHAIRS, authorGroupId, reviewerGroupId],
-      signatories: [authorPlusGroupId]
-    };
-
-    var promises = [
-      or3client.or3request(or3client.mailUrl, author_mail, 'POST', token),
-      or3client.or3request(or3client.inviteUrl, revisionInvitation, 'POST', token),
-      or3client.or3request(or3client.grpUrl, authorGroup, 'POST', token),
-      or3client.or3request(or3client.grpUrl, authorPlusGroup, 'POST', token)];
-
-    if (note.readers[0] == 'ICML.cc/2018/ECA/Authors_and_Committee') {
-        note.readers = [authorPlusGroupId];
-        promises.push(or3client.or3request(or3client.notesUrl, note, 'POST', token));
-    }
-    return Promise.all(promises);
-    })
+  return or3client.or3request(or3client.mailUrl, author_mail, 'POST', token)
+  .then(result => or3client.or3request(or3client.inviteUrl, revisionInvitation, 'POST', token))
   .then(result => done())
   .catch(error => done(error));
 
