94c8f459a12deb4da1feaed9b08165661ecb2cbf
Michael Spector <michaelspector@gmail.com>
2018-08-01 14:46:08 -0400

    add ICLR 19 scripts

---
+++ venues/ICLR.cc/2019/process/commentProcess.js
@@ -0,0 +1,76 @@
+function(){
+    var or3client = lib.or3client;
+
+    var CONFERENCE_ID = 'ICLR.cc/2019/Conference';
+    var SHORT_PHRASE = "ICLR 2019";
+
+    var origNoteP = or3client.or3request(or3client.notesUrl + '?id=' + note.forum, {}, 'GET', token);
+    var replytoNoteP = note.replyto ? or3client.or3request(or3client.notesUrl + '?id=' + note.replyto, {}, 'GET', token) : null;
+
+    var checkReadersMatch = function(regex) {
+      for(reader of note.readers){
+        if(reader.match(regex)){
+          return true;
+        }
+      }
+      return false;
+    };
+
+
+    Promise.all([
+      origNoteP,
+      replytoNoteP
+    ]).then(function(result) {
+
+      var origNote = result[0].notes[0];
+      var replytoNote = note.replyto ? result[1].notes[0] : null;
+      var replytoNoteSignatures = replytoNote ? replytoNote.signatures : [];
+      var author_mail;
+
+      var ac_mail = {
+        'groups': [CONFERENCE_ID + '/Paper' + origNote.number + '/Area_Chair'],
+        'subject': 'Comment posted to a paper in your area. Title: ' + origNote.content.title,
+        'message': 'A comment was posted to a paper for which you are serving as Area Chair.\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      };
+
+      var reviewer_mail = {
+        'groups': [CONFERENCE_ID + '/Paper' + origNote.number + '/Reviewers'],
+        'subject': 'Comment posted to a paper you are reviewing. Title: ' + origNote.content.title,
+        'message': 'A comment was posted to a paper for which you are serving as reviewer.\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      };
+
+      var pc_mail = {
+        'groups': [CONFERENCE_ID + '/Program_Chairs'],
+        'subject': 'A Program Chair-only comment was posted',
+        'message': 'A comment was posted to a paper with readership restricted to only the Program Chairs.\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      };
+
+      author_mail = {
+        "groups": origNote.content.authorids,
+        "subject": "Your submission to " + SHORT_PHRASE + " has received a comment",;
+        "message": "Your submission to " + SHORT_PHRASE + " has received a comment.\n\nComment title: " + note.content.title + "\n\nComment: " + note.content.comment + "\n\nTo view the comment, click here: " + baseUrl + "/forum?id=" + note.forum + '&noteId=' + note.id;
+      };
+
+      var promises = [];
+
+      if(checkReadersMatch(/ICLR.cc\/2018\/Conference\/Paper[0-9]+\/Authors_and_Higher/) ||
+        checkReadersMatch(/everyone/)) {
+        promises.push(or3client.or3request(or3client.mailUrl, author_mail, 'POST', token));
+        promises.push(or3client.or3request(or3client.mailUrl, reviewer_mail, 'POST', token));
+        promises.push(or3client.or3request(or3client.mailUrl, ac_mail, 'POST', token));
+      } else if(checkReadersMatch(/ICLR.cc\/2018\/Conference\/Paper[0-9]+\/Reviewers_and_Higher/)){
+        promises.push(or3client.or3request(or3client.mailUrl, reviewer_mail, 'POST', token));
+        promises.push(or3client.or3request(or3client.mailUrl, ac_mail, 'POST', token));
+      } else if(checkReadersMatch(/ICLR.cc\/2018\/Conference\/Paper[0-9]+\/Area_Chairs_and_Higher/)){
+        promises.push(or3client.or3request(or3client.mailUrl, ac_mail, 'POST', token));
+      } else if(note.readers.indexOf('ICLR.cc/2018/Conference/Program_Chairs') > -1){
+        promises.push(or3client.or3request(or3client.mailUrl, pc_mail, 'POST', token));
+      }
+
+      return Promise.all(promises);
+    })
+    .then(result => done())
+    .catch(error => done(error));
+
+    return true;
+  };
---
+++ venues/ICLR.cc/2019/webfield/recruitResponseWebfield.js
@@ -0,0 +1,50 @@
+// Constants
+var CONFERENCE_ID = 'ICLR.cc/2019/Conference'
+var HEADER = {
+  title: 'ICLR 2019',
+  subtitle: 'International Conference on Machine Learning',
+  location: 'Vancouver Convention Center, Vancouver, BC, Canada',
+  date: 'April 30 - May 3, 2019',
+  website: 'http://www.ICLR.cc',
+  deadline: 'Submission Deadline: 5:00pm Eastern Standard Time, October 27, 2017'
+}
+
+// Main is the entry point to the webfield code and runs everything
+function main() {
+  Webfield.ui.setup('#invitation-container', CONFERENCE_ID);  // required
+  renderConferenceHeader();
+  render();
+}
+
+// RenderConferenceHeader renders the static info at the top of the page. Since that content
+// never changes, put it in its own function
+function renderConferenceHeader() {
+  Webfield.ui.venueHeader(HEADER);
+
+  Webfield.ui.spinner('#notes');
+}
+
+function render() {
+  var accepted = args.response === 'Yes';
+  var message = accepted ?
+    'Thank you for accepting the invitation!' :
+    'You have declined the invitation.';
+
+  var $response = $('#notes');
+  $response.empty().append('<div class="panel"><div class="row"><strong>' + message + '</strong></div></div>');
+
+  if (accepted) {
+    // Display response text
+    $response.append([
+      '<div class="panel">',
+        '<div class="row">',
+          '<p>If you do not already have an OpenReview account, please sign up <a href="/signup">here</a>.</p>',
+          '<p>If you have an existing OpenReview account, please ensure that the email address that received this invitation is linked to your <a href="/profile?mode=edit">profile page</a> and has been confirmed.</p>',
+        '</div>',
+      '</div>'
+    ].join('\n'));
+  }
+}
+
+// Go!
+main();
