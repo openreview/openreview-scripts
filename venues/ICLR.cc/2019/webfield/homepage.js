3b52d933871f53a2710ce79cd2c26ae67ef792b2
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-08-20 12:51:00 -0400

    Fix/iclr19webfield (#744)
    * Commented spinner and other UI elements that are not needed right now
    
    * Updates for iclr19

--- venues/ICLR.cc/2019/webfield/homepage.js
+++ venues/ICLR.cc/2019/webfield/homepage.js
@@ -159,9 +159,9 @@ function main() {
 
   renderSubmissionButton();
 
-  renderConferenceTabs();
+  //renderConferenceTabs();
 
-  load().then(renderContent);
+  //load().then(renderContent);
 }
 
 // Load makes all the API calls needed to get the data to render the page
@@ -203,7 +203,7 @@ function load() {
 function renderConferenceHeader() {
   Webfield.ui.venueHeader(HEADER);
 
-  Webfield.ui.spinner('#notes');
+  //Webfield.ui.spinner('#notes');
 }
 
 function renderSubmissionButton() {
@@ -212,11 +212,11 @@ function renderSubmissionButton() {
       Webfield.ui.submissionButton(invitation, user, {
         onNoteCreated: function() {
           // Callback funtion to be run when a paper has successfully been submitted (required)
-          promptMessage('Your submission is complete. The list of all current submissions is shown below.');
+          promptMessage('Your submission is complete. Check your inbox for a confirmation email. A list of all submissions will be available after the deadline');
 
-          load().then(renderContent).then(function() {
-            $('.tabs-container a[href="#all-submissions"]').click();
-          });
+          // load().then(renderContent).then(function() {
+          //   $('.tabs-container a[href="#all-submissions"]').click();
+          // });x
         }
       });
     });
