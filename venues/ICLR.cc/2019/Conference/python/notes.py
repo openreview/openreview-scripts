75bfaa16a93532efe45fcf77cd7406022b5ffd2c
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-09-18 13:43:29 -0400

    Adding notes.py and post-submission-stage script

---
+++ venues/ICLR.cc/2019/Conference/python/notes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+"""
+A script for managing groups with parameters that depend on the papers to which
+they are attached.
+
+Usage:
+
+python groups.py Reviewers
+python groups.py Reviewers --overwrite
+"""
+
+import openreview
+import argparse
+import iclr19 as ICLR
+
+def getBibtex(client, note):
+    firstWord = note.content["title"].split(' ')[0].lower();
+    url = client.baseurl + '/forums?id=' + note.forum
+    return '@article{\
+    \nanonymous2019' + firstWord + ',\
+    \ntitle={' + note.content["title"] + '},\
+    \nauthor={Anonymous},\
+    \njournal={International Conference on Learning Representations},\
+    \nyear={2019},\
+    \nurl={' + url + '}\
+    \n}'
+
+def create_blind_note(note):
+    return openreview.Note(
+        original= note.id,
+        invitation= ICLR.BLIND_SUBMISSION_ID,
+        forum= None,
+        signatures= [ICLR.CONFERENCE_ID],
+        writers= [ICLR.CONFERENCE_ID],
+        readers= ['everyone'],
+        content= {
+            "authors": ['Anonymous'],
+            "authorids": [],
+            "_bibtex": None
+        })
+
+def freeze_note(note, writers=[iclr19.CONFERENCE_ID]):
+    note.writers = writers
+    return note
+
+if __name__ == '__main__':
+    # Argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument('notes', nargs='*', help="any of the following: " + ", ".join(group_templates.keys()))
+    parser.add_argument('--overwrite', action='store_true', help="if present, overwrites the members of the groups")
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
---
+++ venues/ICLR.cc/2019/Conference/python/post-submissions-stage.py
@@ -0,0 +1,38 @@
+import openreview
+import iclr19
+import notes
+import groups
+import invitations
+
+if __name__ == '__main__':
+    ## Argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument('invitations', nargs='*', help="any of the following: " + ", ".join(invitation_templates.keys()))
+    parser.add_argument('--disable', action='store_true', help='if present, disables the given invitation')
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+    client.post_invitation(iclr19.blind_submission_inv)
+
+    original_notes = openreview.tools.iterget_notes(client,invitation = iclr19.submission_inv.id)
+
+    for original_note in original_notes:
+        blind_note = notes.create_blind_note(original_note)
+    
+        #create paper and paper-author group
+        paper_group = groups.create_paper_group(blind_note)
+        author_group = groups.create_paper_author_group(blind_note)
+
+        blind_note.content["authorids"] = [author_group.id]
+        blind_note.content["_bibtex"] = notes.getBibtex(blind_note)
+
+        client.post_note(blind_note)
+
+        client.post_note(notes.freeze_note(original_note))
+
+
+
