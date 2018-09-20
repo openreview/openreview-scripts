bbafdc55b10e15c358c9110aadac9f840ced927e
Zach Bialecki <zbialecki@users.noreply.github.com>
2018-09-20 12:43:26 -0400

    Disable search for no bid tab (#782)
    * Disable search for no bid tab
    
    * Updating No Bids tab header
    
    * Format instructions to match other webfields

--- venues/ICLR.cc/2019/Conference/webfield/bidWebfield.js
+++ venues/ICLR.cc/2019/Conference/webfield/bidWebfield.js
@@ -9,16 +9,17 @@ var METADATA_INVITATION_ID = CONFERENCE_ID + '/-/Paper_Metadata';
 var ADD_BID = CONFERENCE_ID + '/-/Add_Bid';
 var PAGE_SIZE = 1000;
 
-var INSTRUCTIONS =  '\
-<strong>Please indicate your level of interest in reviewing the submitted papers below, on a scale from "Very Low" to "Very High".</strong><br><br>\
-<p>\
-  <em>A few tips:</em>\
+var INSTRUCTIONS = '\<p class="dark">Please indicate your level of interest in reviewing \
+the submitted papers below, on a scale from "Very Low" to "Very High".</p>\
+<p class="dark">\
+  <strong>A few tips:</strong>\
   <ul>\
     <li>We expect <strong>approximately 50 bids per user</strong>. Please bid on as many papers as possible to ensure that your preferences are taken into account.</li>\
     <li>You may search for papers by keyword or subject area filter.</li>\
     <li>Don\'t worry about suspected conflicts of interest during the bidding process. These will be accounted for during the paper matching process.</li>\
-    <li>Default bid on each paper is \"No bid\"</li>\
-</p><br>'
+    <li>Default bid on each paper is \"No Bid\"</li>\
+  </ul>\
+</p>'
 
 // Main is the entry point to the webfield code and runs everything
 function main() {
@@ -75,7 +76,7 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
     if (!updatedNote) {
       return;
     }
-    var prevVal = _.has(updatedNote.details, 'tags[0].tag') ? updatedNote.details.tags[0].tag : 'No bid';
+    var prevVal = _.has(updatedNote.details, 'tags[0].tag') ? updatedNote.details.tags[0].tag : 'No Bid';
     updatedNote.details.tags[0] = tagObj;
 
     var tagToElemId = {
@@ -84,7 +85,7 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
       'Neutral': '#neutral',
       'Low': '#low',
       'Very Low': '#veryLow',
-      'No bid': '#noBid'
+      'No Bid': '#noBid'
     };
 
     var $sourceContainer = $(tagToElemId[prevVal] + ' .submissions-list');
@@ -147,8 +148,8 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
 
     var bidCount = veryHigh.length + high.length + neutral.length + low.length + veryLow.length;
 
-    $('#header > #bidcount').remove();
-    $('#header').append('<h4 id=bidcount>You have completed ' + bidCount + ' bids</h4>');
+    $('#bidcount').remove();
+    $('#header').append('<h4 id="bidcount">You have completed ' + bidCount + ' bids</h4>');
 
     var sections = [
       {
@@ -157,7 +158,7 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
         content: null
       },
       {
-        heading: 'No bid',
+        heading: 'No Bid',
         headingCount: noBid.length,
         id: 'noBid',
         content: null
@@ -253,7 +254,7 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
     Webfield.ui.submissionList(noBid, {
       heading: null,
       container: '#noBid',
-      search: { enabled: true },
+      search: { enabled: false },
       displayOptions: paperDisplayOptions,
       fadeIn: false
     });
@@ -347,8 +348,8 @@ function renderContent(validNotes, tagInvitations, metadataNotesMap) {
       }
     });
 
-    $('#header > #bidcount').remove();
-    $('#header').append('<h4 id=bidcount>You have completed ' + totalCount + ' bids</h4>');
+    $('#bidcount').remove();
+    $('#header').append('<h4 id="bidcount">You have completed ' + totalCount + ' bids</h4>');
   }
 
   updateNotes(validNotes);
