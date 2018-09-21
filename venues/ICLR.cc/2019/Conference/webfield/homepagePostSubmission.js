40c5ba1f6fe07eed4a9d48f34c50927f72c33406
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-09-20 22:47:40 -0400

    Update homepagePostSubmission.js

--- venues/ICLR.cc/2019/Conference/webfield/homepagePostSubmission.js
+++ venues/ICLR.cc/2019/Conference/webfield/homepagePostSubmission.js
@@ -40,7 +40,7 @@ var HEADER = {
     <a href="mailto:info@openreview.net">info@openreview.net</a> with any questions or concerns about the OpenReview platform.<br/>\
     Please contact the ICLR 2019 Program Chairs at \
     <a href="mailto:iclr2019programchairs@googlegroups.com">iclr2019programchairs@googlegroups.com</a> with any questions or concerns about conference administration or policy.\
-    </p>'
+    </p>',
   deadline: 'Submission Deadline: 6:00 pm Eastern Standard Time, September 27, 2018'
 }
 
