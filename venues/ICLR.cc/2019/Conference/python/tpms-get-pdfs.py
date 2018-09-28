a8844122aa3ba389bb0e2601194ff1cab4578031
Michael Spector <michaelspector@gmail.com>
2018-09-28 15:22:10 -0400

    remove openreview dependency

--- venues/ICLR.cc/2019/Conference/python/tpms-get-pdfs.py
+++ venues/ICLR.cc/2019/Conference/python/tpms-get-pdfs.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import requests
-import openreview
 
 
 '''
@@ -8,16 +7,28 @@ Modify the out_dir variable
 '''
 out_dir = '../data/iclr19_pdfs'
 
-client = openreview.Client(baseurl='https://openreview.net')
 invitation_id = 'ICLR.cc/2019/Conference/-/Blind_Submission'
 
-submissions = openreview.tools.iterget_notes(client, invitation=invitation_id)
+limit = 1000
+offset = 0
+done = False
 
-for n in submissions:
-    pdf_url = 'https://openreview.net{0}'.format(n.content['pdf'])
-    paper_id = n.number
-    print("retrieving paper{0} at {1}".format(paper_id, pdf_url))
-    pdf_response = requests.get(pdf_url, stream=True)
-    with open('{}/paper{}.pdf'.format(out_dir, paper_id), 'wb') as f:
-        f.write(pdf_response.content)
+while not done:
+	submissions_response = requests.get(
+		'https://openreview.net/notes?invitation={}'.format(invitation_id),
+		params={'limit': limit, 'offset': offset})
+	submissions = submissions_response.json()['notes']
+
+	if len(submissions) < limit:
+		done = True
+	else:
+		offset += limit
+
+	for n in submissions:
+	    pdf_url = 'https://openreview.net{0}'.format(n['content']['pdf'])
+	    paper_id = n['number']
+	    print("retrieving paper{0} at {1}".format(paper_id, pdf_url))
+	    pdf_response = requests.get(pdf_url, stream=True)
+	    with open('{}/paper{}.pdf'.format(out_dir, paper_id), 'wb') as f:
+	        f.write(pdf_response.content)
 
