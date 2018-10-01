cac53d42be70f9bf50b28e7e420e628e3f0b5544
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-10-01 17:17:07 -0400

    Fix/update duedate reviewer questionnaire (#818)
    * updated reviewer questionnaire due and expiry dates
    
    * Added new vars for questionnaire due and exp

--- venues/ICLR.cc/2019/Conference/python/iclr19.py
+++ venues/ICLR.cc/2019/Conference/python/iclr19.py
@@ -141,6 +141,8 @@ SUBMISSION_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=9, day=27,
 BLIND_SUBMISSION_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=10, day=10, hour=9)
 ADD_BID_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=10, day=6, hour=0)
 OFFICIAL_REVIEW_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=10, day=29)
+QUESTIONNAIRE_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=10, day=2, hour=22)
+QUESTIONNAIRE_EXPIRY = openreview.tools.timestamp_GMT(year=2018, month=10, day=20)
 META_REVIEW_DEADLINE = openreview.tools.timestamp_GMT(year=2018, month=11, day=7)
 
 
@@ -336,6 +338,8 @@ questionnaire_instructions_invitation = openreview.Invitation.from_json({
     'writers': [CONFERENCE_ID],
     'signatures': [CONFERENCE_ID],
     'invitees': [CONFERENCE_ID],
+    'duedate': QUESTIONNAIRE_DEADLINE,
+    'expdate': QUESTIONNAIRE_EXPIRY,
     'reply': {
         'forum': None,
         'replyto': None,
@@ -369,8 +373,8 @@ questionnaire_response_template = {
     'writers': [CONFERENCE_ID],
     'signatures': [CONFERENCE_ID],
     'invitees': [REVIEWERS_ID],
-    'duedate': OFFICIAL_REVIEW_DEADLINE,
-    'expdate': OFFICIAL_REVIEW_DEADLINE,
+    'duedate': QUESTIONNAIRE_DEADLINE,
+    'expdate': QUESTIONNAIRE_EXPIRY,
     'reply': {
         'forum': '<forum>',
         'replyto': '<forum>',
