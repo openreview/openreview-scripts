c430fbc40efeb7e80e73f0450efd33a8ff14e6d1
mspector <michael.l.spector@gmail.com>
2018-10-10 16:41:39 -0400

    update reviewing-stage.py and add assign.py. also update readme (#839)
    * update reviewing-stage.py and add assign.py. also update readme
    
    * cleanup

--- venues/ICLR.cc/2019/Conference/python/reviewing-stage.py
+++ venues/ICLR.cc/2019/Conference/python/reviewing-stage.py
@@ -27,65 +27,16 @@ from collections import defaultdict
 if __name__ == '__main__':
     ## Argument handling
     parser = argparse.ArgumentParser()
-    parser.add_argument('--reviewer_label', required=True)
-    parser.add_argument('--areachair_label', required=True)
     parser.add_argument('--baseurl', help="base url")
     parser.add_argument('--username')
     parser.add_argument('--password')
     args = parser.parse_args()
 
+    assert args.type in ['areachairs','reviewers'], 'input "type" must be either "areachairs" or "reviewers"'
+
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
     blind_submissions = openreview.tools.iterget_notes(client, invitation=iclr19.BLIND_SUBMISSION_ID)
-
-    assignment_notes = openreview.tools.iterget_notes(client,
-        invitation=iclr19.ASSIGNMENT_INV_ID,
-        details='forumContent')
-
-    def get_number_from_details(note_details):
-        '''
-        The forum content in the "details" field doesn't have the actual paper number,
-        so we have to infer it from the group in authorids.
-        '''
-
-        authorids = note_details['forumContent']['authorids']
-        assert len(authorids) == 1, 'something went wrong: no authorids in paper with title {}'.format(
-            note_details['forumContent']['title'])
-        paper_authors_id = authorids[0]
-        authorgroup_components = paper_authors_id.split('/')
-        paper_num = authorgroup_components[3]
-        num = int(paper_num.split('Paper')[1])
-        return num
-
-    anonreviewerids_by_forum = defaultdict(list)
-
-    for assignment_note in assignment_notes:
-        paper_number = get_number_from_details(assignment_note.details)
-        assignment_entries = assignment_note.content['assignedGroups']
-
-        if args.reviewer_label == assignment_note.content['label']:
-            parent_label = 'Reviewers'
-            individual_label = 'AnonReviewer'
-            individual_group_params = {'readers': [
-                iclr19.AREA_CHAIRS_ID,
-                iclr19.PROGRAM_CHAIRS_ID
-            ]}
-        if args.areachair_label == assignment_note.content['label']:
-            parent_label = 'Area_Chairs'
-            individual_label = 'Area_Chair'
-            individual_group_params = {'readers': [
-                iclr19.PROGRAM_CHAIRS_ID
-            ]}
-
-        for entry in assignment_entries:
-            new_assigned_group = openreview.tools.add_assignment(
-                client, paper_number, iclr19.CONFERENCE_ID, entry['userId'],
-                parent_label = parent_label,
-                individual_label = individual_label,
-                individual_group_params = individual_group_params)
-
-            if 'AnonReviewer' in new_assigned_group.id:
-                anonreviewerids_by_forum[assignment_note.forum].append(new_assigned_group.id)
 
     for blind_note in blind_submissions:
         client.post_invitation(invitations.enable_invitation('Official_Review', blind_note))
