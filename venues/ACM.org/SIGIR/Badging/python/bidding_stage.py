ac00579732e3b7107ff9f83e8eda7ebd62788142
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-10-25 08:20:07 -0400

    acm updates

---
+++ venues/ACM.org/SIGIR/Badging/python/bidding_stage.py
@@ -0,0 +1,98 @@
+'''
+Bidding Stage (Oct 2 - Oct 5)
+
+- Bidding Task / interface enabled and added to the Reviewer Console
+- Reviewers bid on papers.
+- Area chairs bid on papers.
+
+'''
+
+import openreview
+import acm19 as conference_config
+import notes
+import groups
+import invitations
+import argparse
+import random
+import csv
+
+if __name__ == '__main__':
+    ## Argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument('scores_file')
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+    # read in TPMS scores
+    paper_scores_by_number = {}
+    with open(args.scores_file) as f:
+        for row in csv.reader(f):
+            paper_number = int(row[0])
+            profile_id = row[1]
+            score = row[2]
+            if paper_number not in paper_scores_by_number:
+                paper_scores_by_number[paper_number] = {}
+            paper_scores_by_number[paper_number][profile_id] = score
+
+    with open('../webfield/reviewerWebfieldBiddingEnabled.js','r') as f:
+        reviewers = client.get_group(conference_config.REVIEWERS_ID)
+        reviewers.web = f.read()
+        reviewers = client.post_group(reviewers)
+
+    with open('../webfield/areachairWebfieldBiddingEnabled.js','r') as f:
+        area_chairs = client.get_group(conference_config.AREA_CHAIRS_ID)
+        area_chairs.web = f.read()
+        area_chairs = client.post_group(area_chairs)
+
+    conference_config.add_bid.invitees = [conference_config.REVIEWERS_ID, conference_config.AREA_CHAIRS_ID]
+    client.post_invitation(conference_config.add_bid)
+
+    # set up User Score notes
+
+    if not all(['~' in member for member in reviewers.members]):
+        print('WARNING: not all reviewers have been converted to profile IDs. Members without profiles will not have metadata created.')
+    valid_reviewer_ids = [r for r in reviewers.members if '~' in r]
+
+    if not all(['~' in member for member in area_chairs.members]):
+        print('WARNING: not all area chairs have been converted to profile IDs. Members without profiles will not have metadata created.')
+    valid_ac_ids = [r for r in area_chairs.members if '~' in r]
+
+    client.post_invitation(conference_config.scores_inv)
+    user_score_notes = {}
+    for paper in openreview.tools.iterget_notes(client, invitation=conference_config.BLIND_SUBMISSION_ID):
+        for user_id in valid_reviewer_ids + valid_ac_ids:
+            if user_id not in user_score_notes:
+                user_score_notes[user_id] = openreview.Note.from_json({
+                    'invitation': conference_config.SCORES_INV_ID,
+                    'readers': [user_id],
+                    'writers': ['AKBC.ws/2019/Conference'],
+                    'content': {
+                        'user': user_id,
+                        'scores': []
+                    },
+                    'signatures': ['AKBC.ws/2019/Conference'],
+                    'forum': None,
+                    'multiReply': False
+                })
+            score_note = user_score_notes[user_id]
+
+            score_entry = {
+                'forum': paper.forum,
+                'tpmsScore': float(paper_scores_by_number[paper.number].get(user_id,0.0)),
+                # 'conflict': 'cs.umass.edu'
+            }
+
+            score_note.content['scores'].append(score_entry)
+
+    for user_id, score_note in user_score_notes.items():
+        print('posting score note for user {}'.format(user_id))
+        client.post_note(score_note)
+
+
+
+
+
---
+++ venues/ACM.org/SIGIR/Badging/python/review-assignment-stage.py
@@ -0,0 +1,127 @@
+'''
+Reviewer Assignment Stage:
+- Bidding Task / interface is closed.
+- Initial paper-reviewer and paper-areachair matches are computed.
+- Program chairs browse match using OpenReview Matching System and make hand-adjustments as needed.
+- Paper-reviewer and paper-areachair assignments are finalized and deployed.
+
+'''
+
+import argparse
+import openreview
+import acm19 as conference_config
+import invitations
+import notes
+import groups
+import time
+import json
+import csv
+import matcher
+
+def clear(client, invitation):
+    note_list = list(openreview.tools.iterget_notes(client, invitation = invitation))
+    for note in note_list:
+        client.delete_note(note)
+
+def split_reviewers_by_experience(client, reviewers_group):
+    questionnaire_responses = openreview.tools.iterget_notes(
+        client, invitation='AKBC.ws/2019/Conference/-/Reviewer_Questionnaire_Response')
+
+    experience_by_signature = {r.signatures[0]: r.content['Reviewing Experience'] for r in questionnaire_responses}
+
+    senior_reviewers = []
+    junior_reviewers = []
+
+    valid_reviewer_ids = [r for r in reviewers_group.members if '~' in r]
+    for reviewer in valid_reviewer_ids:
+        experience = experience_by_signature.get(reviewer, '')
+        if experience in [
+            '5-10 times  - active community citizen',
+            '10+ times  - seasoned reviewer'
+        ]:
+            senior_reviewers.append(reviewer)
+        else:
+            junior_reviewers.append(reviewer)
+
+    return senior_reviewers, junior_reviewers
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('scores_file')
+    parser.add_argument('-c','--constraints_file')
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+    print('clearing previous metadata, assignments, and configs...')
+    clear(client, conference_config.METADATA_INV_ID)
+    clear(client, conference_config.ASSIGNMENT_INV_ID)
+    clear(client, conference_config.CONFIG_INV_ID)
+
+    # TODO: update the reviewer and AC consoles to indicate that the bidding phase is over
+
+    blind_submissions = openreview.tools.iterget_notes(client,
+        invitation=conference_config.BLIND_SUBMISSION_ID,
+        details='original,tags')
+
+    # At this point, all reviewers & ACs should have been
+    # converted to profile IDs and deduplicated.
+    reviewers_group = client.get_group(conference_config.REVIEWERS_ID)
+    if not all(['~' in member for member in reviewers_group.members]):
+        print('WARNING: not all reviewers have been converted to profile IDs. Members without profiles will not have metadata created.')
+    valid_reviewer_ids = [r for r in reviewers_group.members if '~' in r]
+
+    reviewer_profiles = client.get_profiles(valid_reviewer_ids)
+
+    areachairs_group = client.get_group(conference_config.AREA_CHAIRS_ID)
+    if not all(['~' in member for member in areachairs_group.members]):
+        print('WARNING: not all area chairs have been converted to profile IDs. Members without profiles will not have metadata created.')
+    valid_ac_ids = [r for r in areachairs_group.members if '~' in r]
+
+    ac_profiles = client.get_profiles(valid_ac_ids)
+
+    user_profiles = ac_profiles + reviewer_profiles
+
+    invitations.disable_bids(client)
+    groups.update_AC_console(client, '../webfield/areachairWebfieldReviewerAssignmentStage.js')
+    groups.update_Reviewer_console(client, '../webfield/reviewerWebfieldReviewerAssignmentStage.js')
+
+    # create metadata
+    metadata_inv = client.post_invitation(conference_config.metadata_inv)
+    config_inv = client.post_invitation(conference_config.config_inv)
+    assignment_inv = client.post_invitation(conference_config.assignment_inv)
+
+    # read in TPMS scores
+    paper_scores_by_number = {}
+    with open(args.scores_file) as f:
+        for row in csv.reader(f):
+            paper_number = int(row[0])
+            profile_id = row[1]
+            score = row[2]
+            if paper_number not in paper_scores_by_number:
+                paper_scores_by_number[paper_number] = {}
+            paper_scores_by_number[paper_number][profile_id] = score
+
+    # read in manual conflicts
+    # manual_conflicts_by_id is keyed on tilde IDs, and values are each a list of domains.
+    manual_conflicts_by_id = {}
+    if args.constraints_file:
+        with open(args.constraints_file) as f:
+            for row in csv.reader(f):
+                id = row[0]
+                conflicts = row[1:]
+                manual_conflicts_by_id[id] = conflicts
+
+    for blind_note in blind_submissions:
+        paper_tpms_scores = paper_scores_by_number[blind_note.number]
+        new_metadata_note = notes.post_metadata_note(client, blind_note, user_profiles, metadata_inv, paper_tpms_scores, manual_conflicts_by_id)
+
+    senior_reviewer_ids, junior_reviewer_ids = split_reviewers_by_experience(client, reviewers_group)
+    conference_config.senior_reviewers.members = senior_reviewer_ids
+    conference_config.junior_reviewers.members = junior_reviewer_ids
+    client.post_group(conference_config.senior_reviewers)
+    client.post_group(conference_config.junior_reviewers)
+
