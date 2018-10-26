b79917b2d19580ff0420c3c41c2d890ab1dc9d0c
Zach Bialecki <zach.bialecki@gmail.com>
2018-10-25 21:02:48 -0400

    Update group landing page webfields to be compatible

---
+++ venues/auai.org/UAI/UAI-landing-webfield.js
@@ -0,0 +1,17 @@
+var GROUP_ID = 'auai.org/UAI';
+var HEADER = {
+  title: 'Conference on Uncertainty in Artificial Intelligence',
+  description: ''
+};
+var VENUE_LINKS = [
+  { url: '/group?id=auai.org/UAI/2018', name: 'UAI 2018' },
+  { url: '/group?id=auai.org/UAI/2017', name: 'UAI 2017' }
+];
+
+Webfield.ui.setup('#group-container', GROUP_ID);
+
+Webfield.ui.header(HEADER.title, HEADER.description, { underline: true });
+
+Webfield.ui.linksList(VENUE_LINKS);
+
+OpenBanner.welcome();
