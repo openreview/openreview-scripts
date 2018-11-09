0b27d469f043e3f5043b9e4900be5c03d15d78b6
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-11-09 15:22:44 -0500

    Alternative-give PCs conf level admin rights

--- venues/AKBC.ws/2019/Conference/python/akbc19.py
+++ venues/AKBC.ws/2019/Conference/python/akbc19.py
@@ -150,7 +150,7 @@ conference = openreview.Group.from_json({
     'writers': [CONFERENCE_ID],
     'signatures': ["OpenReview.net"],
     'signatories': [CONFERENCE_ID],
-    'members': []
+    'members': [PROGRAM_CHAIRS_ID]
 })
 with open(os.path.abspath('../webfield/homepage.js')) as f:
     conference.web = f.read()
@@ -245,7 +245,6 @@ submission_inv = invitations.Submission(
         'readers': {
             'values-copied': [
                 CONFERENCE_ID,
-                PROGRAM_CHAIRS_ID,
                 '{content.authorids}',
                 '{signatures}'
             ]
