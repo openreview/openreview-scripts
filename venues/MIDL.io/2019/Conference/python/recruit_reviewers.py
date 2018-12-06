448c6555431782f175127bb2cce9e4b5c2d62969
Melisa Bok <melisabok@gmail.com>
2018-12-06 09:37:30 -0500

    move the builder to the config file

--- venues/MIDL.io/2019/Conference/python/recruit_reviewers.py
+++ venues/MIDL.io/2019/Conference/python/recruit_reviewers.py
@@ -13,20 +13,7 @@ args = parser.parse_args()
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 print('connecting to {0}'.format(client.baseurl))
 
-builder = openreview.conference.ConferenceBuilder(client)
-builder.set_conference_id(config.CONFERENCE_ID)
-builder.set_conference_name('Medical Imaging with Deep Learning')
-builder.set_homepage_header({
-'title': 'Medical Imaging with Deep Learning',
-'subtitle': 'MIDL 2019 Conference',
-'deadline': 'Submission Deadline: 13th of December, 2018',
-'date': '8-10 July 2019',
-'website': 'http://2019.midl.io',
-'location': 'London',
-'instructions': 'Full papers contain well-validated applications or methodological developments of deep learning algorithms in medical imaging. There is no strict limit on paper length. However, we strongly recommend keeping full papers at 8 pages (excluding references and acknowledgements). An appendix section can be added if needed with additional details but must be compiled into a single pdf. The appropriateness of using pages over the recommended page length will be judged by reviewers. All accepted papers will be presented as posters with a selection of these papers will also be invited for oral presentation.<br/><br/> <p><strong>Questions or Concerns</strong></p><p>Please contact the OpenReview support team at <a href=\"mailto:info@openreview.net\">info@openreview.net</a> with any questions or concerns about the OpenReview platform.<br/>    Please contact the MIDL 2019 Program Chairs at <a href=\"mailto:program-chairs@midl.io\">program-chairs@midl.io</a> with any questions or concerns about conference administration or policy.</p><p>We are aware that some email providers inadequately filter emails coming from openreview.net as spam so please check your spam folder regularly.</p>'
-})
-builder.set_conference_submission_name('Full_Submission')
-conference = builder.get_result()
+conference = config.get_conference(client)
 
 formatted_acs = []
 
