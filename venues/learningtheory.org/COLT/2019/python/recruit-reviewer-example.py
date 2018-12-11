090671c01565eda22668bd5b092eb96c066c839c
Melisa Bok <melisabok@gmail.com>
2018-12-11 12:33:06 -0500

    refactor using the builder

--- venues/learningtheory.org/COLT/2019/python/recruit-reviewer-example.py
+++ venues/learningtheory.org/COLT/2019/python/recruit-reviewer-example.py
@@ -1,4 +1,4 @@
-import colt19
+import config
 import argparse
 import openreview
 import datetime
@@ -14,82 +14,52 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
-    # Create super invitation
-    recruit_invitation = openreview.Invitation(id = 'learningtheory.org/COLT/2019/Conference/-/Recruit_Reviewers',
-                                            readers = ['everyone'],
-                                            writers = ['learningtheory.org/COLT/2019/Conference'],
-                                            signatures = ['learningtheory.org/COLT/2019/Conference'],
-                                            process = '../process/recruitReviewersProcess.js',
-                                            web = '../webfield/recruitResponseWebfield.js',
-                                            reply = {
-                                                'forum': None,
-                                                'replyto': None,
-                                                'readers': {
-                                                    'values': ['~Super_User1']
-                                                },
-                                                'signatures': {
-                                                    'values-regex': '\\(anonymous\\)'
-                                                },
-                                                'writers': {
-                                                    'values': []
-                                                },
-                                                'content': {
-                                                    'title': {
-                                                        'order': 1,
-                                                        'value': 'Invitation to review response'
-                                                    },
-                                                    'email': {
-                                                        'description': 'email address',
-                                                        'order': 2,
-                                                        'value-regex': '.*@.*',
-                                                        'required':True
-                                                    },
-                                                    'key': {
-                                                        'description': 'Email key hash',
-                                                        'order': 3,
-                                                        'value-regex': '.{0,100}',
-                                                        'required':True
-                                                    },
-                                                    'response': {
-                                                        'description': 'Invitation response',
-                                                        'order': 4,
-                                                        'value-radio': ['Yes', 'No'],
-                                                        'required':True
-                                                    }
-                                                }
-        })
-    recruit_invitation = client.post_invitation(recruit_invitation)
+    conference = config.get_conference(client)
 
-    #Get the first submission, SUBMIT A PAPER FIRST!
-    paper = client.get_notes(invitation = 'learningtheory.org/COLT/2019/Conference/-/Submission', limit = 1)[0]
+    program_committee = 'spector@cs.umass.edu'
 
-    sub_recruit_invitation = openreview.Invitation(id = 'learningtheory.org/COLT/2019/Conference/-/Paper' + str(paper.number) + '/Recruit_Reviewers',
-                                                super = recruit_invitation.id,
-                                                reply = {
-                                                        'forum' : paper.id
-                                                    }
-                                                )
-    sub_recruit_invitation = client.post_invitation(sub_recruit_invitation)
+    # Create paper groups and assign them to a program committee
+    for n in openreview.tools.iterget_notes(client, invitation = conference.get_submission_id()):
+        pc_group_id = "{conference_id}/Paper{number}/Program_Committee".format(conference_id = conference.get_id(), number = n.number)
+        group_id = "{conference_id}/Paper{number}/Reviewers".format(conference_id = conference.get_id(), number = n.number)
+        group_invited_id = "{conference_id}/Paper{number}/Reviewers/Invited".format(conference_id = conference.get_id(), number = n.number)
+        group_declined_id = "{conference_id}/Paper{number}/Reviewers/Declined".format(conference_id = conference.get_id(), number = n.number)
+        client.post_group(openreview.Group(id = group_id,
+                                        readers = [group_id, conference.get_id(), pc_group_id],
+                                        writers = [conference.get_id(), pc_group_id],
+                                        signatures = [conference.get_id()],
+                                        signatories = [group_id]))
+        client.post_group(openreview.Group(id = group_invited_id,
+                                        readers = [conference.get_id(), pc_group_id],
+                                        writers = [conference.get_id(), pc_group_id],
+                                        signatures = [conference.get_id()],
+                                        signatories = [conference.get_id()]))
+        client.post_group(openreview.Group(id = group_declined_id,
+                                        readers = [conference.get_id(), pc_group_id],
+                                        writers = [conference.get_id(), pc_group_id],
+                                        signatures = [conference.get_id()],
+                                        signatories = [conference.get_id()]))
 
-    #Create the necessary group, should we create them in the process function?
-    paper_group = client.post_group(openreview.Group(id = 'learningtheory.org/COLT/2019/Conference/Paper' + str(paper.number),
-    readers = ['learningtheory.org/COLT/2019/Conference'],
-    writers = ['learningtheory.org/COLT/2019/Conference'],
-    signatures = ['learningtheory.org/COLT/2019/Conference'],
-    signatories = ['learningtheory.org/COLT/2019/Conference/Paper' + str(paper.number)]))
-
-    pc_group = client.post_group(openreview.Group(id = paper_group.id + '/Program_Committee',
-    readers = ['everyone'],
-    writers = ['learningtheory.org/COLT/2019/Conference'],
-    signatures = ['learningtheory.org/COLT/2019/Conference'],
-    signatories = [paper_group.id + '/Program_Committee']))
-
-    pc_declined_group = client.post_group(openreview.Group(id = pc_group.id + '/Declined',
-    readers = ['everyone'],
-    writers = ['learningtheory.org/COLT/2019/Conference'],
-    signatures = ['learningtheory.org/COLT/2019/Conference'],
-    signatories = [pc_group.id + '/Declined']))
-
-    hashkey = HMAC.new(hash_seed.encode('utf-8'), msg='mbok@mail.com'.encode('utf-8'), digestmod=SHA256).hexdigest()
-    print('http://localhost:3000/invitation?id=' + sub_recruit_invitation.id + '&email=mbok@mail.com&key=' + hashkey + '&response=Yes')
+        openreview.tools.assign(client=client,
+                                conference=conference.get_id(),
+                                paper_number=n.number,
+                                reviewer_to_add=program_committee,
+                                parent_label = 'Program_Committees',
+                                individual_label = 'Program_Committee',
+                                individual_group_params = {
+                                    'readers': [
+                                        conference.get_id(),
+                                        conference.get_program_chairs_id(),
+                                        'learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(n.number)
+                                    ],
+                                    'signatories': ['learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(n.number)]
+                                    },
+                            parent_group_params = {
+                                    'readers': [
+                                        conference.get_id(),
+                                        conference.get_program_chairs_id(),
+                                        'learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committees'.format(n.number)
+                                    ],
+                                    'signatories': ['learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committees'.format(n.number)]
+                                    })
     print('DONE.')
