cf62f957c24157e31b2c28ee611bd8883046bd6d
pmandler <mandler@cs.umass.edu>
2019-01-16 10:54:11 -0500

    Feature/iclr deep gen struct (#928)
    * NIPS IRASL update duedates
    
    * Initial checkout for double blind workshop ICLR DeepGenStruct
    
    * ICRL DeepGenStruct after Workshop directory update
    
    * Delete submissionProcess.js
    
    In Workshops
    
    * Delete config.py
    
    In Workshops
    
    * Delete conferenceWebfield.js
    
    In Workshops
    
    * Delete init_conference.py
    
    In Workshops

---
+++ venues/ICLR.cc/2019/Workshop/DeepGenStruct/process/submissionProcess.js
@@ -0,0 +1,77 @@
+function () {
+  var or3client = lib.or3client;
+
+  //<CONSTANTS>
+  var TITLE = 'ICLR DeepGenStruct 2019';
+  var CONFERENCE = 'ICLR.cc/2019/Workshop/DeepGenStruct';
+  var BLIND_INVITATION = CONFERENCE+'/-/Blind_Submission';
+  var PAPER_GROUP = CONFERENCE + '/Paper' + note.number;
+  var PAPER_AUTHORS = PAPER_GROUP + '/Authors';
+
+  var revisionInvitation = {
+    id: CONFERENCE + '/-/Paper' + note.number + '/Add_Revision',
+    readers: ['everyone'],
+    writers: [CONFERENCE],
+    invitees: note.content['authorids'],
+    signatures: [CONFERENCE],
+    duedate: invitation.duedate,
+    reply: {
+      referent: note.id,
+      forum: note.forum,
+      content: invitation.reply.content,
+      signatures: invitation.reply.signatures,
+      writers: invitation.reply.writers,
+      readers: invitation.reply.readers
+    }
+  };
+  or3client.or3request(or3client.inviteUrl, revisionInvitation, 'POST', token)
+  .then(result=> {
+    var paperGroup = {
+      id: PAPER_GROUP,
+      readers: [CONFERENCE],
+      writers: [CONFERENCE],
+      members: [],
+      signatories: [],
+      signatures: [CONFERENCE]
+    }
+    return or3client.or3request(or3client.grpUrl, paperGroup, 'POST', token);
+  })
+  .then(result => {
+    var authorGroup = {
+      id: PAPER_AUTHORS,
+      readers: [CONFERENCE, PAPER_AUTHORS],
+      writers: [CONFERENCE],
+      members: note.content.authorids,
+      signatories: [PAPER_AUTHORS],
+      signatures: [CONFERENCE]
+    };
+    return or3client.or3request(or3client.grpUrl, authorGroup, 'POST', token);
+  })
+  .then(result => { console.log(JSON.stringify(result));
+    var blindSubmission = {
+      original: note.id,
+      invitation: BLIND_INVITATION,
+      forum: null,
+      parent: null,
+      signatures: [CONFERENCE],
+      writers: [CONFERENCE],
+      readers: ['everyone'],
+      content: {
+        authors: ['Anonymous'],
+        authorids: [PAPER_AUTHORS],
+      }
+    };
+    return or3client.or3request(or3client.notesUrl, blindSubmission, 'POST', token);
+  })
+  .then(result => { console.log(JSON.stringify(result));
+    var authorMail = {
+      groups: note.content.authorids,
+      subject: 'Your submission to '+ TITLE + ' has been received: ' + note.content.title,
+      message: 'Your submission to '+ TITLE + ' has been posted.\n\nTitle: ' + note.content.title + '\n\nAbstract: ' + note.content.abstract + '\n\nTo view your submission, click here: ' + baseUrl + '/forum?id=' + result.forum
+    };
+    return or3client.or3request(or3client.mailUrl, authorMail, 'POST', token);
+  })
+  .then(result => done())
+  .catch(error => done(error));
+  return true;
+};
