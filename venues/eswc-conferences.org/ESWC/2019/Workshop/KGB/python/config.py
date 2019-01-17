f99e39808191eaa4f64dc6c29303c3ada3d7506b
Pam Mandler <mandler@cs.umass.edu>
2019-01-17 15:42:08 -0500

    Initialize KGB workshop

---
+++ venues/eswc-conferences.org/ESWC/2019/Workshop/KGB/python/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import openreview
+
+def get_conference(client):
+
+    builder = openreview.conference.ConferenceBuilder(client)
+
+    builder.set_conference_id('eswc-conferences.org/ESWC/2019/Workshop/KGB')
+    builder.set_conference_name('Knowledge Graph Building Workshop')
+    builder.set_conference_short_name('KGB 2019')
+    builder.set_homepage_header({
+    'title': 'Knowledge Graph Building Workshop',
+    'subtitle': 'Co-located with the Extended Semantic Web Conference 2019',
+    'deadline': 'Submission Deadline: 1st of March, 2019, 23:59 Hawaii time',
+    'date': '3 June 2019',
+    'website': 'http://kgb-workshop.org/',
+    'location': 'Portorož, Slovenia',
+    'instructions': ' '
+    })
+    builder.set_conference_submission_name('Full_Submission')
+    return builder.get_result()
+
+
---
+++ venues/eswc-conferences.org/ESWC/2019/Workshop/KGB/python/init_conference.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys, os
+import argparse
+import datetime
+import openreview
+import config
+
+"""
+OPTIONAL SCRIPT ARGUMENTS
+	baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+ 	username - the email address of the logging in user
+	password - the user's password
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print('connecting to {0}'.format(client.baseurl))
+
+conference = config.get_conference(client)
+
+# 3/1/19 23:59 Hawaii time
+conference.open_submissions(due_date = datetime.datetime(2019, 3, 2, 9, 59), public = True, additional_fields = {
+        'contribution': {
+            'order': 5,
+            'value-dropdown': [
+                'Full research papers (8-12 pages)',
+                'In Use and Experience papers (8-12 pages)',
+                'Position papers (6-8 pages)',
+                'Short research papers (4-6 pages)',
+                'System/demo papers (4-6 pages)'
+            ],
+            'required': True
+        },
+    })
