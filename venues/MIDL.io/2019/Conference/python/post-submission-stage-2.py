07c40335af76cbada7ab2d2ab8327e7d2589d4fa
Pam Mandler <mandler@cs.umass.edu>
2019-01-28 09:48:58 -0500

    Make reviews visible to reviewers and authors of the paper

--- venues/MIDL.io/2019/Conference/python/post-submission-stage-2.py
+++ venues/MIDL.io/2019/Conference/python/post-submission-stage-2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-import sys, os
 import argparse
-import datetime
 import openreview
+from openreview import tools
 import config
 
 """
@@ -24,6 +23,21 @@ client = openreview.Client(baseurl=args.baseurl, username=args.username, passwor
 print('connecting to {0}'.format(client.baseurl))
 
 conference = config.get_conference(client)
+
+print('make reviews visible to reviewers and authors of the paper')
+invitations = tools.iterget_invitations(client, regex=conference.get_id() + '/-/Paper.*/Official_Review')
+for invite in invitations:
+    paper_num = invite.id.split('/Paper')[1].split('/')[0]
+    invite.reply['readers']['values'].extend([conference.get_id() + '/Paper' + paper_num + '/Reviewers',
+                                              conference.get_id() + '/Paper' + paper_num + '/Authors'])
+    client.post_invitation(invite)
+
+reviews = tools.iterget_notes(client, invitation=conference.get_id() + '/-/Paper.*/Official_Review')
+for review in reviews:
+    paper_num = review.invitation.split('/Paper')[1].split('/')[0]
+    review.readers.extend([conference.get_id() + '/Paper' + paper_num + '/Reviewers',
+                           conference.get_id() + '/Paper' + paper_num + '/Authors'])
+    client.post_note(review)
 
 print('open comment invitations')
 conference.set_authors()
