c3df59cf381268430c3d67b8081eb33f402b5ca1
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-02-06 10:17:26 -0500

    split spc-pc into 2 files

--- venues/auai.org/UAI/2019/python/init.py
+++ venues/auai.org/UAI/2019/python/init.py
@@ -16,38 +16,6 @@ if __name__ == '__main__':
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
     conference = config.get_conference(client)
 
-    program_chair_group = conference.set_program_chairs(emails=[])
-    with open('../webfield/programChairWebfield.js') as f:
-        program_chair_group.web = f.read()
-        client.post_group(program_chair_group)
-
-    # set_area_chairs_name
-    conference.set_area_chairs_name('Senior_Program_Committee')
-    area_chair_group = conference.set_area_chairs(emails=[])
-    # with open('../webfield/programChairWebfield.js') as f:
-    #     program_chair_group.web = f.read()
-    #     client.post_group(program_chair_group)
-
-    conference_group = client.get_group(conference.get_id())
-    with open(os.path.abspath('../webfield/homepage.js')) as f:
-        conference_group.web = f.read()
-        client.post_group(conference_group)
-
     print('Conference creation complete.')
-
-    print ('inviting SPC members now')
-
-    spc_emails_to_invite = [
-        'mohituniyal16apr@gmail.com',
-        '~Mohit_Uniyal1',
-        'mbok@cs.umass.edu']
-    message = 'Please join the Justice league: Accept : {accept_url} \n\n Decline : {decline_url}'
-
-    conference.recruit_reviewers(
-        emails = spc_emails_to_invite,
-        title = 'UAI 2019: Invitation to serve on the Program Committee',
-        message = message,
-        reviewers_name = 'Senior_Program_Committee'
-    )
 
     print ('done')
