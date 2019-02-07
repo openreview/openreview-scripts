83ae8dcd1a67f31a996345afc6d9b31ec71960cf
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-02-07 15:47:41 -0500

    create unsub and submitted groups for pc members

--- venues/learningtheory.org/COLT/2019/python/assign.py
+++ venues/learningtheory.org/COLT/2019/python/assign.py
@@ -2,6 +2,12 @@ import openreview
 import config
 import argparse
 
+def getPCmembersByForum(blind_note):
+    pc_member_ids = []
+    reg = 'learningtheory.org/COLT/2019/Conference/Paper' + str(blind_note.number) + '/Program_Committee_Member.*'
+    anonreviewers = client.get_groups(regex=reg)
+    anonreviewer_ids = [anon.id for anon in anonreviewers]
+    return anonreviewer_ids
 
 if __name__ == '__main__':
     ## Argument handling
@@ -18,14 +24,16 @@ if __name__ == '__main__':
 
     # find paper_number by forum because Paper_Assignments include forum info,
     # but add_assignment() needs paper_number
-    notes = openreview.tools.iterget_notes(client, invitation=conference.get_id() + '/-/Blind_Submission')
+    blind_notes = list(openreview.tools.iterget_notes(client, invitation=conference.get_id() + '/-/Blind_Submission'))
     number_by_forum = {}
-    for note in notes:
+    for note in blind_notes:
         number_by_forum[note.forum] = note.number
 
-    assignment_notes = openreview.tools.iterget_notes(client,
-                                                      invitation='/'.join([conference.get_id(), '-', 'Paper_Assignment']),
-                                                      content = {'label' : args.label})
+    assignment_notes = openreview.tools.iterget_notes(
+        client, 
+        invitation='/'.join([conference.get_id(), '-', 'Paper_Assignment']), 
+        content = {'label' : args.label}
+    )
     
     unprocessed_assignments = {}
     for assignment_note in assignment_notes:
@@ -40,6 +48,9 @@ if __name__ == '__main__':
                     conference.get_id(),
                     conference.get_program_chairs_id(),
                     'learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(paper_number)
+                ],
+                'nonreaders': [
+                    'learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee/Unsubmitted'.format(paper_number)
                 ],
                 'signatories': ['learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(paper_number)]
             }
@@ -65,3 +76,44 @@ if __name__ == '__main__':
             unprocessed_assignments[assignment_note.forum] = assignment_note.id
     print ('Count of unprocesses assignment notes : ', len(unprocessed_assignments.keys()))
     print ('Forums of unprocessed assignment notes : ', unprocessed_assignments.keys())
+
+    # Templates for unsubmitted and submitted groups of Program Committee
+    pcs_unsubmitted_template = {
+        'id': conference.get_id() + '/Paper<number>/Program_Committee/Unsubmitted',
+        'readers':[
+            conference.get_id(),
+            conference.get_program_chairs_id()
+        ],
+        'writers': [conference.get_id()],
+        'signatures': [conference.get_id()],
+        'signatories': [conference.get_id()],
+        'members': []
+    }
+
+    pcs_submitted_template = {
+        'id': conference.get_id() + '/Paper<number>/Program_Committee/Submitted',
+        'readers':[
+            conference.get_id(),
+            conference.get_program_chairs_id(),
+            conference.get_id() + '/Paper<number>/Program_Committee'
+        ],
+        'writers': [conference.get_id()],
+        'signatures': [conference.get_id()],
+        'signatories': [conference.get_id()],
+        'members': []
+    }
+    # Post unsubmitted and submitted program committee groups for each blind note
+    for paper in blind_notes:
+        group_to_post = openreview.Group.from_json(
+        openreview.tools.fill_template(
+            pcs_unsubmitted_template, paper))
+
+        members = getPCmembersByForum(paper)
+        group_to_post.members = members
+        print ("Group posted :", client.post_group(group_to_post).id)
+
+        group_to_post = openreview.Group.from_json(
+        openreview.tools.fill_template(
+            pcs_submitted_template, paper))
+        print ("Group posted :", client.post_group(group_to_post).id)
+
