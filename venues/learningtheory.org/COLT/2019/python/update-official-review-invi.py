b74f28de5b27cad59a4d67e5604e4af535716ed6
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-02-08 15:59:45 -0500

    Colt/official review fix (#969)
    * added a py script to update official review invis
    
    * update to review process

---
+++ venues/learningtheory.org/COLT/2019/python/update-official-review-invi.py
@@ -0,0 +1,70 @@
+import openreview
+import config
+import argparse
+
+
+if __name__ == '__main__':
+    ## Argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+    conference = config.get_conference(client)
+
+    official_review_template = {
+        'id': conference.id + '/-/Paper<number>/Official_Review',
+        'readers': ['everyone'],
+        'writers': [conference.id],
+        'invitees': [
+            conference.id + '/Paper<number>/Reviewers', 
+            conference.id + '/Paper<number>/Program_Committee'],
+        'noninvitees': [],
+        'signatures': [conference.id],
+        'duedate': openreview.tools.timestamp_GMT(year=2019, month=3, day=21, hour=7),
+        'multiReply': False,
+        'process' : None,
+        'reply': {
+            'forum': '<forum>',
+            'replyto': '<forum>',
+            'readers': {
+                'description': 'The users who will be allowed to read the reply content.',
+                'values': [
+                    conference.id + '/Paper<number>/Program_Committee',
+                    conference.id + '/Program_Chairs'
+                    ]
+            },
+            'signatures': {
+                'description': 'How your identity will be displayed with the above content.',
+                'values-regex': conference.id + '/Paper<number>/AnonReviewer[0-9]*|' + conference.id + '/Paper<number>/Program_Committee_Member[0-9]*'
+            },
+            'writers': {
+                'description': 'Users that may modify this record.',
+                'values-copied':  [
+                    conference.id,
+                    '{signatures}'
+                ]
+            },
+            'nonreaders': {
+                'description': 'Users not allowed to read the reply',
+                'values': [
+                    conference.id + '/Paper<number>/Program_Committee/Unsubmitted'
+                ]
+            },
+            'content': openreview.invitations.content.review
+        }
+    }
+    with open('../process/officialReviewProcess.js', 'r') as f:
+        official_review_template['process'] = f.read()
+    blind_notes = list(openreview.tools.iterget_notes(client, invitation=conference.get_id() + '/-/Blind_Submission'))
+    for index, note in enumerate(blind_notes):
+        official_review_invitation = client.post_invitation(
+            openreview.Invitation.from_json(
+                openreview.tools.fill_template(official_review_template, note)
+            )
+        )
+        if (index+1)%10 == 0 :
+            print ('Processed ', index+1)
+    print ('Processed ', index+1)
