21cc444a34b44fa35a7ec4218e99d9f67eab0903
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-02-08 12:29:04 -0500

    added a py script to update official review invis

--- venues/learningtheory.org/COLT/2019/process/officialReviewProcess.js
+++ venues/learningtheory.org/COLT/2019/process/officialReviewProcess.js
@@ -3,7 +3,7 @@
 function(){
     var or3client = lib.or3client;
 
-    var CONFERENCE_ID = 'ICLR.cc/2019/Conference';
+    var CONFERENCE_ID = 'learningtheory.org/COLT/2019/Conference';
 
     var origNote = or3client.or3request(or3client.notesUrl+'?id='+note.forum, {}, 'GET', token);
     var list = note.invitation.replace(/_/g,' ').split('/');
@@ -14,11 +14,11 @@ function(){
       var forum = result.notes[0];
       var note_number = forum.number;
 
-      var reviewers = [CONFERENCE_ID + '/Paper' + note_number + '/Reviewers'];
-      var areachairs = [CONFERENCE_ID + '/Paper' + note_number + '/Area_Chairs'];
+      var pcMembers = [CONFERENCE_ID + '/Paper' + note_number + '/Program_Committee'];
+      // var areachairs = [CONFERENCE_ID + '/Paper' + note_number + '/Area_Chairs'];
       var authors = forum.content.authorids;
 
-      var areachair_mail = {
+      var pc_mail = {
         "groups": areachairs,
         "subject": "Review posted to your assigned paper: \"" + forum.content.title + "\"",
         "message": "A submission to " + conference + ", for which you are an official area chair, has received an official review. \n\nTitle: " + note.content.title + "\n\nComment: " + note.content.review + "\n\nTo view the review, click here: " + baseUrl + "/forum?id=" + note.forum
