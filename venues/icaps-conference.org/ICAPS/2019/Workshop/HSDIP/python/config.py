9cbf5e80296b75800a9e8532b708f80c433bc1cf
pmandler <mandler@cs.umass.edu>
2019-02-13 10:29:41 -0500

    Feature/icaps hsdip (#979)
    * NIPS IRASL update duedates
    
    * ICAPS HSDIP init site without consoles
    
    * Remove files without 2019 in file structure.'
    
    * Initial code with right directory structure.

---
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/HSDIP/python/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import openreview
+
+
+def get_conference(client):
+
+    builder = openreview.conference.ConferenceBuilder(client)
+
+    builder.set_conference_id('icaps-conference.org/ICAPS/2019/Workshop/HSDIP')
+    builder.set_conference_name('Heuristics and Search for Domain-independent Planning')
+    builder.set_conference_short_name('ICAPS HSDIP 2019')
+    builder.set_homepage_header({
+    'title': 'Heuristics and Search for Domain-independent Planning',
+    'subtitle': 'ICAPS 2019 Workshop',
+    'deadline': 'Submission Deadline: March 15, 2019',
+    'date': 'July 11-15, 2019',
+    'website': 'https://icaps19.icaps-conference.org/workshops/HSDIP/index.html',
+    'location': 'Berkeley, CA, USA'
+    })
+    builder.set_double_blind(True)
+
+    return builder.get_result()
+
