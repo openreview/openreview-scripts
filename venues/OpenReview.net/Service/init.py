7d0c2c97c6ed8a4b4bf20c2bb1da3ed826f5e5e0
Michael Spector <michaelspector@gmail.com>
2019-02-15 11:50:18 -0500

    updates to service request form

--- venues/OpenReview.net/Service/init.py
+++ venues/OpenReview.net/Service/init.py
@@ -19,6 +19,147 @@ service_requests = client.post_group(openreview.Group(**{
     'web': './serviceRequestsWeb.js'
 }))
 
+reader_options = [
+    'Program Chairs',
+    'Assigned Area Chairs/Metareviewers',
+    'Assigned Reviewers',
+    'All Area Chairs/Metareviewers',
+    'All Reviewers',
+    'All Authors',
+    'Everyone directly involved in the Conference',
+    'Logged-in OpenReview Users',
+    'General Public',
+    'Other'
+]
+
+request_content = {
+    'title': {
+        'value-copied': '{content[\'Official Conference Name\']}',
+        'description': 'Used for display purposes. Will be copied from the Official Conference Name'
+    },
+#     'Data Sharing Agreement': {
+#         'values-checkbox': [
+#             'I agree to participate in the data annotation task',
+#             'I grant permission to OpenReview to publish data annotations',
+#             'I grant permission to OpenReview to publish anonymous paper-reviewer matching data'
+#         ],
+#         'description': 'As a condition of this free service, we ask that conference organizers agree to participate in a short (~1 hour) data annotation task \
+# to advance OpenReview\'s research objectives. \
+# This data will be used to support research  \
+# on paper-reviewer assignment optimization, natural language processing of scientific text, paper-reviewer affinity modeling, and improving the effectiveness of the peer review process, \
+# and will be made publicly available after collection.\
+# \n\n\
+# For users of the OpenReview Paper Matching System, we also ask that conference organizers grant us permission to publish data inputs to the matching system after acceptance decisions have been made. \
+# This data will be completely anonymous and will not jepoardize the anonymity of any conference participant.',
+#         'scroll': True
+#     },
+    'Official Conference Name': {
+        'description': 'This will appear on your conference\'s OpenReview page. Example: "Seventh International Conference on Learning Representations"',
+        'value-regex': '.*',
+        'required': True
+    },
+    'Abbreviated Conference Name': {
+        'description': 'Please include the year as well. This will be used to identify your conference on OpenReview and in email subject lines. Example: "ICLR 2019"',
+        'value-regex': '.*',
+        'required': True
+    },
+    'Official Website URL': {
+        'description': 'Please provide the official website URL of the conference.',
+        'value-regex': '.*',
+        'required': True
+    },
+    'Contact Emails': {
+        'description': 'Please provide the email addresses of all the Program Chairs or Organizers (comma-separated)',
+        'values-regex': '.*',
+        'required': True
+    },
+    'Requested Services': {
+        'description': 'Which of OpenReview\'s services are you requesting?',
+        'values-checkbox': [
+            'Peer Review Management',
+            'OpenReview Paper Matching System',
+            'Other (describe below)'
+        ],
+        'required': True
+    },
+    'Area Chairs (Metareviewers)': {
+        'description': 'Does your conference have Area Chairs?',
+        'value-radio': [
+            'Yes, our conference has Area Chairs',
+            'No, our conference does not have Area Chairs',
+            'Other (describe below)'
+        ],
+        'required': True
+    },
+    'Submission Start Date': {
+        'description': 'When would you (ideally) like to have your OpenReview submission portal opened? Please submit in the following format: YYYY/MM/DD (e.g. 2019/01/31). (Skip this if only requesting paper matching service)',
+        'value-regex': '.*',
+    },
+    'Submission Deadline': {
+        'value-regex': '.*',
+        'description': 'By when do authors need to submit their manuscripts? Please submit in the following format: YYYY/MM/DD (e.g. 2019/01/31)',
+    },
+    'Conference Start Date': {
+        'description': 'What is the start date of conference itself? Please submit in the following format: YYYY/MM/DD (e.g. 2019/01/31)',
+        'value-regex': '.*'
+    },
+    'Peer Review Management': {
+        'description': 'Select the peer review management options you would like to include for your conference. Leave this section blank if you do not wish to use our peer review management service. See the top of the page for a description of each option.',
+        'values-checkbox': [
+            'Reviewer Recruitment by Email',
+            'Reviewer Bidding for Papers',
+            'Reviewer Recommendations',
+            'Other (describe below)'
+        ]
+    },
+    'Paper Matching': {
+        'description': 'Choose options for assigning papers to reviewers. If using the OpenReview Paper Matching System, see the top of the page for a description of each feature type.',
+        'values-checkbox': [
+            'OpenReview Affinity',
+            'TPMS',
+            'Reviewer Bid Scores',
+            'Reviewer Recommendation Scores',
+            'Organizers will assign papers manually',
+            'Other (describe below)'
+        ]
+    },
+    'Author and Reviewer Anonymity': {
+        'description': 'What policy best describes your anonymity policy? (Select "Other" if none apply, and describe your request below)',
+        'value-radio': [
+            'Double-blind',
+            'Single-blind (Reviewers are anonymous)',
+            'No anonymity',
+            'Other (describe below)'
+        ]
+    },
+    'Open Reviewing Policy': {
+        'description': 'Should submitted papers and/or reviews be visible to the public? (This is independent of anonymity policy)',
+        'value-radio': [
+            'Submissions and reviews should both be public.',
+            'Submissions should be public, but reviews should be private.',
+            'Submissions and reviews should both be private.',
+            'Other (describe below)'
+        ]
+    },
+    'Public Commentary': {
+        'description': 'Would you like to allow members of the public to comment on papers?',
+        'value-radio': [
+            'Yes, allow members of the public to comment anonymously.',
+            'Yes, allow members of the public to comment non-anonymously.',
+            'No, do not allow public commentary.',
+            'Other (describe below)'
+        ]
+    },
+    'Other Important Information': {
+        'value-regex': '[\\S\\s]{1,5000}',
+        'description': 'Please use this space to clarify any questions above for which you responded "Other", and to clarify any other information that you think we may need.'
+    },
+    'How did you hear about us?': {
+        'value-regex': '.*',
+        'description': 'Please briefly describe how you heard about OpenReview.'
+    }
+}
+
 request_inv = client.post_invitation(openreview.Invitation(**{
     'id': 'OpenReview.net/Service/-/Request_Form',
     'readers': ['everyone'],
@@ -28,7 +169,7 @@ request_inv = client.post_invitation(openreview.Invitation(**{
     'process': 'serviceProcess.js',
     'reply': {
         'readers': {
-            'values': ['OpenReview.net']
+            'values-copied': ['OpenReview.net', '{content["Program Chair Emails"]}']
         },
         'writers': {
             'values-regex': '~.*',
@@ -36,30 +177,6 @@ request_inv = client.post_invitation(openreview.Invitation(**{
         'signatures': {
             'values-regex': '~.*'
         },
-        'content': {
-            'Name of contact': {
-                'value-regex': '.*',
-                'required': True
-            },
-            'Contact email': {
-                'value-regex': '.*',
-                'required': True
-            },
-            'Service requested': {
-                'value-regex': '[\\S\\s]{1,5000}',
-                'description': 'Please describe the type of service you are requesting (peer review, paper matching, both, etc.). Please include as much detail as possible.'
-            },
-            'Submission start date': {
-                'value-regex': '.*',
-                'description': 'When would you (ideally) like to have your OpenReview submission portal opened? (Skip this if only requesting paper matching service)'
-            },
-            'Submission deadline': {
-                'value-regex': '.*',
-            },
-            'Other important dates': {
-                'value-regex': '[\\S\\s]{1,5000}',
-                'description': 'Please describe any other key dates that we should be aware of.'
-            }
-        }
+        'content': request_content
     }
 }))
--- venues/OpenReview.net/Service/serviceRequestsWeb.js
+++ venues/OpenReview.net/Service/serviceRequestsWeb.js
@@ -6,6 +6,67 @@
 // and a list of all submitted papers (#notes).
 // ------------------------------------
 
+
+var instructionsHtml = '\
+<h3>Getting Started</h3>\
+<p class="dark">\
+If you would like to use OpenReview for your upcoming Journal, Conference, or Workshop, please fill out and submit the form below. \
+Please see the sections below for more details.\
+</p>\
+<h3>Frequently Asked Questions</h3>\
+<h4><em>How much does all this cost?</em></h4>\
+<p class="dark">\
+OpenReview is free, and will remain free for the foreseeable future. \
+</p>\
+<h4><em>Can I request features not listed here?</em></h4>\
+<p class="dark">\
+The options provided in the form describe the most common set of variables that are requested. \
+That being said, OpenReview is built for flexibility, and it may be possible for us to accommodate special requests. \
+If you have a special request, please describe it in the text box at the end of the request form. \
+We are more likely to honor requests that are useful to many conferences, and that we can incorporate into our regular offerings.\
+</p>\
+<h3>Peer Review Management Options</h3>\
+<p class="dark">\
+OpenReview offers a suite of peer review management tools and features for conference organizers. \
+The set of most commonly requested features is listed below, but \
+if you have a specific request, please describe it in the free text field at the end of the form. \
+<ul>\
+<li><strong>Reviewer Recruitment by Email</strong>: OpenReview can deploy custom recruitment emails to potential reviewers and track which have accepted or declined the invitation.</li>\
+<li><strong>Reviewer Bidding for Papers</strong>: OpenReview supports reviewer bidding (i.e. reviewers indicate their preference to review papers).</li>\
+<li><strong>Reviewer Recommendations</strong>: OpenReview supports specific reviewer recommendations by members of the conference (e.g. recommendations made by Area Chairs, Program Chairs, or even other Reviewers).</li>\
+</ul>\
+</p>\
+<h3>The OpenReview Paper Matching System</h3>\
+<p class="dark">\
+OpenReview offers automated paper-reviewer assignment optimization and conflict of interest detection, \
+as well as a web interface for browsing, viewing statistics, and modifying the resulting assignments. \
+The OpenReview Matching System supports multiple optimization objectives and arbitrary numerical inputs as features for the match. \
+If you would like to use the OpenReview Paper Matching System for your conference, please indicate which features you would like to use in the form below. \
+Our most commonly used features are described in the following list: \
+<ul>\
+<li><strong>OpenReview Affinity</strong>: our in-house paper-reviewer affinity model based on TF-IDF vector similarity. </li>\
+<li><strong>TPMS</strong>: <a href="http://torontopapermatching.org/webapp/profileBrowser/about_us/">The Toronto Paper Matching System</a>, a third-party affinity model (if selected, conferences are responsible for coordinating with TPMS)</li>\
+<li><strong>Reviewer Bid Scores</strong>: Reviewers indicate their preference to review papers, which are then converted to scores.</li>\
+<li><strong>Reviewer Recommendation Scores</strong>: Reviewers, Area Chairs, and/or Program Chairs recommend specific reviewers to review papers. These recommendations are then converted to scores according to your specifications.</li>\
+</ul>\
+</p>\
+<h3>Public vs. Private content</h3>\
+<p class="dark">\
+In terms of the openness of submissions, reviews, and comments, OpenReview offers pre-configured settings \
+that describe our most commonly requested permissions. We refer to them below as "Public" and "Private." \
+<ul>\
+<li><strong>Public</strong> content is visible to anyone who visits OpenReview.</li> \
+<li><strong>Private</strong> content is visible only to applicable conference officials. For example, \
+Private Reviews and Comments are visible only to the Program Chairs, to the Reviewers (and Area Chairs if applicable) assigned to the reviewed paper, \
+and to the paper\'s authors. Private Submissions are visible only to the Program Chairs, all Reviewers, and all Area Chairs.</li>\
+</ul>\
+If you would like a different set of permissions, please describe the permissions in the free text box at the end of the form.\
+</p>\
+<h3>Questions or Concerns?</h3> \
+<p class="dark">\
+Please contact the OpenReview support team at \
+<a href="mailto:info@openreview.net">info@openreview.net</a> with any questions or concerns.</p>'
+
 // Constants
 var CONFERENCE = "OpenReview.net/Service";
 var SUBMISSION = CONFERENCE + '/-/Request_Form';
@@ -42,11 +103,7 @@ function renderConferenceHeader() {
     location: "Amherst, MA",
     date: "Ongoing",
     website: "https://openreview.net",
-    instructions: '<p><strong>Journal, Conference, and Workshop Organizers who wish to use OpenReview:</strong><br>\
-          If you would like to use OpenReview for your upcoming Journal, Conference, or Workshop, please fill out and submit the form below. \
-          <p><strong>Questions or Concerns:</strong><br> \
-          Please contact the OpenReview support team at \
-          <a href="mailto:info@openreview.net">info@openreview.net</a> with any questions or concerns. \</br>\</p>',
+    instructions: instructionsHtml
   });
 
   Webfield.ui.spinner('#notes');
