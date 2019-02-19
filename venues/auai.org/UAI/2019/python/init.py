1c1d4a123bf3e4420c58432c1b2340482747fd63
Melisa Bok <melisabok@gmail.com>
2019-02-19 15:41:50 -0500

    set bidding stage, refactor file names

--- venues/auai.org/UAI/2019/python/init.py
+++ venues/auai.org/UAI/2019/python/init.py
@@ -21,6 +21,8 @@ if __name__ == '__main__':
     print ('done')
     conference.set_program_chairs(emails = [#hidden
     ])
+    conference.set_reviewers(emails = [])
+    conference.set_area_chairs(emails = [])
     conference.open_submissions(due_date = datetime.datetime(2019, 3, 5, 11, 59), public = False, subject_areas = config.subject_areas, remove_fields = ['pdf'], additional_fields = {
         'pdf': {
             'description': 'Upload a PDF file that ends with .pdf',
