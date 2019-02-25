f6bef7a81d79d413cb6228076e7a1f5ea23eba51
Melisa Bok <melisabok@gmail.com>
2019-02-25 12:09:19 -0500

    fix variable name

--- venues/learningtheory.org/COLT/2019/python/config.py
+++ venues/learningtheory.org/COLT/2019/python/config.py
@@ -17,5 +17,5 @@ def get_conference(client):
     'location': 'Phoenix, Arizona, United States'
     })
     builder.set_conference_area_chairs_name('Program_Committee')
-    builder.enable_double_blind(read_area_chairs = True, read_program_chairs = True)
+    builder.set_double_blind(area_chairs_read_original = True)
     return builder.get_result()
