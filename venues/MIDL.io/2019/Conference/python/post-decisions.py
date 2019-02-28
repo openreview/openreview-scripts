fd59721a718265487cee7ad7d522e7d3e68a1ed4
pmandler <mandler@cs.umass.edu>
2019-02-28 11:20:31 -0500

    Feature/midl borderline (#1008)
    * NIPS IRASL update duedates
    
    * Handle sending decision emails w/ special way for borderline cases

--- venues/MIDL.io/2019/Conference/python/post-decisions.py
+++ venues/MIDL.io/2019/Conference/python/post-decisions.py
@@ -43,6 +43,50 @@ submissions = {}
 for paper in iterator:
     submissions[paper.number]=paper
 
+subject_line = "MIDL Submission decision"
+message = {}
+message['Reject']='''Dear Author,
+
+We regret to inform you that your submission to MIDL 2019 has been rejected for the full paper tract. This year, we received a large number of high quality submissions and could only accept a certain portion for the full paper track.
+
+Please note that the submission deadline for the abstract has not yet passed (12 April 2019 17:00 UTC). We encourage you to revise your article based on reviewers’ comments, and consider submitting a shorter version of it to the abstract tract. Details about the abstract track can be found here: http://2019.midl.io/call-for-papers/
+
+Thank you once again for your interest in MIDL 2019. We hope that despite the outcome, you consider joining us in London for the conference.
+
+Sincerely,
+Program Chairs for MIDL 2019.'''
+
+
+message['LP Accept / Borderline']= '''Dear Author,
+
+{0}
+
+Unfortunately, given the concerns raised by the reviewers and our target acceptance rate, the PC decided not to accept your submission for the full paper track. However, there is clear consensus that your submission has merit for presentation at MIDL, and is likely to result in valuable discussion. Given the quality of your submission, we would be delighted to have your submission presented in the abstract track and like to offer you an early acceptance if you decide to summarise and resubmit the article as an extended abstract.
+
+If you accept our offer, we ask you to submit a faithful summary of your initial full submission as a 3 page extended abstract, excluding references and acknowledgements. Further details for the abstract submissions can be found here: http://2019.midl.io/call-for-papers/. We also would like to remind you our dual submission policy, if you are also considering submitting your work to elsewhere. Details for the dual submission policy can be found here: https://2019.midl.io/submissions/.
+
+Additionally, please let us know if you would like us to remove your full paper from the OpenReview environment since the full paper is not accepted.
+
+Thank you for your interest in MIDL 2019. We hope you accept our offer and decide to join us in the conference. We are looking forward to seeing you in London.
+
+Sincerely,
+Program Chairs for MIDL 2019.'''
+
+message['Accept'] = '''Dear Author,
+
+We are happy to inform you that your submission {0} to MIDL has been accepted for presentation in the conference as a full submission. We will inform you about the type of presentation (Oral / Poster) in the next two weeks. In the meanwhile, there are two important points we would like to draw your attention to:
+
+For the camera-ready version, we encourage you to take into account the reviewers’ concerns and apply appropriate changes to your submission to improve its quality. To this end, if you need to go over the suggested page limit, you may do so while still aiming for conciseness. The quality of the published article in the PMLR proceedings is the most important concern. We acknowledge that these changes might also take time. To provide you enough time, we have set the deadline for submitting the camera-ready version to 15 April 2019. Please inform us as soon as possible, if you think you will not be able to prepare the camera ready article until then. Further information on the process for submitting the camera-ready manuscript will be provided in due course.
+
+
+Importantly, in order to ensure publication of your paper, please be aware that each conference paper must have at least one author registering to the conference no later than 25th of March.
+Details about registration can be found here: https://2019.midl.io/registration/
+
+Congratulations once again. We are looking forward to seeing you in London in July.
+
+Sincerely,
+Program Chairs for MIDL 2019
+'''
 def create_decision_invite():
     ## Decision
 
@@ -94,22 +138,37 @@ def create_decision_invite():
         invite = openreview.Invitation(paperinv, **decision_parameters)
         client.post_invitation(invite)
 
-def remove_if_rejected(paper_num, decision):
+def remove_if_rejected(paper_num, decision, add_text):
     if int(paper_num) not in submissions.keys():
         # already removed
+        print("already removed "+paper_num)
         return True
     else:
         paper = submissions[int(paper_num)]
-        if decision == "Reject" and paper.content.get('remove if rejected', False):
+        if (decision == "Reject" or decision == "LP Accept / Borderline")\
+                and paper.content.get('remove if rejected', False):
             # this is actually the time now here as though it were GMT so it's off by 5 hours.
             paper.ddate = tools.datetime_millis(datetime.datetime.now())
             client.post_note(paper)
             print("Remove note: " + paper_num)
+            client.send_mail(subject_line, paper.content['authorids'], message[decision].format(add_text))
             return True
     return False
 
-def post_decision(paper_num, decision):
-    if (decision == "Reject") or (decision == "Accept"):
+def post_decision(paper_num, decision, add_text):
+    if (decision == "Reject") or (decision == "Accept") or (decision == "LP Accept / Borderline"):
+        paper = submissions[int(paper_num)]
+        if decision == "LP Accept / Borderline":
+            # if borderline, use additional text in email
+            # but treat as if rejected
+            client.send_mail(subject_line, paper.content['authorids'],
+                             message[decision].format(add_text))
+            decision = "Reject"
+        else:
+            client.send_mail(subject_line, paper.content['authorids'],
+                             message[decision].format(paper.content['title']))
+
+        # post decision note as Accept or Reject
         paperinv = conference.get_id() + '/-/Paper' + paper_num + '/Decision'
         decision_note= openreview.Note(
             invitation= paperinv,
@@ -121,7 +180,7 @@ def post_decision(paper_num, decision):
                       'decision':decision}
         )
         client.post_note(decision_note)
-        print("Post note: "+paper_num)
+        print("Post note: "+paper_num+decision)
     elif decision:
         print("Decision invalid: <"+decision+">")
 
@@ -135,12 +194,16 @@ if args.assignments.endswith('.csv'):
         next(reader,None)
         for row in reader:
             paper_number = row[1]
-            if len(row) >= 12:
-                decision = row[11]
-                if not remove_if_rejected(paper_number,decision):
-                    post_decision(paper_number, decision)
+            if len(row) >= 13:
+                decision = row[12]
+            if len(row) >= 14:
+                add_text = row[13]
+            else:
+                add_text = ""
+            if not remove_if_rejected(paper_number, decision, add_text):
+                post_decision(paper_number, decision, add_text)
 else:
     paper_number = args.assignments.split(',')[1]
     decision = args.assignments.split(',')[0]
-    if not remove_if_rejected(paper_number, decision):
-        post_decision(paper_number, decision)
+    if not remove_if_rejected(paper_number, decision, ""):
+        post_decision(paper_number, decision, "")
