5cb283018b3b8f789792fb6f06bd0caedec18dd8
Michael Spector <michaelspector@gmail.com>
2019-03-05 14:32:03 -0500

    update the support request form process function. it should now create a comment sub-invitation for its forum, with reply permission granted to contact emails

--- venues/OpenReview.net/Support/supportProcess.js
+++ venues/OpenReview.net/Support/supportProcess.js
@@ -9,19 +9,33 @@ function(){
     }
 
     var openreviewMailPayload = {
-      'groups': ['info@openreview.net'],
-      'subject': 'A request for service has been submitted',
-      'message': adminMessage
+      groups: ['info@openreview.net'],
+      subject: 'A request for service has been submitted',
+      message: adminMessage
     };
 
     var programchairMailPayload = {
-      'groups': note.content['Contact Emails'],
-      'subject': 'Your request for OpenReview service has been received.',
-      'message': 'You recently requested conference management services from OpenReview. A member of our support team will contact you shortly. You can view the request here: ' + baseUrl + '/forum?id=' + note.forum
+      groups: note.content['Contact Emails'],
+      subject: 'Your request for OpenReview service has been received.',
+      message: 'You recently requested conference management services from OpenReview. A member of our support team will contact you shortly. You can view the request here: ' + baseUrl + '/forum?id=' + note.forum
     };
+
+    var commentInvitation = {
+      id: 'OpenReview.net/Support/-/Request' + note.number + '/Comment',
+      super: 'OpenReview.net/Support/-/Comment',
+      reply: {
+        forum: note.forum,
+        replyto: null,
+        readers: {
+            values: note.content['Contact Emails'].concat(['OpenReview.net/Support'])
+        }
+      },
+      signatures: ['OpenReview.net/Support']
+    }
 
     or3client.or3request(or3client.mailUrl, openreviewMailPayload, 'POST', token)
     .then(result => or3client.or3request(or3client.mailUrl, programchairMailPayload, 'POST', token))
+    .then(result => or3client.or3request(or3client.inviteUrl, commentInvitation, 'POST', token))
     .then(result => done())
     .catch(error => done(error));
 
