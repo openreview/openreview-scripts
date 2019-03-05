721327df3686bfb5c82aef44c3d5527be7a237ee
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-03-05 07:06:59 -0500

    added start date to submission invi

--- venues/auai.org/UAI/2019/Conference/python/post-submissions-stage.py
+++ venues/auai.org/UAI/2019/Conference/python/post-submissions-stage.py
@@ -16,14 +16,16 @@ if __name__ == '__main__':
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
     conference = config.get_conference(client)
 
-    conference.set_program_chairs(emails = [])
-    conference.set_reviewers(emails = [])
-    conference.set_area_chairs(emails = [])
+    conference.create_blind_submissions()
+    conference.set_authors()
 
-    conference.open_submissions(start_date = datetime.datetime(2019, 2, 15, 11, 59), due_date = datetime.datetime(2019, 3, 5, 11, 59), remove_fields = ['pdf'], additional_fields = {
+    conference.open_revise_submissions(name = 'Revision', start_date = datetime.datetime(2019, 2, 5, 11, 59), due_date = datetime.datetime(2019, 3, 9, 11, 59), additional_fields = {
         'pdf': {
             'description': 'Upload a PDF file that ends with .pdf',
-            'required': False,
-            'value-regex': 'upload'
+            'required': True,
+            'value-regex': 'upload',
+            'order': 99
         }
-    })
+    }, remove_fields = ['title', 'pdf'])
+
+    
