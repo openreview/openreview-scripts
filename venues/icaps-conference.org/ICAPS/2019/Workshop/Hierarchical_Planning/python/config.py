0130328d3ba0fa2de53da9750fe2942d19b92229
Pam Mandler <mandler@cs.umass.edu>
2019-03-11 16:19:00 -0400

    ICAPS Heirarchical Planning initialization

---
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/Hierarchical_Planning/python/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import openreview
+
+
+def get_conference(client):
+
+    builder = openreview.conference.ConferenceBuilder(client)
+
+    builder.set_conference_id('icaps-conference.org/ICAPS/2019/Workshop/Hierarchical_Planning')
+    builder.set_conference_name('Second ICAPS Workshop on Hierarchical Planning')
+    builder.set_conference_short_name('ICAPS 2019 Workshop Hierarchical Planning')
+    builder.set_homepage_header({
+    'title': 'Second Workshop on Hierarchical Planning',
+    'subtitle': 'ICAPS 2019 Workshop',
+    'deadline': 'Submission Deadline: March 24, 2019 midnight AoE',
+    'date': 'July 11-15, 2019',
+    'website': 'https://icaps19.icaps-conference.org/workshops/Hierarchical-Planning/index.html',
+    'location': 'Berkeley, CA, USA'
+    })
+    builder.set_double_blind(True)
+    builder.set_submission_public(True)
+    return builder.get_result()
+
---
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/Hierarchical_Planning/python/init.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import argparse
+import openreview
+from openreview import tools
+from openreview import invitations
+import config
+import datetime
+
+"""
+OPTIONAL SCRIPT ARGUMENTS
+
+	baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+ 	username - the email address of the logging in user
+	password - the user's password
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print('connecting to {0}'.format(client.baseurl))
+
+conference = config.get_conference(client)
+# March 24 midnight anywhere on earth
+conference.open_submissions(due_date = datetime.datetime(2019, 3, 25, 11, 59))
+
+
+
+
+
