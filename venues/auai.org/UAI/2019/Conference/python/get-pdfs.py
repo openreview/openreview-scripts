bc88451cf9ea28bde086ccb2cc1d734009404d7a
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-03-14 10:24:32 -0400

    py script to download all pdfs

---
+++ venues/auai.org/UAI/2019/Conference/python/get-pdfs.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import openreview
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print ('connecting to {0}'.format(client.baseurl))
+
+submission_invitation = 'auai.org/UAI/2019/Conference/-/Blind_Submission'
+submissions = openreview.tools.iterget_notes(client, invitation=submission_invitation)
+
+if not os.path.exists('../pdfs'):
+    os.makedirs('../pdfs')
+
+for submission in submissions[:20]:
+    pdf_url = '{0}{1}'.format(client.baseurl, submission.content['pdf'])
+    paper_id = submission.number
+    print ('retrieving Paper{0} at {1}'.format(paper_id, pdf_url))
+    pdf_response = requests.get(pdf_url, stream=True)
+
+    with open('../pdfs/Paper{0}.pdf'.format(paper_id), 'wb') as f:
+        f.write(pdf_response.content)
+
