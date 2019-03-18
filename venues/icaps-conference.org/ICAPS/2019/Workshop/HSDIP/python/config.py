cd09f22856d3fe5228ae621d8833e3f05aca01f8
pmandler <mandler@cs.umass.edu>
2019-03-18 10:30:24 -0400

    Extend HSDIP duedate to Sunday night (#1034)

--- venues/icaps-conference.org/ICAPS/2019/Workshop/HSDIP/python/config.py
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/HSDIP/python/config.py
@@ -13,12 +13,12 @@ def get_conference(client):
     builder.set_homepage_header({
     'title': 'Heuristics and Search for Domain-independent Planning',
     'subtitle': 'ICAPS 2019 Workshop',
-    'deadline': 'Submission Deadline: March 15, 2019',
+    'deadline': 'Submission Deadline: March 17, 2019 midnight AoE',
     'date': 'July 11-15, 2019',
     'website': 'https://icaps19.icaps-conference.org/workshops/HSDIP/index.html',
     'location': 'Berkeley, CA, USA'
     })
     builder.set_double_blind(True)
-
+    builder.set_submission_public(True)
     return builder.get_result()
 
