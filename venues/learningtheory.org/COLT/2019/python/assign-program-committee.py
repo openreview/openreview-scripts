278c9c075f95a147e13855d1037afcae62298cda
Melisa Bok <melisabok@gmail.com>
2019-03-18 09:59:53 -0400

    Fix/pc reassignment (#1041)
    * Fix bid due date
    
    * Remove signatories parameter

--- venues/learningtheory.org/COLT/2019/python/assign-program-committee.py
+++ venues/learningtheory.org/COLT/2019/python/assign-program-committee.py
@@ -74,9 +74,7 @@ if __name__ == "__main__":
                 conference.get_id(),
                 conference.get_program_chairs_id(),
                 'learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(paper_number)
-            ],
-            'signatories': ['learningtheory.org/COLT/2019/Conference/Paper{0}/Program_Committee'.format(paper_number)]
-            },
+            ]},
         parent_group_params = {
             'readers': [
                 conference.get_id(),
