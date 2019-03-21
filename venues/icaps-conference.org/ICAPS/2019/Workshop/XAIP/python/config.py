872215f6655e49392cfdc11c8c571e9b92bafcdf
Pam Mandler <mandler@cs.umass.edu>
2019-03-21 09:44:24 -0400

    XAIP extend deadline

--- venues/icaps-conference.org/ICAPS/2019/Workshop/XAIP/python/config.py
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/XAIP/python/config.py
@@ -13,12 +13,13 @@ def get_conference(client):
     builder.set_homepage_header({
     'title': 'Explainable Artificial Intelligence Planning',
     'subtitle': 'ICAPS 2019 Workshop on Explainable AI Planning (XAIP)',
-    'deadline': 'Submission Deadline: March 22, 2019',
+    'deadline': 'Submission Deadline: March 31, 2019',
     'date': 'July 11-15, 2019',
     'website': 'https://icaps19.icaps-conference.org/workshops/XAIP/',
     'location': 'Berkeley, CA, USA'
     })
     builder.set_double_blind(True)
     builder.set_submission_public(True)
+    builder.set_override_homepage(True)
     return builder.get_result()
 
