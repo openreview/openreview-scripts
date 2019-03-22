fe716d6ac8bdbcb45dc18ecd5e497776a00a3087
pmandler <mandler@cs.umass.edu>
2019-03-22 08:51:40 -0400

    SPARK change duedate to Apr 5" (#1052)

--- venues/icaps-conference.org/ICAPS/2019/Workshop/SPARK/python/config.py
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/SPARK/python/config.py
@@ -13,12 +13,13 @@ def get_conference(client):
     builder.set_homepage_header({
     'title': 'Scheduling and Planning Applications Workshop',
     'subtitle': 'ICAPS 2019 Workshop',
-    'deadline': 'Submission Deadline: March 25, 2019 23:59 GMT',
+    'deadline': 'Submission Deadline: April 5, 2019 23:59 GMT',
     'date': 'July 11-15, 2019',
     'website': 'https://icaps19.icaps-conference.org/workshops/SPARK/index.html',
     'location': 'Berkeley, CA, USA'
     })
     builder.set_double_blind(True)
     builder.set_submission_public(True)
+    builder.set_override_homepage(True)
     return builder.get_result()
 
--- venues/icaps-conference.org/ICAPS/2019/Workshop/SPARK/python/init.py
+++ venues/icaps-conference.org/ICAPS/2019/Workshop/SPARK/python/init.py
@@ -26,7 +26,7 @@ client = openreview.Client(baseurl=args.baseurl, username=args.username, passwor
 print('connecting to {0}'.format(client.baseurl))
 
 conference = config.get_conference(client)
-conference.open_submissions(due_date = datetime.datetime(2019, 3, 25, 23, 59), additional_fields = {
+conference.open_submissions(due_date = datetime.datetime(2019, 4, 5, 23, 59), additional_fields = {
     "author_identity_visibility": {
         "order": 4,
         "value-checkbox": "Reveal author identities to reviewers",
