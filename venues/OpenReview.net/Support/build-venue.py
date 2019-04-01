240cd76094f1c43853547adf58fd255ad3458f2d
Melisa Bok <melisabok@gmail.com>
2019-04-01 11:41:46 -0400

    Create a comment in the request service forum instead of sending an email directly. Improve comment notifications

--- venues/OpenReview.net/Support/build-venue.py
+++ venues/OpenReview.net/Support/build-venue.py
@@ -10,11 +10,23 @@ args = parser.parse_args()
 
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
-conference = openreview.helpers.get_conference(client, args.id)
+note = client.get_note(args.id)
+
+conference = openreview.helpers.get_conference(client, note.id)
 
 if conference.is_new():
-    subject = 'Your venue is available in OpenReview',
-    message = '''
+    readers = note.content['Contact Emails']
+    readers.append('OpenReview.net/Support')
+    comment_note = openreview.Note(
+        invitation = 'OpenReview.net/Support/-/Request' + str(note.number) + '/Comment',
+        forum = note.id,
+        replyto = note.id,
+        readers = readers,
+        writers = ['OpenReview.net/Support'],
+        signatures = ['OpenReview.net/Support'],
+        content = {
+            'title': 'Your venue is available in OpenReview',
+            'comment': '''
 Hi Program Chairs,
 
 Thanks for submitting a venue request.
@@ -28,13 +40,15 @@ Conference Program Chairs console: https://openreview.net/group?id={program_chai
 
 If you need to make a change to the information provided in your request form, please edit it directly. We will update your venue accordingly.
 
-If you need special features that are not included in your request form, please contact us at info@openreview.net and we will assist you.
+If you need special features that are not included in your request form, you can create a comment here or contact us at info@openreview.net and we will assist you.
 
 Thanks!
 
 OpenReview Team
-    '''.format(noteId = args.id, conference_id = conference.get_id(), program_chairs_id = conference.get_program_chairs_id())
-    recipients = [conference.get_program_chairs_id()]
-    client.send_mail(subject, recipients, message)
+    '''.format(noteId = note.id, conference_id = conference.get_id(), program_chairs_id = conference.get_program_chairs_id())
+        }
+    )
+
+    client.post_note(comment_note)
 
 print('Conference: ', conference.get_id())
