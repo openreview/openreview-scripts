4fead30cf14a3f4c828bd70b73784327f02dfd8c
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-04-02 16:23:14 -0400

    Add invitation for admin updates (#1070)
    * Add invitation for admin updates
    
    * updated invitation name
    
    * update writers of request forms

--- venues/OpenReview.net/Support/supportProcess.js
+++ venues/OpenReview.net/Support/supportProcess.js
@@ -43,10 +43,21 @@ function(){
       signatures: ['OpenReview.net/Support']
     }
 
+    var reviseInvitationSupportTeam = {
+      id: 'OpenReview.net/Support/-/Request' + note.number + '/Deploy',
+      super: 'OpenReview.net/Support/-/Deploy',
+      reply: {
+        referent: note.forum,
+        forum: note.forum
+      },
+      signatures: ['OpenReview.net/Support']
+    }
+
     or3client.or3request(or3client.mailUrl, openreviewMailPayload, 'POST', token)
     .then(result => or3client.or3request(or3client.mailUrl, programchairMailPayload, 'POST', token))
     .then(result => or3client.or3request(or3client.inviteUrl, commentInvitation, 'POST', token))
     .then(result => or3client.or3request(or3client.inviteUrl, reviseInvitation, 'POST', token))
+    .then(result => or3client.or3request(or3client.inviteUrl, reviseInvitationSupportTeam, 'POST', token))
     .then(result => done())
     .catch(error => done(error));
 
