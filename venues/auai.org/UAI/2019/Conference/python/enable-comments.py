4c9b3bb471721a7aacc269c3cc04186641f92f75
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-04-10 11:43:12 -0400

    enabling comments (#1073)

---
+++ venues/auai.org/UAI/2019/Conference/python/enable-comments.py
@@ -0,0 +1,19 @@
+import argparse
+import openreview
+from openreview import invitations
+import datetime
+import os
+import config
+
+if __name__ == '__main__':
+    ## Argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--baseurl', help="base url")
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+    conference = config.get_conference(client)
+
+    conference.open_comments(anonymous=True)
