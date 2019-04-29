66aa691aea145aba465b7e8c2c0d5755bacb2a68
Michael Spector <michaelspector@gmail.com>
2019-04-29 07:50:28 -0400

    experimental changes (see corresponding openreview-py PR)

--- venues/EMNLP/python/init.py
+++ venues/EMNLP/python/init.py
@@ -23,14 +23,10 @@ client = openreview.Client(baseurl=args.baseurl, username=args.username, passwor
 print('connecting to {0}'.format(client.baseurl))
 
 emnlp_request_id = 'rylllwkbKN'
-start_date = datetime.datetime(2019, 4, 2, 13, 30)
-due_date = datetime.datetime(2019, 8, 19, 23, 59)
-
-conference = openreview.helpers.get_conference(client, emnlp_request_id)
 
 author_consent_agreement = (
     '**IMPORTANT: PLEASE READ**\n\n'
-    'The workshop is collecting a research dataset containing a set of peer'
+    'The workshop is collecting a research dataset containing a set of peer '
     'reviews for research purposes. This dataset may be released to the '
     'public domain six months after the final accept/reject decisions are made. '
     'Do you agree to include your reviews in this dataset?'
@@ -39,8 +35,8 @@ author_consent_agreement = (
 additional_fields = {
     "consent": {
         "value-radio": [
-                "Yes, include my anonymized reviews in the \"workshop research dataset\".",
-                "No. Do not include my reviews in the \"workshop research dataset\"."
+            "Yes, include my anonymized reviews in the \"workshop research dataset\".",
+            "No. Do not include my reviews in the \"workshop research dataset\"."
         ],
         "default": "Yes, include my anonymized reviews in the \"workshop research dataset\".",
         "description": author_consent_agreement,
@@ -48,8 +44,11 @@ additional_fields = {
     }
 }
 
-conference.open_submissions(
-    start_date=start_date,
-    due_date=due_date,
-    additional_fields=additional_fields)
+content_override = {
+    'Custom Submission Fields': additional_fields
+}
+
+conference = openreview.helpers.get_conference(
+    client, emnlp_request_id, content_override_args=content_override)
+
 
