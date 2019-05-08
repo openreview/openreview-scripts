704e88a5f4609b78ad0519fa902c5fd3aaff1852
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-05-08 13:44:23 -0400

    add submission options to revision invitation (#1087)
    * add submission options to revision invi
    
    * run revision proces with super-user's permissions

--- venues/OpenReview.net/Support/init.py
+++ venues/OpenReview.net/Support/init.py
@@ -205,12 +205,17 @@ comment_inv = client.post_invitation(openreview.Invitation(**{
 
 remove_fields = ['Area Chairs (Metareviewers)', 'Author and Reviewer Anonymity', 'Open Reviewing Policy', 'Public Commentary']
 revision_content = {key: request_content[key] for key in request_content if key not in remove_fields}
+revision_content['Additional Submission Options'] = {
+    'order' : 18,
+    'value-regex': '[\\S\\s]{1,10000}',
+    'description': 'Configure additional options in the submission form. Valid JSON expected.'
+}
 
 revision_inv = client.post_invitation(openreview.Invitation(**{
     'id': 'OpenReview.net/Support/-/Revision',
     'readers': ['everyone'],
     'writers': [],
-    'signatures': ['OpenReview.net/Support'],
+    'signatures': ['OpenReview.net'],
     'invitees': ['everyone'],
     'process': 'revisionProcess.py',
     'reply': {
