7bfe54df74b7fec8441f5bb85111da0b5303f9ba
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-05-13 16:22:51 -0400

    post decisions using csv

--- venues/auai.org/UAI/2019/Conference/python/enable-decisions.py
+++ venues/auai.org/UAI/2019/Conference/python/enable-decisions.py
@@ -2,6 +2,7 @@ import argparse
 import openreview
 import datetime
 import config
+import csv
 
 if __name__ == '__main__':
     ## Argument handling
@@ -9,6 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--baseurl', help="base url")
     parser.add_argument('--username')
     parser.add_argument('--password')
+    parser.add_argument('decisions_file')
     args = parser.parse_args()
 
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
@@ -17,4 +19,46 @@ if __name__ == '__main__':
     conference.open_decisions(
         options = ['Accept', 'Reject'],
         start_date = datetime.datetime(2019, 5, 12, 11, 59),
-        due_date = datetime.datetime(2019, 5, 13, 23, 59))
+        due_date = datetime.datetime(2019, 5, 14, 23, 59))
+
+    paper_number_to_decisions = {int(metarev.invitation.split('Paper')[1].split('/')[0]): metarev for metarev in \
+    openreview.tools.iterget_notes(
+        client,
+        invitation = conference.id + '/-/Paper.*/Decision'
+    )}
+    failures = []
+    new_decisions = 0
+    with open(args.decisions_file) as f:
+        for row in csv.reader(f):
+            if row[0] and row[0] != 'Area Chair':
+                paper_number = int(row[1])
+                if paper_number not in paper_number_to_decisions:
+                    decision = row[6].strip()
+                    decision_text = row[7].strip()
+                    if not decision:
+                        if row[5].strip() in ['Accept (Poster)', 'Accept (Oral)']:
+                            decision = 'Accept'
+                        elif row[5].strip() in ['Weak Reject', 'Reject']:
+                            decision = 'Reject'
+                        else:
+                            failures.append(str(paper_number))
+                    else:
+                        print ('Change found for paper {0}, decision: {1} and decision-text:{2}'.format(
+                            paper_number,
+                            decision,
+                            decision_text)
+                        )
+
+                    # post decision note for this paper
+                    decision_note = client.post_note(openreview.Note(
+                        invitation = conference.id + '/-/Paper{0}/Decision'.format(paper_number),
+                        content = {
+                            'title' : 'Final Decision',
+                            'decision' : decision,
+                            'comment' : decision_text
+                        }
+                    ))
+                    new_decisions += 1
+
+    print ('Posted {0} new decisions'.format(new_decisions))
+    print ('Failed to post decisions for ', ', '.join(failures))
