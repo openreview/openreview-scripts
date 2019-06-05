798f07b56c63ae1f305b40691b94239b6ac6d86f
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-06-05 15:37:45 -0400

    recruit reviewer/ac in process

--- venues/OpenReview.net/Support/reviewerRecruitmentProcess.py
+++ venues/OpenReview.net/Support/reviewerRecruitmentProcess.py
@@ -1,7 +1,14 @@
 def process(client, note, invitation):
-    print('client:', client.baseurl)
-    print('note:', note.id)
-    print('invitation:', invitation.id)
     conference = openreview.helpers.get_conference(client, note.forum)
-
     print('Conference: ', conference.get_id())
+    note.content['invitation_email_subject'] = note.content['invitation_email_subject'].replace('{Abbreviated_Venue_Name}', conference.get_short_name())
+    note.content['invitation_email_content'] = note.content['invitation_email_content'].replace('{Abbreviated_Venue_Name}', conference.get_short_name())
+    note = client.post_note(note)
+    reviewer_names_str = note.content.get('reviewer_names', None)
+    reviewer_names = [s.strip() for s in reviewer_names_str.split(',')] if reviewer_names_str else []
+    conference.recruit_reviewers(
+        emails = [s.strip() for s in note.content['reviewer_emails'].split(',')],
+        title = note.content['invitation_email_subject'].strip(),
+        message = note.content['invitation_email_content'].strip(),
+        invitee_names = reviewer_names
+    )
