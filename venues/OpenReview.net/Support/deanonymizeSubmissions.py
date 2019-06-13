033a03a9f58d1b048d20fae167c5431971148e44
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-06-13 16:32:36 -0400

    remove anon and deanon process files

--- venues/OpenReview.net/Support/deanonymizeSubmissions.py
+++
@@ -1,4 +0,0 @@
-def process(client, note, invitation):
-    conference = openreview.helpers.get_conference(client, note.forum)
-    print('Conference: ', conference.get_id())
-    # Call the deanonymizer for blind notes
