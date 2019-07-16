dabe5bf9711d8f3e65c049f7ca35d6b135ed2677
pmandler <mandler@cs.umass.edu>
2019-07-16 11:02:46 -0400

    Update init.py

--- venues/OpenReview.net/Support/init.py
+++ venues/OpenReview.net/Support/init.py
@@ -60,7 +60,7 @@ request_content = {
         'order': 6
     },
     'Submission Start Date': {
-        'description': 'When would you (ideally) like to have your OpenReview submission portal opened? Please use following format for the GMT timezone: YYYY/MM/DD HH:MM(e.g. 2019/01/31 23:59). (Skip this if only requesting paper matching service)',
+        'description': 'When would you (ideally) like to have your OpenReview submission portal opened? Please specify the date and time in GMT using the following format: YYYY/MM/DD HH:MM(e.g. 2019/01/31 23:59). (Skip this if only requesting paper matching service)',
         'value-regex': '.*',
         'order': 7
     },
@@ -70,7 +70,7 @@ request_content = {
         'order': 8
     },
     'Venue Start Date': {
-        'description': 'What does the venue start? Please specify the starting date in GMT using the following format: YYYY/MM/DD (e.g. 2019/01/31)',
+        'description': 'What does the venue start? Please use the following format: YYYY/MM/DD (e.g. 2019/01/31)',
         'value-regex': '^[0-9]{4}\/([1-9]|0[1-9]|1[0-2])\/([1-9]|0[1-9]|[1-2][0-9]|3[0-1])(\s+)?((2[0-3]|[01][0-9]|[0-9]):[0-5][0-9])?(\s+)?$',
         'order': 9
     },
