f91eaa4f15237ff0f3249fc4e21848a4cd42a498
Pam Mandler <mandler@cs.umass.edu>
2019-08-28 08:18:55 -0400

    rest of files for first draft

---
+++ venues/reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019/process/claimProcess.py
@@ -0,0 +1,20 @@
+def process(client, note, invitation):
+    conference_id = 'reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019'
+    # create and post claim_hold note
+    hold_invite_id = invitation.id.replace('Claim', 'Claim_Hold')
+    client.post_note(openreview.Note(
+        id=None,
+        original=None,
+        invitation=hold_invite_id,
+        forum=note.forum,
+        signatures=[conference_id],
+        writers=[conference_id],
+        readers=['everyone'],
+        content={"title": "Claimed by "+ note.content['institution']}
+        ))
+
+    # send confirmation email
+    submission = client.get_note(invitation.forum)
+    msg = 'Your claim to NeurIPS 2019 Reproducibility Challenge for paper {title} has been posted.'.format(
+        title=submission.content['title'])
+    client.send_mail("NeurIPS Reproducibility Claim", [note.tauthor], msg)
