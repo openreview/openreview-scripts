a75e426831fe5f1c027731afdb875b7c00d8fb9b
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-09-11 18:21:32 +0530

    add ac and buddy as each other's readers

--- venues/ICLR.cc/2020/Conference/python/buddy_ac.py
+++ venues/ICLR.cc/2020/Conference/python/buddy_ac.py
@@ -43,7 +43,11 @@ if __name__ == '__main__':
 
     map_meta_review_invitations = {invitation.id.split('Paper')[1].split('/')[0]: invitation for invitation in openreview.tools.iterget_invitations(client, regex = conference_id + '/Paper[0-9]+/-/Meta_Review')}
 
-    map_paper_ac_groups = {group.id.split('Paper')[1].split('/')[0]: group for group in openreview.tools.iterget_groups(client, regex = conference_id + '/Paper[0-9]+/Area_Chairs$')}
+    map_paper_ac_parent_groups = {group.id.split('Paper')[1].split('/')[0]: group for group in openreview.tools.iterget_groups(client, regex = conference_id + '/Paper[0-9]+/Area_Chairs$')}
+
+    map_paper_ac_individual_groups = {group.id.split('Paper')[1].split('/')[0]: group for group in openreview.tools.iterget_groups(client, regex = conference_id + '/Paper[0-9]+/Area_Chair1$')}
+
+    map_paper_buddy_ac_groups = {group.id.split('Paper')[1].split('/')[0]: group for group in openreview.tools.iterget_groups(client, regex = conference_id + '/Paper[0-9]+/' + buddy_ac_individual_group_name + '$')}
 
     submissions = conference.get_submissions()
     for paper in submissions:
@@ -51,29 +55,36 @@ if __name__ == '__main__':
         individual_buddy_group_id = conference_id + '/Paper' + paper_number + '/' + buddy_ac_individual_group_name
 
         ## Create buddy ac group for this paper
-        individual_buddy_group = client.post_group(
-            openreview.Group(
-                id = individual_buddy_group_id,
-                readers = [
-                    conference_id,
-                    conference.get_program_chairs_id(),
-                    individual_buddy_group_id],
-                nonreaders = [conference_id + '/Paper' + paper_number + '/Authors'],
-                signatories = [individual_buddy_group_id],
-                signatures = [conference.get_id()],
-                writers = [conference.get_id()],
-                members = []
+        if paper_number not in map_paper_buddy_ac_groups:
+            individual_buddy_group = client.post_group(
+                openreview.Group(
+                    id = individual_buddy_group_id,
+                    readers = [
+                        conference_id,
+                        conference.get_program_chairs_id(),
+                        conference.get_area_chairs_id(paper_number),
+                        individual_buddy_group_id],
+                    nonreaders = [conference_id + '/Paper' + paper_number + '/Authors'],
+                    signatories = [individual_buddy_group_id],
+                    signatures = [conference.get_id()],
+                    writers = [conference.get_id()],
+                    members = []
+                )
             )
-        )
 
-        # Add paper's buddy AC as member to paper's AC group
-        paper_ac_group = map_paper_ac_groups[paper_number]
-        client.add_members_to_group(paper_ac_group, individual_buddy_group_id)
+        paper_ac_individual_group = map_paper_ac_individual_groups.get(paper_number)
+        if paper_ac_individual_group and (individual_buddy_group_id not in paper_ac_individual_group.readers):
+            paper_ac_individual_group.readers.append(individual_buddy_group_id)
+            client.post_group(paper_ac_individual_group)
+
+        # Add paper's buddy AC as member to paper's AC parent group
+        paper_ac_parent_group = map_paper_ac_parent_groups[paper_number]
+        client.add_members_to_group(paper_ac_parent_group, individual_buddy_group_id)
 
         # Add paper's buddy AC as non-invitee for paper's meta-review invitation
         paper_meta_rev_invitation = map_meta_review_invitations.get(paper_number, None)
         if (paper_meta_rev_invitation):
-            paper_meta_rev_invitation.noninvitees = individual_buddy_group_id
+            paper_meta_rev_invitation.noninvitees = [individual_buddy_group_id]
             client.post_invitation(paper_meta_rev_invitation)
 
         ## Post AC & AC-Buddy only Comment invitation for this paper
