81b093b4b4b34b70e6bda05aa010f157e316c157
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-09-11 18:51:37 +0530

    fixes as per PR comments

--- venues/ICLR.cc/2020/Conference/python/buddy_ac.py
+++ venues/ICLR.cc/2020/Conference/python/buddy_ac.py
@@ -92,55 +92,45 @@ if __name__ == '__main__':
             id = conference_id + '/Paper' + paper_number + '/-/Area_Chair_Only_Comment',
             readers = [
                 conference.get_area_chairs_id(number = paper_number),
-                individual_buddy_group_id,
                 conference_id
             ],
             writers = [conference_id],
             signatures = [conference_id],
             invitees = [
-                conference.get_area_chairs_id(number = paper_number), individual_buddy_group_id
+                conference.get_area_chairs_id(number = paper_number)
             ],
             reply = {
-                "forum" : paper.forum,
-                "readers": {
-                    "description": "Select all user groups that should be able to read this comment.",
-                    "values": [
-                        "ICLR.cc/2020/Conference/Paper" + paper_number + "/Area_Chairs",
-                        "ICLR.cc/2020/Conference/Paper" + paper_number + "/Buddy_Area_Chair1",
+                'forum' : paper.forum,
+                'readers': {
+                    'description': 'Select all user groups that should be able to read this comment.',
+                    'values': [
+                        conference.get_area_chairs_id(number = paper_number),
                         conference.get_program_chairs_id()
                     ]
                     },
-                "writers": {
-                    "values-regex": "ICLR.cc/2020/Conference/Paper6/(Buddy_)*Area_Chair[0-9]+",
-                    "description": "How your identity will be displayed."
+                'writers': {
+                    'values-regex': conference_id + '/Paper' + + '/(Buddy_)*Area_Chair[0-9]+',
+                    'description': 'How your identity will be displayed.'
                 },
-                "signatures": {
-                    "values-regex": "ICLR.cc/2020/Conference/Paper6/(Buddy_)*Area_Chair[0-9]+",
-                    "description": "How your identity will be displayed."
+                'signatures': {
+                    'values-regex': conference_id + '/Paper6/(Buddy_)*Area_Chair[0-9]+',
+                    'description': 'How your identity will be displayed.'
                 },
-                "content": {
-                    "comment": {
-                        "value-regex": "[\\S\\s]{1,5000}",
-                        "required": True,
-                        "order": 1,
-                        "description": "Your comment or reply (max 5000 characters)."
+                'content': {
+                    'comment': {
+                        'value-regex': '[\\S\\s]{1,5000}',
+                        'required': True,
+                        'order': 1,
+                        'description': 'Your comment or reply (max 5000 characters).'
                     },
-                    "title": {
-                        "value-regex": ".{1,500}",
-                        "required": True,
-                        "order": 0,
-                        "description": "Brief summary of your comment."
+                    'title': {
+                        'value-regex': '.{1,500}',
+                        'required': True,
+                        'order': 0,
+                        'description': 'Brief summary of your comment.'
                     }
                 }
             },
             process = 'buddyAcCommentProcess.js'
         )
         client.post_invitation(ac_conversation_invitation)
-
-    all_anon_revs = openreview.tools.iterget_groups(client, regex = 'ICLR.cc/2020/Conference/Paper[0-9]+/AnonReviewer[0-9]+')
-    for anon_rev in all_anon_revs:
-        paper_number = str(anon_rev.id.split('Paper')[1].split('/')[0])
-        individual_buddy_group_id = conference_id + '/Paper' + paper_number + '/' + buddy_ac_individual_group_name
-        if individual_buddy_group_id not in anon_rev.readers:
-            anon_rev.readers.append(individual_buddy_group_id)
-        upd_anon = client.post_group(anon_rev)
