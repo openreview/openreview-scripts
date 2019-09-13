d4df4e1d3e9b2f125e28f953f3be0dec9b06849a
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-09-13 21:22:12 +0530

    updated recruit scripts

--- venues/ICLR.cc/2020/Conference/python/recruit-area-chairs.py
+++ venues/ICLR.cc/2020/Conference/python/recruit-area-chairs.py
@@ -11,39 +11,61 @@ if __name__ == '__main__':
 
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
-    conference = openreview.helpers.get_conference(client, 'Skli7GteaN')
+    conference = openreview.helpers.get_conference(client, 'SkxpQPWdA4')
 
-    title = '[ICLR 2020]: Invitation to Review'
-    message = '''Dear {name},
+    title = '[ICLR2020]: Invitation to serve as an Area Chair'
+    message = '''
+TL;DR: We would be honoured if you would agree to serve as an Area Chair for the 2020 International Conference on Learning Representations (ICLR2020). Please accept by clicking {accept_url} .
 
-        You have been nominated by the program chair committee of ICLR 2020 to serve as an area chair.  As a respected researcher in the area, we hope you will accept and help us make the conference a success.
+Dear {name},
 
-        Reviewers are also welcome to submit papers, so please also consider submitting to the conference!
+Please accept our invitation to serve as an area chair for ICLR2020, which will be held in April 2020 in Addis Ababa, Ethiopia.
 
-        We will be using OpenReview.net and a reviewing process that we hope will be engaging and inclusive of the whole community.
+Areas Chairs at ICLR serve many roles: you guide the reviewing process to enable a rigorous and respectful review process for submitted papers; you are the principal decision maker in the final status of papers; and you are also an action editor of the conference proceedings, which is one of its key outputs. We believe that your experience makes you ideal to serve as an AC and take on these roles. We hope you will accept.
 
-        The success of the conference depends on the quality of the reviewing process and ultimately on the quality and dedication of the reviewers. We hope you will accept our invitation.
+Here are the key dates that we expect to work towards for ICLR2020:
+Paper submissions close: Wednesday, 25 September 2019
+Review period opens: Thursday, 3 October 2019
+Reviewing period ends: Friday, 18 October 2019
+Substitute reviewing period starts: Monday, 21 October 2019
+Substitute reviewing period ends: Friday, 1 November 2019
+Release Reviews: Monday, 4 November 2019
+Discussion period starts: Monday, 4 November 2019
+Discussion period ends: Friday, 22 November 2019
+PC/AC Calibrations ends: Friday, 12 December 2019
 
-        To ACCEPT the invitation, please click on the following link:
+The entire process should take around 3 months to complete.
+At the initial assignment phase, we will ask you to look through your papers to alert the programme chairs of any papers that don’t meet the submission criteria and to check with your reviewers that their assignments are appropriate.
+Because there are always late reviewers and some that can’t contribute in the end, we have an explicit period in which substitute reviewers will need to be found and assigned (this period is not communicated to reviewers).
+Most of the AC work is in the month of November where the discussion and clarification on papers’ claims and contributions are held. For borderline cases, we will reach out to ask with help in calibrating reviews in December (in the week after NeurIPS).
 
-        {accept_url}
+To ACCEPT the invitation, please click on the following link:
 
-        To DECLINE the invitation, please click on the following link:
+{accept_url}
 
-        {decline_url}
+To DECLINE the invitation, please click on the following link:
 
-        Please answer within 10 days.
+{decline_url}
 
-        If you accept, please make sure that your OpenReview account is updated and lists all the emails you are using.  Visit http://openreview.net/profile after logging in.
+If you accept, please make sure that your OpenReview account is updated and lists all the emails you are using.  Visit http://openreview.net/profile after logging in.
 
-        If you have any questions, please contact us at info@openreview.net.
+If there are any other questions, please don’t hesitate to reach out to us at iclr2020programchairs@googlegroups.com.
 
-        Cheers!
+Thank you as always for your ongoing service to our community, and we look forward to working with you to make this the best ICLR yet.
 
-        Program Chairs
+ICLR2020 Programme Chairs,
+Shakir Mohamed, Dawn Song, Kyungyhun Cho, Martha White
     '''
 
+    # details = ''''''
 
-    emails = ['ac_iclr2020@mail.com', 'ac2_iclr2020@mail.com', '~Melisa_Bok1']
+    # emails = []
+    # names = []
+    # for detail in details.split('\n'):
+    #     name, email = [i.strip() for i in detail.split(',')]
+    #     emails.append(email)
+    #     names.append(name)
 
-    conference.recruit_reviewers(emails= emails, reviewers_name = 'Area_Chairs', title = title , message = message)
+    # conference.recruit_reviewers(emails= emails, reviewers_name = 'Area_Chairs', title = title , message = message, invitee_names = names)
+
+    conference.recruit_reviewers(reviewers_name = 'Area_Chairs', title = title , message = message, remind = True)
--- venues/ICLR.cc/2020/Conference/python/recruit-reviewers.py
+++ venues/ICLR.cc/2020/Conference/python/recruit-reviewers.py
@@ -1,5 +1,6 @@
 import openreview
 import argparse
+import re
 
 if __name__ == '__main__':
     ## Argument handling
@@ -11,39 +12,72 @@ if __name__ == '__main__':
 
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
-    conference = openreview.helpers.get_conference(client, 'Skli7GteaN')
+    conference = openreview.helpers.get_conference(client, 'SkxpQPWdA4')
 
     title = '[ICLR 2020]: Invitation to Review'
     message = '''Dear {name},
 
-        You have been nominated by the program chair committee of ICLR 2020 to serve as a reviewer.  As a respected researcher in the area, we hope you will accept and help us make the conference a success.
+Please accept our invitation to serve as a Reviewer for ICLR2020, which will be held in April 2020 in Addis Ababa, Ethiopia. See call for papers at: www.iclr.cc. ICLR has grown immensely, and the quality of the conference relies heavily on the quality of the reviews. As a recognized researcher, we highly value your input and hope you can contribute to the review process of ICLR 2020. We are planning for a load of at most 6 papers, so the commitment should not be too onerous.
 
-        Reviewers are also welcome to submit papers, so please also consider submitting to the conference!
+Here are the key dates that we expect to work towards for ICLR2020:
+Paper submissions close: Wednesday, 25 September 2019
+Review period opens: Thursday, 3 October 2019
+Reviewing period ends: Friday, 18 October 2019
+Release Reviews: Monday, 4 November 2019
+Discussion period starts: Monday, 4 November 2019
+Discussion period ends: Friday, 22 November 2019
+Notifications on Thursday, 19 December 2019.
 
-        We will be using OpenReview.net and a reviewing process that we hope will be engaging and inclusive of the whole community.
+The review process will be similar to last year. There will be three weeks to review and three weeks for discussion. We will be using OpenReview throughout the review process. The submissions are double-blind, and only your public review will be visible to everyone -- not your name nor any private discussion about the paper. We will provide Reviewer Guidelines, to support you in writing your reviews.
 
-        The success of the conference depends on the quality of the reviewing process and ultimately on the quality and dedication of the reviewers. We hope you will accept our invitation.
+To ACCEPT the invitation, please click on the following link:
 
-        To ACCEPT the invitation, please click on the following link:
+{accept_url}
 
-        {accept_url}
+To DECLINE the invitation, please click on the following link:
 
-        To DECLINE the invitation, please click on the following link:
+{decline_url}
 
-        {decline_url}
+We would appreciate an answer as soon as possible.
 
-        Please answer within 10 days.
+If you have not already signed up on openreview.net, please do so. Also, make sure to add the address you received this email on to your OpenReview profile.
 
-        If you accept, please make sure that your OpenReview account is updated and lists all the emails you are using.  Visit http://openreview.net/profile after logging in.
+If you have any questions, please don’t hesitate to reach out to us at iclr2020programchairs@googlegroups.com .
 
-        If you have any questions, please contact us at info@openreview.net.
+Thank you as always for your ongoing service to our community, and we look forward to working with you to make this the best ICLR yet.
 
-        Cheers!
+ICLR2020 Programme Chairs,
+Shakir Mohamed, Dawn Song, Kyungyhun Cho, Martha White
+'''
 
-        Program Chairs
-    '''
+    # suggested_group = client.get_group('ICLR.cc/2020/Conference/Reviewers/Suggested')
+    # suggested_members = suggested_group.members
+    # new_member_list = []
+    # removal_list = []
+    # pattern = re.compile("^~.*[0-9]+$")
+    # for index, member in enumerate(suggested_members):
+    #     invited_reviewers_groups = client.get_groups(member = member, regex = 'ICLR.cc/2020/Conference/Reviewers/Invited')
+    #     ac_groups = client.get_groups(member = member, regex = 'ICLR.cc/2020/Conference/Area_Chairs')
+    #     if member.startswith("~"):
+    #         if pattern.match(member):
+    #             if len(invited_reviewers_groups) == 0 and len(ac_groups) == 0:
+    #                 new_member_list.append(member)
+    #                 print(index, '+', member)
+    #             else:
+    #                 print(index, '.')
+    #         else:
+    #             print (index, '-', member)
+    #             removal_list.append(member)
+    #     else:
+    #         if '@' in member and len(invited_reviewers_groups) == 0 and len(ac_groups) == 0:
+    #             new_member_list.append(member)
+    #             print(index, '+', member)
+    #         else:
+    #             print(index, '.')
 
+    # print (len(new_member_list), " :: ", new_member_list)
+    # print (len(removal_list), " :: ", removal_list)
 
-    emails = ['reviewer_iclr2020@mail.com', '~Mohit_Uniyal1']
+    # conference.recruit_reviewers(emails = new_member_list, title = title , message = message)
 
-    conference.recruit_reviewers(emails= emails, title = title , message = message)
+    conference.recruit_reviewers(remind=True, title = title , message = message)
