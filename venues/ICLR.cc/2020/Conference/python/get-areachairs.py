3736883d780cb1628807a7f03ec876c0949b3a95
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-10-07 10:33:43 -0400

    minor label updates

--- venues/ICLR.cc/2020/Conference/python/get-areachairs.py
+++ venues/ICLR.cc/2020/Conference/python/get-areachairs.py
@@ -41,27 +41,27 @@ if __name__ == "__main__":
 
     if paper_number:
         paper_url = 'ICLR.cc/2020/Conference/Paper{}'.format(paper_number)
-        reviewer_group = client.get_group(id=paper_url + '/Area_Chairs')
-        anon_reviewers_groups = client.get_groups(regex=paper_url+'/Area_Chair[0-9]+$')
-        print('Reviewers for paper {paper_number}:'.format(paper_number=paper_number))
-        for reviewer in reviewer_group.members:
-            anon_reviewers = [g for g in anon_reviewers_groups if reviewer in g.members]
-            if anon_reviewers:
-                anon_reviewer = anon_reviewers[0]
+        ac_group = client.get_group(id=paper_url + '/Area_Chairs')
+        individual_ac_groups = client.get_groups(regex=paper_url+'/Area_Chair[0-9]+$')
+        print('Area Chair for paper {paper_number}:'.format(paper_number=paper_number))
+        for reviewer in ac_group.members:
+            individual_ac = [g for g in individual_ac_groups if reviewer in g.members]
+            if individual_ac:
+                anon_reviewer = individual_ac[0]
                 print('{reviewer} - (Area_Chair{number})'.format(reviewer=reviewer, number=anon_reviewer.id[-1]))
             else:
-                print('AnonReviewer not found for {reviewer}, something wrong is here'.format(reviewer=reviewer))
+                print('Area Chair not found for {reviewer}, something wrong is here'.format(reviewer=reviewer))
 
     if areachair_name:
-        reviewer_groups = client.get_groups(member=areachair_name, regex='ICLR.cc/2020/Conference/Paper.*/Area_Chairs$')
-        anon_reviewers_groups = client.get_groups(member=areachair_name, regex='ICLR.cc/2020/Conference/Paper.*/Area_Chair[0-9]+$')
-        print('Papers assigned to reviewer {reviewer}:'.format(reviewer=areachair_name))
-        for group in reviewer_groups:
+        ac_groups = client.get_groups(member=areachair_name, regex='ICLR.cc/2020/Conference/Paper.*/Area_Chairs$')
+        individual_ac_groups = client.get_groups(member=areachair_name, regex='ICLR.cc/2020/Conference/Paper.*/Area_Chair[0-9]+$')
+        print('Papers assigned to Area Chair {ac}:'.format(ac=areachair_name))
+        for group in ac_groups:
             number = [token for token in group.id.split('/') if token.startswith('Paper')][0].replace('Paper', '')
-            anon_reviewers = [g for g in anon_reviewers_groups if g.id.startswith('ICLR.cc/2020/Conference/Paper{number}'.format(number=number))]
+            anon_reviewers = [g for g in individual_ac_groups if g.id.startswith('ICLR.cc/2020/Conference/Paper{number}'.format(number=number))]
             if anon_reviewers:
                 anon_reviewer = anon_reviewers[0]
                 print('Paper {paper_number} - (Area_Chair{number})'.format(paper_number=number, number=anon_reviewer.id[-1]))
             else:
-                print('AnonReviewer not found for {number}, something wrong is here'.format(number=number))
+                print('Area Chair not found for {number}, something wrong is here'.format(number=number))
 
