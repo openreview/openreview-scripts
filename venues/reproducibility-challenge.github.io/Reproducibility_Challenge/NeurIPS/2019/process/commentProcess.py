4cca8653dd52de976966ee9b609866fcc5215985
Pam Mandler <mandler@cs.umass.edu>
2019-10-18 08:58:32 -0400

    Make one notification invite for the venue (not one per paper) and use new tag retrieval options.

--- venues/reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019/process/commentProcess.py
+++ venues/reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019/process/commentProcess.py
@@ -1,34 +1,25 @@
 def process(client, note, invitation):
     SHORT_PHRASE = "NeurIPS Reproducibility Challenge"
     # get all notification tags for this paper
-    notify_inv = invitation.id.replace('Comment', 'Comment_Notification')
+    notify_inv = invitation.id.replace('Comment', 'Notification_Subscription')
     # TODO change to iterget
     notifications = client.get_tags(invitation=notify_inv)
 
-    email_list = []
-    all_notifiers = []
-    for tag in notifications:
-        if tag.tag == 'Immediate':
-            email_list.append(tag.signatures[0])
-        all_notifiers.append(tag.signatures[0])
+    email_list = [tag.signatures[0] for tag in notifications if tag.tag == 'Immediate']
+    all_notifiers = [tag.signatures[0] for tag in notifications]
 
     # get submission author and add to email list if author hasn't set a notification tag
     forumNote = client.get_note(id=note.forum)
+
+    # add comment author to email list if does not have a tag
+    if note.signatures[0] and note.signatures[0] not in all_notifiers:
+        email_list.append[note.signatures[0]]
 
     if email_list:
         # send email to those in the immediate notification group
         subject = '[' + SHORT_PHRASE + '] Paper Title: "' + forumNote.content['title'] + '" received a comment'
         formatted_msg = 'The NeurIPS submission titled "' + forumNote.content['title'] + '" has received a comment.\n\nComment title: ' \
                         + note.content['title'] + '\n\nComment: ' + note.content['comment'] + '\n\nTo view the comment, click here: ' + \
-                        client.baseurl + '/forum?id=' + note.forum + '&noteId=' + note.id + '\n\nIf you do not wish to receive notifications for comments on this paper, visit the link above and change the Notification frequency.'
+                        client.baseurl + '/forum?id=' + note.forum + '&noteId=' + note.id + '\n\nIf you wish to change your email notification preferences for comments on this paper, log into OpenReview.net, visit the link above and change the Notification Subscription frequency.'
 
         response = client.send_mail(subject, email_list, formatted_msg)
-
-    # if comment author doesn't have notifications set up,
-    # send email on how to set notifications
-    if note.signatures[0] not in all_notifiers:
-        print("comment author needs instructions")
-        subject = '[' + SHORT_PHRASE + '] Paper Title: "' + forumNote.content['title'] + '" received your comment'
-        message = "If you wish to receive email when people comment on this paper, go to "+client.baseurl + '/forum?id=' + note.forum + \
-            " and select the Comment Notification frequency."
-        response = client.send_mail(subject, [note.signatures[0]], message)
--- venues/reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019/python/comment_notification.py
+++ venues/reproducibility-challenge.github.io/Reproducibility_Challenge/NeurIPS/2019/python/comment_notification.py
@@ -13,58 +13,57 @@ client = openreview.Client(baseurl=args.baseurl, username=args.username, passwor
 print("Connected to "+client.baseurl)
 conference_id = 'NeurIPS.cc/2019/Reproducibility_Challenge'
 
-# for each submission, create group (so sub invites can be published)
 # create invitation for notification frequency tags
+notify_inv = openreview.Invitation(
+    id='{}/-/Notification_Subscription'.format(conference_id),
+    readers=['everyone'],
+    invitees=['~'],
+    writers=[conference_id],
+    signatures=[conference_id],
+    details={'writable': True},
+    multiReply=False,
+    reply={
+        'invitation': conference_id+"/-/NeurIPS_Submission",
+        'content': {
+            "tag": {
+                "required": True,
+                "description": "Select Frequency",
+                "value-dropdown": [
+                    "Immediate",
+                    "Daily",
+                    "Weekly",
+                    "Never"
+                ]
+            }
+        },
+        'signatures': {
+            'description': 'Your authorized identity to be associated with the above content.',
+            'values-regex': '~.*'
+        },
+        'readers': {
+            'description': 'The users who will be allowed to read the above content.',
+            'values': [conference_id, conference_id+'/Program_Chairs', '{signatures}']
+        },
+        'writers': {
+            'values-copied': [conference_id, '{signatures}']
+        }
+    }
+)
+client.post_invitation(notify_inv)
+
+# for each submission, create group (so sub invites can be published)
 # create invite for comment
 notes = tools.iterget_notes(client,invitation=conference_id+"/-/NeurIPS_Submission")
 for note in notes:
     # need paper group to publish sub-groups
     paper_group = client.post_group(openreview.Group(
-        id='{conference_id}/NeurIPS{number}'.format(conference_id=conference_id, number=note.number),
+        id='{conference_id}/{number}'.format(conference_id=conference_id, number=note.number),
         signatures=[conference_id], signatories=[conference_id],
         readers=[conference_id], writers=[conference_id]))
 
-    notify_inv = openreview.Invitation(
-        id='{}/NeurIPS{}/-/Comment_Notification'.format(conference_id, note.number),
-        readers=['everyone'],
-        invitees=['~'],
-        writers=[conference_id],
-        signatures=[conference_id],
-        details= {'writable':True},
-        multiReply= False,
-        reply={
-            'forum': note.forum,
-            'replyto': note.forum,
-            'content': {
-                "tag": {
-                    "required": True,
-                    "description": "Frequency",
-                    "value-dropdown": [
-                        "Immediate",
-                        "Daily",
-                        "Weekly",
-                        "Never"
-                    ]
-                }
-            },
-            'signatures': {
-                'description': 'Your authorized identity to be associated with the above content.',
-                'values-regex': '~.*'
-            },
-            'readers': {
-                'description': 'The users who will be allowed to read the above content.',
-                'values': [conference_id]
-            },
-            'writers': {
-                'values-copied': [conference_id, '{signatures}']
-            }
-        }
-    )
-    client.post_invitation(notify_inv)
-
     # add comment invite
     comment_inv = openreview.Invitation(
-        id='{}/NeurIPS{}/-/Comment'.format(conference_id, note.number),
+        id=paper_group.id+'/-/Comment',
         readers=['everyone'],
         invitees=['~'],
         writers=[conference_id],
