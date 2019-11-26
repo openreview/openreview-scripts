f7fb123ac86062d2bffb8aa653f68f9909245392
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-11-25 20:30:41 -0500

    disallow reviewers from commenting anymore

--- venues/ICLR.cc/2020/Conference/python/meta_review_process.js
+++ venues/ICLR.cc/2020/Conference/python/meta_review_process.js
@@ -10,6 +10,18 @@ function(){
             var invitation = result.invitations[0];
             invitation.expdate = Date.now();
             return or3client.or3request(or3client.inviteUrl, invitation, 'POST', token);
+        })
+        .then(function(result) {
+            return or3client.or3request(or3client.inviteUrl + '?id=' + CONFERENCE_ID + '/Paper' + forum.number + '/-/Official_Comment', {}, 'GET', token)
+        })
+        .then(function(result) {
+            var comment_invitation = result.invitations[0];
+            comment_invitation.reply.signatures['values-regex'] = 'ICLR.cc/2020/Conference/Paper' + forum.number + '/Area_Chair[0-9]+|ICLR.cc/2020/Conference/Program_Chairs|ICLR.cc/2020/Conference/Paper' + forum.number + '/Buddy_Area_Chair1';
+            var invitee_list = comment_invitation.invitees.filter(function(invitee){
+                return invitee !== CONFERENCE_ID+'/Paper'+forum.number+'/Reviewers'
+            });
+            comment_invitation.invitees = invitee_list;
+            return or3client.or3request(or3client.inviteUrl, comment_invitation, 'POST', token);
         });
     })
     .then(result => done())
