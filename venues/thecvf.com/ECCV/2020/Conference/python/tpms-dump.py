2f5e352de89bd614e6e9d2988e6c9e09f4d3519e
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-03-05 15:00:52 +0530

    minor updates to file names

--- venues/thecvf.com/ECCV/2020/Conference/python/tpms-dump.py
+++ venues/thecvf.com/ECCV/2020/Conference/python/tpms-dump.py
@@ -3,19 +3,16 @@
 
     - in csv format: ReviewerEmail,FirstName,LastName,<comma-separated-additional-emails>
 
-2) Papers: Once the deadline has passed you package and provide TPMS
-with all of the submissions, in pdf format. The simplest, given the
-size of that file, is probably for me to download that package from
-some server.
+2) Area Chairs: The reviewer list should be in csv file.
 
-  - papers should be given a numeric ID and files should be named:
-      paperID.pdf (e.g., paper134.pdf)
+    - in csv format: AreaChairEmail,FirstName,LastName,<comma-separated-additional-emails>
 '''
 
 import argparse
 import openreview
 import csv
 from tqdm import tqdm
+import os
 
 def get_valid_profiles(client, group_id):
     group = client.get_group(group_id)
@@ -34,40 +31,45 @@ args = parser.parse_args()
 
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
+if not os.path.exists('eccv2020_users'):
+	os.makedirs('eccv2020_users')
+
 # At this point, all reviewers & ACs should have been
 # converted to profile IDs and deduplicated.
 
-valid_reviewer_profiles = get_valid_profiles(client, 'thecvf.com/ECCV/2020/Conference/Reviewers')
+valid_reviewer_profiles = {'reviewers' : get_valid_profiles(client, 'thecvf.com/ECCV/2020/Conference/Reviewers')}
 
-valid_ac_profiles = get_valid_profiles(client, 'thecvf.com/ECCV/2020/Conference/Area_Chairs')
+valid_ac_profiles = {'areachairs' : get_valid_profiles(client, 'thecvf.com/ECCV/2020/Conference/Area_Chairs')}
 
-user_profiles = valid_ac_profiles + valid_reviewer_profiles
+all_user_profiles = [valid_ac_profiles, valid_reviewer_profiles]
 
 rows = []
 
-for profile in tqdm(user_profiles):
-    row = []
-    preferred_email = profile.content.get('preferred_email', None)
-    emails = profile.content.get('emails', [])
-    if not emails:
-        print('No emails in profile {}'.format(profile.id))
-    else:
-        if not preferred_email:
-            preferred_email = emails[0]
-        row.append(preferred_email)
-        name = openreview.tools.get_preferred_name(profile)
-        name_parts = name.split(' ')
-        firstname = name_parts[0]
-        lastname = name_parts[-1]
-        row.append(firstname)
-        row.append(lastname)
-        for email in [e for e in emails if e != preferred_email]:
-            row.append(email)
-        rows.append(row)
+for profile_group in all_user_profiles:
+    user_type, user_profiles = list(profile_group.items())[0]
+    for profile in tqdm(user_profiles):
+        row = []
+        preferred_email = profile.content.get('preferred_email', None)
+        emails = profile.content.get('emails', [])
+        if not emails:
+            print('No emails in profile {}'.format(profile.id))
+        else:
+            if not preferred_email:
+                preferred_email = emails[0]
+            row.append(preferred_email)
+            name = openreview.tools.get_preferred_name(profile)
+            name_parts = name.split(' ')
+            firstname = name_parts[0]
+            lastname = name_parts[-1]
+            row.append(firstname)
+            row.append(lastname)
+            for email in [e for e in emails if e != preferred_email]:
+                row.append(email)
+            rows.append(row)
 
-with open('../data/ECCV2020-tpms-reviewers-dump.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(rows)
+    with open('./eccv2020_users/tpms-{}-dump.csv'.format(user_type), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
 
 
 
