d918385b151d1084a7b621665c9716c2ec091035
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-05-18 22:41:31 +0530

    updated scipt

--- venues/thecvf.com/ECCV/2020/Conference/python/emergency_reviews.py
+++ venues/thecvf.com/ECCV/2020/Conference/python/emergency_reviews.py
@@ -14,21 +14,10 @@ if __name__ == '__main__':
     client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
     reviewer_group = client.get_group('thecvf.com/ECCV/2020/Conference/Reviewers')
-    emergency_reviewer_group = client.post_group(openreview.Group(
-        id = 'thecvf.com/ECCV/2020/Conference/Emergency_Reviewers',
-        signatures = ['thecvf.com/ECCV/2020/Conference'],
-        signatories = ['thecvf.com/ECCV/2020/Conference/Emergency_Reviewers'],
-        readers = [
-            'thecvf.com/ECCV/2020/Conference',
-            'thecvf.com/ECCV/2020/Conference/Area_Chairs',
-            'thecvf.com/ECCV/2020/Conference/Emergency_Reviewers'],
-        writers = ['thecvf.com/ECCV/2020/Conference',
-        'thecvf.com/ECCV/2020/Conference/Area_Chairs'],
-        members = []
-    ))
-    print ('Posted group: {}\n'.format(emergency_reviewer_group.id))
+    emergency_reviewer_group = client.get_group('thecvf.com/ECCV/2020/Conference/Emergency_Reviewers')
+    print ('Found emergency group {} with {} members\n'.format(emergency_reviewer_group.id, len(emergency_reviewer_group.members)))
 
-    emergency_load_invitation = client.post_invitation(openreview.Invitation(
+    emergency_load_invitation = openreview.Invitation(
         id='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Custom_Max_Papers',
         signatures=['thecvf.com/ECCV/2020/Conference'],
         readers=[
@@ -61,10 +50,13 @@ if __name__ == '__main__':
                 }
             }
         }
-    ))
-    print('Posted invitation:{}\n'.format(emergency_load_invitation.id))
-    print('Deleting edges for invitation {}\n'.format(emergency_load_invitation.id))
-    client.delete_edges(invitation=emergency_load_invitation.id)
+    )
+    # emergency_load_invitation = client.post_invitation(emergency_load_invitation)
+    # print('Posted invitation:{}\n'.format(emergency_load_invitation.id))
+
+    # DO SEPARATELY
+    # print('Deleting edges for invitation {}\n'.format(emergency_load_invitation.id))
+    # client.delete_edges(invitation=emergency_load_invitation.id)
 
     confirmations = {}
     confirmation_notes = openreview.tools.iterget_notes(
@@ -85,7 +77,7 @@ if __name__ == '__main__':
     profile_map = {}
     tildes = []
 
-    for member in reviewer_group.members:
+    for member in emergency_reviewer_group.members:
         if member.startswith('~'):
             tildes.append(member)
 
@@ -97,7 +89,7 @@ if __name__ == '__main__':
     emergency_load_edges = []
     total_review_capacity = 0
 
-    for reviewer in tqdm(reviewer_group.members):
+    for reviewer in tqdm(emergency_reviewer_group.members):
         review_capacity = 0
 
         profile = profile_map.get(reviewer, None)
@@ -128,18 +120,16 @@ if __name__ == '__main__':
                     weight = review_capacity
                 )
                 emergency_load_edges.append(edge)
-                emergency_reviewer_group.members.append(profile.id)
-
-    x = client.post_group(emergency_reviewer_group)
-    print('Created updated emergency reviewers group\n')
 
     print('Posting {0} edges'.format(len(emergency_load_edges)))
     posted_edges = openreview.tools.post_bulk_edges(client, emergency_load_edges)
     print('Posted {0} edges\n'.format(len(posted_edges)))
 
     map_submissions = {note.number: note for note in openreview.tools.iterget_notes(client, invitation = 'thecvf.com/ECCV/2020/Conference/-/Blind_Submission')}
+
     all_reviews = list(openreview.tools.iterget_notes(client, invitation='thecvf.com/ECCV/2020/Conference/Paper[0-9]*/-/Official_Review$'))
     print('Found {} official reviews\n'.format(len(all_reviews)))
+    
     map_paper_to_reviews = {}
     for review in all_reviews:
         paper_num = int(review.invitation.split('Paper')[1].split('/')[0])
@@ -153,7 +143,7 @@ if __name__ == '__main__':
         if paper_num not in map_paper_to_reviews:
             map_paper_to_reviews[paper_num] = []
 
-    emergency_demand_invitation = client.post_invitation(openreview.Invitation(
+    emergency_demand_invitation = openreview.Invitation(
         id='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Custom_User_Demands',
         signatures=['thecvf.com/ECCV/2020/Conference'],
         readers=[
@@ -186,11 +176,30 @@ if __name__ == '__main__':
                 }
             }
         }
-    ))
+    )
+    emergency_demand_invitation = client.post_invitation(emergency_demand_invitation)
     print('Posted invitation {}\n'.format(emergency_demand_invitation.id))
-    print('Deleting old edges for invitation {}\n'.format(emergency_demand_invitation.id))
-    client.delete_edges(invitation=emergency_demand_invitation.id)
 
+    # DO SEPARATELY
+    # print('Deleting old edges for invitation {}\n'.format(emergency_demand_invitation.id))
+    # client.delete_edges(invitation=emergency_demand_invitation.id)
+
+    ac_groups = openreview.tools.iterget_groups(client, regex='thecvf.com/ECCV/2020/Conference/Paper[0-9]*/Area_Chairs$')
+    map_paper_to_ac = {}
+    for grp in ac_groups:
+        paper_num = int(grp.id.split('Paper')[1].split('/')[0])
+        if paper_num in map_submissions and grp.members:
+            map_paper_to_ac[paper_num] = grp.members[0]
+
+    map_paper_to_reviewers = {}
+    reviewer_groups = list(openreview.tools.iterget_groups(client, regex='thecvf.com/ECCV/2020/Conference/Paper[0-9]*/AnonReviewer[0-9]*$'))
+    for grp in reviewer_groups:
+        paper_num = int(grp.id.split('Paper')[1].split('/')[0])
+        if paper_num in map_submissions and grp.members:
+            if paper_num not in map_paper_to_reviewers:
+                map_paper_to_reviewers[paper_num] = []
+            map_paper_to_reviewers[paper_num].append(grp.members[0])
+    
     ac_recommendation_edges = list(openreview.tools.iterget_edges(
         client, 
         invitation='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Recommendation'))
@@ -202,29 +211,50 @@ if __name__ == '__main__':
     print('Found {} papers with AC recommendations\n'.format(len(papers_with_recommendations)))
 
     emergency_review_demands = []
-    total_review_demand = 0
+
+    recommendation_based_review_demand = 0
+    filtered_demand = 0
+    absolute_demand = 0
+    ac_with_missing_recs = set()
+    paper_left_due_to_filtering = 0
+    reviewers_left_due_to_filtering = 0
+
     for paper_num, reviews in map_paper_to_reviews.items():
         paper_note = map_submissions[paper_num]
-        if len(reviews) < 3 and paper_note.id in papers_with_recommendations:
-            emergency_review_demands.append(openreview.Edge(
-                head=paper_note.id,
-                tail='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers',
-                invitation='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Custom_User_Demands',
-                readers=[
-                    'thecvf.com/ECCV/2020/Conference',
-                    'thecvf.com/ECCV/2020/Conference/Area_Chairs',
-                    ],
-                writers=['thecvf.com/ECCV/2020/Conference'],
-                signatures=['thecvf.com/ECCV/2020/Conference'],
-                weight=3-len(reviews)
-            ))
-            total_review_demand += 3-len(reviews)
+        ac = map_paper_to_ac[paper_num]
+        missing_reviews = 3-len(reviews)
+        if missing_reviews > 0:
+            if paper_note.id in papers_with_recommendations:
+                recommendation_based_review_demand += missing_reviews
+            else:
+                ac_with_missing_recs.add(ac)
+
+            if (len(map_paper_to_reviewers[paper_num]) <= 3 and missing_reviews > 0) or (len(map_paper_to_reviewers[paper_num]) > 3 and missing_reviews > 1):
+                # Don't use papers with 1 missing review AND more than 3 assigned reviewers
+                emergency_review_demands.append(openreview.Edge(
+                    head=paper_note.id,
+                    tail='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers',
+                    invitation='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Custom_User_Demands',
+                    readers=[
+                        'thecvf.com/ECCV/2020/Conference',
+                        'thecvf.com/ECCV/2020/Conference/Area_Chairs',
+                        ],
+                    writers=['thecvf.com/ECCV/2020/Conference'],
+                    signatures=['thecvf.com/ECCV/2020/Conference'],
+                    weight=missing_reviews
+                ))
+                filtered_demand += missing_reviews
+            else:
+                paper_left_due_to_filtering += 1
+                reviewers_left_due_to_filtering += missing_reviews
+
+            absolute_demand += missing_reviews
 
     print('Custom_User_Demands: Posting {0} edges'.format(len(emergency_review_demands)))
     posted_edges = openreview.tools.post_bulk_edges(client, emergency_review_demands)
     print('Custom_User_Demands: Posted {0} edges\n'.format(len(posted_edges)))
 
-    emergency_aggregate_invitation = client.post_invitation(openreview.Invitation(
+    emergency_aggregate_invitation = openreview.Invitation(
         id='thecvf.com/ECCV/2020/Conference/Emergency_Reviewers/-/Aggregate_Score',
         signatures=['thecvf.com/ECCV/2020/Conference'],
         readers=[
@@ -257,18 +287,15 @@ if __name__ == '__main__':
                 }
             }
         }
-    ))
+    )
+    emergency_aggregate_invitation = client.post_invitation(emergency_aggregate_invitation)
     print('Posted invitation {}\n'.format(emergency_aggregate_invitation.id))
 
+    # DO SEPARATELY
+    # print('\nDeleting old edges for invitation:"thecvf.com/ECCV/2020/Conference/Reviewers/-/Conflict" and label: "Already Assigned"\n')
+    # client.delete_edges(invitation='thecvf.com/ECCV/2020/Conference/Reviewers/-/Conflict', label='Already Assigned')
+
     conflicts = []
-
-    print('\nDeleting old edges for invitation:"thecvf.com/ECCV/2020/Conference/Reviewers/-/Conflict" and label: "Already Assigned"\n')
-    client.delete_edges(invitation='thecvf.com/ECCV/2020/Conference/Reviewers/-/Conflict', label='Already Assigned')
-
-    reviewer_groups = list(openreview.tools.iterget_groups(
-        client, 
-        regex='thecvf.com/ECCV/2020/Conference/Paper.*/AnonReviewer[0-9]*$'))
-    print('\nFound {} anonymous reviewer groups'.format(len(reviewer_groups)))
 
     for grp in reviewer_groups:
         paper_num = int(grp.id.split('Paper')[1].split('/')[0])
@@ -307,6 +334,10 @@ if __name__ == '__main__':
         # conference.setup_matching()
         # print('Emergency Reviewer match setup done.')
 
-
-    print('\nTotal Emergency Review Demand: {}'.format(total_review_demand))
+    print('\nFiltered Emergency Review Demand: {}'.format(filtered_demand))
+    print ('\nAbsolute Emergency Review Demand: {}'.format(absolute_demand))
     print('\nTotal Emergency Review Supply: {}\n'.format(total_review_capacity))
+    print('\nACs with missing recommendations: {}\n'.format(len(ac_with_missing_recs)))
+
+    print('\nPapers left out due to filtering: {}\n'.format(paper_left_due_to_filtering))
+    print('\nReviews left out due to filtering: {}\n'.format(reviewers_left_due_to_filtering))
