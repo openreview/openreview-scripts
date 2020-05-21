368a5752c2faa686cca0765c443882971386d8ad
celestemg <acmartinez@umass.edu>
2020-05-20 23:21:07 -0400

    Added author emails to note

--- venues/L4DC/2020/Conference/python/upload-submissions.py
+++ venues/L4DC/2020/Conference/python/upload-submissions.py
@@ -5,8 +5,9 @@ import openreview
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('l4dc_papers', help='csv file')
+parser.add_argument('--l4dc_papers', help='csv file')
 parser.add_argument('--pdfs_filepath', help='filepath to directory with pdfs')
+parser.add_argument('--author_details', help='file with author names and emails')
 parser.add_argument('--baseurl', help='base url')
 parser.add_argument('--username')
 parser.add_argument('--password')
@@ -28,6 +29,20 @@ submission_invitation.reply['writers'] = {
 }
 client.post_invitation(submission_invitation)
 
+#Dictionary to match authors to emails from author_details.csv
+author_to_email = {}
+
+with open(args.author_details) as csv_file1:
+    csv_reader1 = csv.reader(csv_file1, delimiter = ',')
+    next(csv_reader1, None)
+    
+    for row in csv_reader1:
+        title = row[1]
+        name = row[2]+' '+row[3]
+        email = row[4].lower()
+        author_to_email[(name, title)] = email
+
+failed_papers = []
 with open(args.l4dc_papers) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
     next(csv_reader, None)
@@ -36,6 +51,11 @@ with open(args.l4dc_papers) as csv_file:
         
         pdf_link = client.put_attachment(args.pdfs_filepath+'/'+row[2], 'L4DC.org/2020/Conference/-/Submission', 'pdf')
         authors = [author.strip() for author in row[0].replace(' and ', ', ').split(',')]
+        try:
+            authorids = [author_to_email[(author, row[1])] for author in authors]
+        except:
+            failed_papers.append(row[1])
+            continue
 
         l4dc_paper = openreview.Note(
             invitation=conference_id+'/-/Submission',
@@ -45,7 +65,7 @@ with open(args.l4dc_papers) as csv_file:
             content={
                 'title': row[1],
                 'authors': authors,
-                'authorids': ['celeste@openreview.net']*len(authors),
+                'authorids': authorids,
                 'abstract': row[3],
                 'venue': 'L4DC 2020', 
                 'pdf': pdf_link
@@ -58,3 +78,6 @@ with open(args.l4dc_papers) as csv_file:
             print('Posted paper', posted_paper.forum)
         except openreview.OpenReviewException as e:
             print(e)
+
+print(failed_papers)
+
