77b697c72660cfa1a16a36ca52177a8f13118f66
celestemg <acmartinez@umass.edu>
2020-05-20 21:36:36 -0400

    Initial script for posting papers

---
+++ venues/L4DC/2020/Conference/upload-submissions.py
@@ -0,0 +1,60 @@
+import argparse
+import re
+import csv
+import openreview
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('l4dc_papers', help='csv file')
+parser.add_argument('--pdfs_filepath', help='filepath to directory with pdfs')
+parser.add_argument('--baseurl', help='base url')
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+conference_id = 'L4DC.org/2020/Conference'
+
+submission_invitation = client.get_invitation('L4DC.org/2020/Conference/-/Submission')
+
+submission_invitation.reply['readers'] = {
+    'values': ['everyone']
+    }
+submission_invitation.reply['signatures'] =  {
+    'values': [conference_id]
+    }
+submission_invitation.reply['writers'] = {
+    'values': [conference_id, conference_id+'/Program_Chairs']
+}
+client.post_invitation(submission_invitation)
+
+with open(args.l4dc_papers) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        
+        pdf_link = client.put_attachment(args.pdfs_filepath+'/'+row[2], 'L4DC.org/2020/Conference/-/Submission', 'pdf')
+        authors = [author.strip() for author in row[0].replace(' and ', ', ').split(',')]
+
+        l4dc_paper = openreview.Note(
+            invitation=conference_id+'/-/Submission',
+            signatures=[conference_id],
+            writers=[conference_id, conference_id+'/Program_Chairs'],
+            readers=['everyone'],
+            content={
+                'title': row[1],
+                'authors': authors,
+                'authorids': ['celeste@openreview.net']*len(authors),
+                'abstract': row[3],
+                'venue': 'L4DC 2020', 
+                'pdf': pdf_link
+
+            }
+        )
+
+        try:
+            posted_paper = client.post_note(l4dc_paper)
+            print('Posted paper', posted_paper.forum)
+        except openreview.OpenReviewException as e:
+            print(e)
