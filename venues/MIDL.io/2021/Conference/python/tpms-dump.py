05a5f00b1481c3ce5bdf9534dfd942a05b1f6dea
celestemg <acmartinez@umass.edu>
2021-02-17 13:06:17 -0500

    Add tpms scripts

---
+++ venues/MIDL.io/2021/Conference/python/tpms-dump.py
@@ -0,0 +1,53 @@
+'''
+1) Reviewers: The reviewer list should be in csv file.
+
+    - in csv format: ReviewerEmail,FirstName,LastName,<comma-separated-additional-emails>
+
+'''
+
+import argparse
+import openreview
+import csv
+from openreview import tools
+
+## Argument handling
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base url")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+
+reviewers_group = client.get_group(id='MIDL.io/2021/Conference/Reviewers')
+
+rows = []
+count = 0
+for email in reviewers_group.members:
+    row = []
+    profile = openreview.tools.get_profile(client, email)
+    if(profile):
+        preferred_email = profile.content.get('preferred_email', None)
+        emails = profile.content.get('emails')
+        if not preferred_email:
+            preferred_email = emails[0]
+        row.append(preferred_email)
+        name = openreview.tools.get_preferred_name(profile)
+        name_parts = name.split(' ')
+        firstname = name_parts[0]
+        lastname = name_parts[-1]
+        row.append(firstname)
+        row.append(lastname)
+        for email in [e for e in emails if e != preferred_email]:
+            row.append(email)
+    else:
+        count+=1
+        row.append(email)
+    rows.append(row)
+
+with open('MIDL2021-tpms-reviewers-dump.csv', 'w') as f:
+    writer = csv.writer(f)
+    for row in rows:
+        writer.writerow(row)
+
+print(count, 'reviewers with no profile')
---
+++ venues/MIDL.io/2021/Conference/python/tpms-get-pdfs.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import requests
+import openreview
+import argparse
+import datetime
+import os
+from tqdm import tqdm
+import concurrent.futures
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base url")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+print ('connecting to {0}'.format(client.baseurl))
+start = datetime.datetime.utcnow()
+
+submission_invitation = 'MIDL.io/2021/Conference/-/Submission'
+
+print('Get Submissions')
+submissions = openreview.tools.iterget_notes(client, invitation=submission_invitation)
+
+if not os.path.exists('midl2021_pdfs'):
+	os.makedirs('midl2021_pdfs')
+
+submissions_without_pdf = 0
+
+def get_pdf(submission):
+	if 'pdf' in submission.content:
+		pdf_url = '{0}{1}'.format(client.baseurl, submission.content['pdf'])
+		paper_number = submission.number
+		try:
+			with open('midl2021_pdfs/Paper{0}.pdf'.format(paper_number), 'wb') as f:
+				f.write(client.get_pdf(submission.id))
+		except Exception as e:
+			print ('Error during pdf download for paper number {}, error: {}'.format(submission.number, e))
+		return pdf_url
+	return None
+
+print('Download files')
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    future_to_url = {executor.submit(get_pdf, submission): submission for submission in submissions}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+
+end = datetime.datetime.utcnow()
+
+print('Done', (end - start).total_seconds(), 'seconds')
