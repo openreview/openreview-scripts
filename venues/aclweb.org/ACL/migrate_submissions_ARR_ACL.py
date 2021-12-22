3af02162f2bddd846209a1204731f3cd2cbc7806
Nadia L\'Bahy <nlbahy@umass.edu>
2021-12-22 13:06:40 -0500

    outputs into csv

--- venues/aclweb.org/ACL/migrate_submissions_ARR_ACL.py
+++ venues/aclweb.org/ACL/migrate_submissions_ARR_ACL.py
@@ -23,11 +23,12 @@ client = openreview.Client(baseurl=args.baseurl, username=args.username, passwor
 sac_name_dictionary = {
     'Ethics in NLP': 'Ethics_NLP', 
     'Linguistic theories, Cognitive Modeling and Psycholinguistics': 'LCMP', 
+    'Linguistic Theories, Cognitive Modeling and Psycholinguistics': 'LCMP',
     'Machine Learning for NLP': 'Machine_Learning_NLP', 
     'Phonology, Morphology and Word Segmentation': 'Phonology_Morphology_Word_Segmentation', 
     'Resources and Evaluation': 'Resources_Evaluation', 
     'Semantics: Lexical': 'Semantics_Lexical', 
-    'Semantics: Sentence-level Semantics, Textual Inference, and Other areas': 'Semantics_STO', 
+    'Semantics: Sentence level, Textual Inference and Other areas': 'Semantics_STO', 
     'Syntax: Tagging, Chunking and Parsing': 'Syntax_TCP', 
     'Information Extvaluesraction': 'Information_Extraction', 
     'Computational Social Science and Cultural Analytics': 'CSSCA', 
@@ -49,17 +50,21 @@ sac_name_dictionary = {
 # Post acl submission (calls post_blind_submission)
 def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_output_dict):
     # Check for later duplicates 
-    later_duplicates = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/ACL/ARR/2021/.*',content = {'previous_URL':f'https://openreview.net/forum?id={arr_submission_forum}'}))  
+    
     eligible_arr_invitations = ['aclweb.org/ACL/ARR/2021/May/', 'aclweb.org/ACL/ARR/2021/Jun/', 'aclweb.org/ACL/ARR/2021/July/', 'aclweb.org/ACL/ARR/2021/August/', 'aclweb.org/ACL/ARR/2021/September/','aclweb.org/ACL/ARR/2021/October/','aclweb.org/ACL/ARR/2021/November/']    
     original_arr_sub_id = None
-    submission_output_dict['acl_commitment_forum'] = acl_commitment_note.forum
+    #submission_output_dict['acl_commitment_forum'] = acl_commitment_note.forum
     try:
-        original_arr_sub_id = client.get_note(arr_submission_forum).original
+        original_arr_sub = client.get_note(arr_submission_forum)
+        if(original_arr_sub.original):
+            original_arr_sub_id = original_arr_sub.original
+        else: 
+            original_arr_sub_id = original_arr_sub.id
     except:
         print(f"Note {arr_submission_forum} does not exist")
     if original_arr_sub_id and ((client.get_note(original_arr_sub_id)).invitation.startswith(tuple(eligible_arr_invitations))):
         original_arr_sub = client.get_note(original_arr_sub_id)
-        submission_output_dict['arr_submission_forum'] = original_arr_sub.forum
+        submission_output_dict[acl_commitment_note.forum]['arr_submission_forum'] = original_arr_sub.forum
         if acl_commitment_note.signatures[0] in original_arr_sub.content['authorids']:
             # Create new note to submit to ACL 
             acl_sub = openreview.Note(
@@ -96,24 +101,23 @@ def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_ou
             acl_submitted = client.post_note(acl_sub)
             if(acl_submitted):
                 submission_output_dict['was_migrated'] = True
-                if later_duplicates:
-                    submission_output_dict['is_latest_version'] = False 
                 acl_submission_dict[acl_submitted.content['paper_link'].split('=')[1]]=acl_submitted
-                post_blind_submission(acl_submitted.id, acl_submitted, client.get_note(original_arr_sub_id), submission_output_dict)
+                post_blind_submission(acl_submitted.id, acl_submitted, client.get_note(original_arr_sub_id), submission_output_dict, acl_commitment_note)
             else: 
                 print(f'Nothing posted for ARR Submission {arr_submission_forum}')
+                return(submission_output_dict)
         else:
             print(f"signature of acl commitment note {acl_commitment_note.id} not in arr submission {arr_submission_forum} authors")
-            if later_duplicates:
-                submission_output_dict['is_latest_version'] = False
+            return submission_output_dict
     else:
         print(f"acl commitment note {acl_commitment_note.id} links to arr submission {arr_submission_forum} with invalid invitation")
+        return submission_output_dict
             
         
     
         
 # Post blind submission (calls post_reviews)
-def post_blind_submission(acl_submission_id, acl_submission, arr_submission, submission_output_dict):
+def post_blind_submission(acl_submission_id, acl_submission, arr_submission, submission_output_dict, acl_commitment_note):
     # Post requisite groups 
     number = acl_submission.number
     # Create paperX/Authors 
@@ -147,15 +151,21 @@ def post_blind_submission(acl_submission_id, acl_submission, arr_submission, sub
             '{conf_id}/Paper{number}/Area_Chairs'.format(conf_id = conf_id, number = arr_number),
             'aclweb.org/ACL/2022/Conference/Paper{number}/Authors'.format(number = number)
             ]
-    author_group = []
-    for author in authors.members:
-        author_group.append(openreview.tools.get_profile(client, author))
+    author_group = openreview.tools.get_profiles(client, ids_or_emails = authors.members, with_publications=False)
+    
+    print(acl_submission.content['track'])
     for SAC in (openreview.tools.get_group(client, "aclweb.org/ACL/2022/Conference/{sac_track}/Senior_Area_Chairs".format(sac_track = sac_name_dictionary[acl_submission.content['track']]))).members: 
-        #print(SAC)
-        SAC = openreview.tools.get_profile(client, SAC)
-        conflicts = openreview.tools.get_conflicts(author_group, SAC)
-        if conflicts: 
-            conflict_members.append(SAC)
+        print(SAC)
+        SAC_profile = openreview.tools.get_profiles(client, SAC)
+        if(SAC_profile):
+            conflicts = openreview.tools.get_conflicts(author_group, SAC_profile[0])
+            if conflicts: 
+                conflict_members.append(SAC_profile[0].id)
+        else:
+            #print(f'SAC {SAC} does not have an OR profile')
+            conflicts = openreview.tools.get_conflicts(author_group, SAC_profile[0])
+            if conflicts: 
+                conflict_members.append(SAC_profile[0])
     conflicts = openreview.Group(
         id = 'aclweb.org/ACL/2022/Conference/Paper{number}/Conflicts'.format(number = number),
         signatures = [
@@ -201,12 +211,15 @@ def post_blind_submission(acl_submission_id, acl_submission, arr_submission, sub
     
     blinded_note_posted = client.post_note(blinded_note)
     if blinded_note_posted:
-        submission_output_dict['acl_blind_submission_forum'] = blinded_note_posted.forum
+        #print(acl_commitment_note)
+        #print(blinded_note_posted.forum)
+        submission_output_dict[acl_commitment_note.forum]['acl_blind_submission_forum'] = blinded_note_posted.forum
         blind_submissions[blinded_note_posted.original] = blinded_note_posted
-        post_reviews(blinded_note_posted.forum, blinded_note_posted, arr_submission, submission_output_dict)
-
+        post_reviews(blinded_note_posted.forum, blinded_note_posted, arr_submission, submission_output_dict, acl_commitment_note)
+    else:
+        return submission_output_dict
 # Post reviews (calls post_metareviews)
-def post_reviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict):
+def post_reviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict, acl_commitment_note):
     # Migrate all reviews from the original ARR Submission 
     # Get invitation for that month's reviews 
     conf_id = arr_submission.invitation.rsplit('/', 2)[0]
@@ -215,7 +228,7 @@ def post_reviews(acl_blind_submission_forum, acl_blind_submission, arr_submissio
     # Get all reviews from the original ARR Submission 
     arr_reviews = list(openreview.tools.iterget_notes(client, invitation = review_invitation_arr))
     
-    submission_output_dict['num_reviews'] = len(arr_reviews)
+    submission_output_dict[acl_commitment_note.forum]['num_reviews'] = len(arr_reviews)
 
     # Iterate through each review and for each, create and post a new review 
     for arr_review in arr_reviews:
@@ -234,11 +247,11 @@ def post_reviews(acl_blind_submission_forum, acl_blind_submission, arr_submissio
             acl_review_posted = client.post_note(acl_review)
             assert acl_review_posted, print('failed to post review ', acl_review.id)
             acl_reviews_dictionary[acl_review_posted.signatures[0]] = acl_review_posted.replyto
-    post_metareviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict)
+    post_metareviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict, acl_commitment_note)
     
         
 # Post metareviews 
-def post_metareviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict):
+def post_metareviews(acl_blind_submission_forum, acl_blind_submission, arr_submission, submission_output_dict, acl_commitment_note):
     # Migrate all reviews from the original ARR Submission 
     # Get invitation for that month's reviews 
     conf_id = arr_submission.invitation.rsplit('/', 2)[0]
@@ -246,7 +259,7 @@ def post_metareviews(acl_blind_submission_forum, acl_blind_submission, arr_submi
     
     # Get all reviews from the original ARR Submission 
     arr_metareviews = list(openreview.tools.iterget_notes(client, invitation = metareview_invitation_arr))
-    submission_output_dict['num_metareviews'] = len(arr_metareviews)
+    submission_output_dict[acl_commitment_note.forum]['num_metareviews'] = len(arr_metareviews)
      
     
     # Iterate through each review and for each, create and post a new review 
@@ -278,24 +291,50 @@ acl_metareviews_dictionary = {review.signatures[0] : review.replyto for review i
 
 # Save all submissions in a dictionary by paper_link
 acl_submission_dict = {acl_submission.content['paper_link'].split('=')[1]:acl_submission for acl_submission in acl_submissions}
+print("acl_commitment_forum", "acl_blind_submission_forum", "arr_submission_forum", "num_reviews", "num_metareviews", "is_latest_version", "was_migrated")
 # Create an empty list to store information about each paper for the PCs to view 
-for note in commitment_notes: 
-    submission_output_dict = {'acl_commitment_forum': None, 'acl_blind_submission_forum': None, 'arr_submission_forum': None, 'num_reviews':None, 'num_metareviews':None, 'is_latest_version':None, 'was_migrated':False}
-    # Get the arr submission forum. Then check if that is in the submission dictionary. If it is, check for blind. If not, run post_acl
+submission_output_dict = {}
+for note in commitment_notes:
     arr_submission_forum = note.content['paper_link'].split('=')[1]
+    submission_output_dict[note.forum] = {'acl_blind_submission_forum': None, 'arr_submission_forum': None, 'num_reviews':None, 'num_metareviews':None, 'is_latest_version':None, 'was_migrated':False}
+
+    later_duplicates = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/ACL/ARR/2021/.*',content = {'previous_URL':f'https://openreview.net/forum?id={arr_submission_forum}'}))  
+    if later_duplicates:
+        submission_output_dict[note.forum]['is_latest_version'] = False
+    else:
+        submission_output_dict[note.forum]['is_latest_version'] = True
+    # Get the arr submission forum. Then check if that is in the submission dictionary. If it is, check for blind. If not, run post_acl
+    
     if arr_submission_forum not in acl_submission_dict:
         post_acl_submission(arr_submission_forum, note, submission_output_dict)
-        print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
+        #print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
         
-    # Check if the acl_submission.id is in blind_submissions
+    # Check if the acl_submission.id is in blind_submissions (it is in acl_submissions)
     elif acl_submission_dict[arr_submission_forum].id not in blind_submissions:
-        post_blind_submission(acl_submission_dict[arr_submission_forum].id, acl_submission_dict[arr_submission_forum], client.get_note(arr_submission_forum), submission_output_dict)
-        print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
+        submission_output_dict[note.forum]['arr_submission_forum'] = arr_submission_forum
+        submission_output_dict[note.forum]['was_migrated'] = True
+        post_blind_submission(acl_submission_dict[arr_submission_forum].id, acl_submission_dict[arr_submission_forum], client.get_note(arr_submission_forum), submission_output_dict, note)
+        #print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
 
-    # Check if the blind submission has reviews
+    # Check if the blind submission has reviews (it is in blind submissions)
     else: 
-        post_reviews(blind_submissions[acl_submission_dict[arr_submission_forum].id].forum, blind_submissions[acl_submission_dict[arr_submission_forum].id], client.get_note(arr_submission_forum), submission_output_dict)
+        submission_output_dict[note.forum]['arr_submission_forum'] = arr_submission_forum
+        submission_output_dict[note.forum]['acl_blind_submission_forum'] = (acl_submission_dict[note.content['paper_link'].split('=')[1]]).forum
+        submission_output_dict[note.forum]['was_migrated'] = True
+        post_reviews(blind_submissions[acl_submission_dict[arr_submission_forum].id].forum, blind_submissions[acl_submission_dict[arr_submission_forum].id], client.get_note(arr_submission_forum), submission_output_dict, note)
         #post_metareviews(blind_submissions[acl_submission_dict[arr_submission_forum].id].forum, blind_submissions[acl_submission_dict[arr_submission_forum].id], client.get_note(arr_submission_forum), submission_output_dict)
-        print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
+        #print(f"{submission_output_dict['acl_commitment_forum']},{submission_output_dict['acl_blind_submission_forum']},{submission_output_dict['arr_submission_forum']},{submission_output_dict['num_reviews']},{submission_output_dict['num_metareviews']}, {submission_output_dict['is_latest_version']}, {submission_output_dict['was_migrated']}")
 
-
+fields = ['acl_commitment_note', 'acl_blind_submission', 'original_arr_submission', 'num_reviews', 'num_metareviews', 'is_latest_version', 'was_migrated']
+rows = []
+for key,value in submission_output_dict.items():
+    list = [key, value['acl_blind_submission_forum'], value['arr_submission_forum'], value['num_reviews'], value['num_metareviews'], value['is_latest_version'], value['was_migrated']]
+    rows.append(list)
+with open('output_data', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile) 
+        
+    # writing the fields 
+    csvwriter.writerow(fields) 
+        
+    # writing the data rows 
+    csvwriter.writerows(rows)
