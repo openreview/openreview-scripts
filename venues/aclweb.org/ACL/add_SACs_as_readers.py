7d9cce486e5d3c0e693e708b82b725a841946b99
Melisa Bok <melisabok@gmail.com>
2022-01-20 15:38:02 -0500

    refactor

--- venues/aclweb.org/ACL/add_SACs_as_readers.py
+++ venues/aclweb.org/ACL/add_SACs_as_readers.py
@@ -3,6 +3,7 @@ from re import sub
 import openreview
 from tqdm import tqdm
 import csv
+import tracks
 
 """
 OPTIONAL SCRIPT ARGUMENTS
@@ -19,60 +20,52 @@ parser.add_argument('--password')
 args = parser.parse_args()
 client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
 
-# Creating SAC groups from Track Name 
-sac_name_dictionary = {
-    'Ethics in NLP': 'Ethics_NLP', 
-    'Linguistic theories, Cognitive Modeling and Psycholinguistics': 'LCMP', 
-    'Linguistic Theories, Cognitive Modeling and Psycholinguistics': 'LCMP',
-    'Machine Learning for NLP': 'Machine_Learning_NLP', 
-    'Phonology, Morphology and Word Segmentation': 'Phonology_Morphology_Word_Segmentation', 
-    'Resources and Evaluation': 'Resources_Evaluation', 
-    'Semantics: Lexical': 'Semantics_Lexical', 
-    'Semantics: Sentence level, Textual Inference and Other areas': 'Semantics_STO', 
-    'Syntax: Tagging, Chunking and Parsing': 'Syntax_TCP', 
-    'Information Extraction': 'Information_Extraction', 
-    'Computational Social Science and Cultural Analytics': 'CSSCA', 
-    'Information Retrieval and Text Mining': 'Info_Retrieval_Text_Mining', 
-    'Interpretability and Analysis of Models for NLP': 'IAM_for_NLP', 
-    'Machine Translation and Multilinguality': 'Machine_Translation_Multilinguality', 
-    'NLP Applications': 'NLP_Applications', 
-    'Question Answering': 'Question_Answering', 
-    'Dialogue and Interactive Systems': 'Dialogue_and_Interactive_Systems', 
-    'Discourse and Pragmatics': 'Discourse_and_Pragmatics', 
-    'Generation': 'Generation', 
-    'Language Grounding to Vision, Robotics, and Beyond': 'LGVRB', 
-    'Sentiment Analysis, Stylistic Analysis, and Argument Mining': 'SASAAM', 
-    'Speech and Multimodality': 'Speech_and_Multimodality', 
-    'Summarization': 'Summarization', 
-    'Special Theme on Language Diversity: From Low Resource to Endangered Languages': 'Special_Theme',
-    'PC Track': 'PC_Track'
-    }
+program_chairs_id = 'aclweb.org/ACL/2022/Conference/Program_Chairs'
+
+# Creating SAC groups from Track Name
+sac_name_dictionary = tracks.sac_name_dictionary
 
 acl_blind_submissions = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/ACL/2022/Conference/-/Blind_Submission'))
 acl_meta_reviews = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/ACL/2022/Conference/-/Meta_Review'))
 acl_reviews = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/ACL/2022/Conference/-/Official_Review'))
 reviews_by_forum = {}
 for metareview in acl_meta_reviews:
-    if metareview.forum in reviews_by_forum: 
+    if metareview.forum in reviews_by_forum:
         reviews_by_forum[metareview.forum].append(metareview)
     else:
         reviews_by_forum[metareview.forum] = [metareview]
 for review in acl_reviews:
-    if review.forum in reviews_by_forum: 
+    if review.forum in reviews_by_forum:
         reviews_by_forum[review.forum].append(review)
     else:
         reviews_by_forum[review.forum] = [review]
 
-# For each blind submission, set the readers to the SAC track group 
-for acl_blind_submission in acl_blind_submissions:
-    acl_submission = client.get_note(acl_blind_submission.original)
-    acl_blind_submission.readers.append(f'aclweb.org/ACL/2022/Conference/{sac_name_dictionary[acl_submission.content["track"]]}/Senior_Area_Chairs')
+# For each blind submission, set the readers to the SAC track group
+for acl_blind_submission in tqdm(acl_blind_submissions):
+    paper_track = sac_name_dictionary[acl_blind_submission.content["track"]]
+    track_sac_id = f'aclweb.org/ACL/2022/Conference/{paper_track}/Senior_Area_Chairs'
+    conflict_id = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Conflicts'
+
+    acl_blind_submission.readers = [
+        'aclweb.org/ACL/2022/Conference',
+        program_chairs_id,
+        track_sac_id
+    ]
+    acl_blind_submission.nonreaders = [
+        conflict_id
+    ]
     acl_blind_submission.content = {
         'authors': ['Anonymous'],
         'authorids': [f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Authors']
     }
     client.post_note(acl_blind_submission)
     reviews = reviews_by_forum[acl_blind_submission.forum]
-    for review in reviews: 
-        review.readers.append(f'aclweb.org/ACL/2022/Conference/{sac_name_dictionary[acl_submission.content["track"]]}/Senior_Area_Chairs')
+    for review in reviews:
+        review.readers = [
+            program_chairs_id,
+            track_sac_id
+        ]
+        review.nonreaders = [
+            conflict_id
+        ]
         client.post_note(review)
