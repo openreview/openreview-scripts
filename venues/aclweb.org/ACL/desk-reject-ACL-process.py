df3b372fe89c34f020c2e6425efc98df3315b36e
nlbahy <40001427+nlbahy@users.noreply.github.com>
2022-01-25 14:57:31 -0500

    forum_note -> note (#1229)
    * forum_note -> note
    
    * added link

--- venues/aclweb.org/ACL/desk-reject-ACL-process.py
+++ venues/aclweb.org/ACL/desk-reject-ACL-process.py
@@ -30,6 +30,6 @@ def process(client, note, invitation):
 
     PAPER_AUTHORS_ID = f'aclweb.org/ACL/2022/Conference/Paper{forum_note.number}/Authors'
 
-    recipients = forum_note.readers
+    recipients = note.readers
     recipients.append(PAPER_AUTHORS_ID)
-    client.post_message(subject=email_subject, recipients=recipients, message=email_body, ignoreRecipients=forum_note.nonreaders)
+    client.post_message(subject=email_subject, recipients=recipients, message=email_body, ignoreRecipients=note.nonreaders)
--- venues/aclweb.org/ACL/suggested_decision_process.py
+++ venues/aclweb.org/ACL/suggested_decision_process.py
@@ -8,5 +8,5 @@ def process(client, note, invitation):
     
     
     email_subject = f'{CONFERENCE_SHORT_NAME}: A suggested decision has been posted to a paper in your area'
-    email_body = f'A suggested decision has been posted to Paper {forum_note.number} in {CONFERENCE_SHORT_NAME} track {forum_note.content["track"]}. \n\nSuggested decision: {note.content["suggested_decision"]}'
-    client.post_message(subject=email_subject, recipients=forum_note.readers, message=email_body, ignoreRecipients=forum_note.nonreaders)
+    email_body = f'A suggested decision has been posted to Paper {forum_note.number} in {CONFERENCE_SHORT_NAME} track {forum_note.content["track"]}. \n\nSuggested decision: {note.content["suggested_decision"]}\n\nView it here: https://openreview.net/forum?id={note.forum}&noteId={note.id}'
+    client.post_message(subject=email_subject, recipients=note.readers, message=email_body, ignoreRecipients=note.nonreaders)
