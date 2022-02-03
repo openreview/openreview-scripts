711c379cd7d2632994c755dc83fa95bcfbf0428a
Melisa Bok <melisabok@gmail.com>
2022-02-03 16:01:02 -0500

    ethics reviewer assignment invitation and console (#1232)
    * ethics reviewer assignment invitation and console
    
    * script to assign edge
    
    * Update venues/aclweb.org/ACL/post_ethics_reviewers_assignment_invitation.py
    
    Co-authored-by: nlbahy <40001427+nlbahy@users.noreply.github.com>
    
    * Update venues/aclweb.org/ACL/post_ethics_reviewers_assignment_invitation.py
    
    Co-authored-by: nlbahy <40001427+nlbahy@users.noreply.github.com>
    
    * Update venues/aclweb.org/ACL/post_ethics_reviewers_assignment_invitation.py
    
    Co-authored-by: nlbahy <40001427+nlbahy@users.noreply.github.com>
    
    * read preprocess function
    
    * fix readers/writers
    
    * updated post assignment edges
    
    Co-authored-by: Nadia L\'Bahy <nlbahy@umass.edu>
    Co-authored-by: nlbahy <40001427+nlbahy@users.noreply.github.com>

---
+++ venues/aclweb.org/ACL/post_ethics_reviewers_assignment_invitation.py
@@ -0,0 +1,80 @@
+import argparse
+from re import sub
+import openreview
+from tqdm import tqdm
+import csv
+import tracks
+
+"""
+OPTIONAL SCRIPT ARGUMENTS
+
+    baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+    username - the email address of the logging in user
+    password - the user's password
+
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+sac_name_dictionary = tracks.sac_name_dictionary
+
+preprocess = None
+with open('./ethics_reviewer_assignment_preprocess.py') as f:
+    preprocess = f.read()
+
+assignment_invitation = openreview.Invitation(
+    id = "aclweb.org/ACL/2022/Conference/Ethics_Reviewers/-/Assignment",
+    readers = ["aclweb.org/ACL/2022/Conference", "aclweb.org/ACL/2022/Conference/Ethics_Chairs"],
+    writers = ["aclweb.org/ACL/2022/Conference"],
+    signatures = ["aclweb.org/ACL/2022/Conference"],
+    reply = {
+        "readers": {
+            "values-copied": [
+                "aclweb.org/ACL/2022/Conference",
+                "aclweb.org/ACL/2022/Conference/Ethics_Chairs",
+                "{tail}"
+            ]
+        },
+        "nonreaders": {
+            "values": [
+                "aclweb.org/ACL/2022/Conference/Paper{head.number}/Authors"
+            ]
+        },
+        "writers": {
+            "values": [
+                "aclweb.org/ACL/2022/Conference",
+                "aclweb.org/ACL/2022/Conference/Ethics_Chairs"
+            ]
+        },
+        "signatures": {
+            "values-regex": "aclweb.org/ACL/2022/Conference/Ethics_Chairs|aclweb.org/ACL/2022/Conference/Program_Chairs",
+            "default": "aclweb.org/ACL/2022/Conference/Program_Chairs"
+        },
+        "content": {
+            "head": {
+                "type": "Note",
+                "query": {
+                    "invitation": "aclweb.org/ACL/2022/Conference/-/Blind_Submission"
+                }
+            },
+            "tail": {
+                "type": "Profile",
+                "query": {
+                    "value-regex": "~.*|.+@.+"
+                },
+            },
+            "weight": {
+                "value-regex": "[-+]?[0-9]*\\.?[0-9]*"
+            },
+            "label": {
+                "value-regex": ".*"
+            }
+        }
+    },
+    preprocess=preprocess,
+    process='./ethics_reviewer_assignment_process.py'
+)
+client.post_invitation(assignment_invitation)
