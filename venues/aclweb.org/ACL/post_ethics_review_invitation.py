51ef6351a663e48913459cc74fbe6e164dbeaf69
nlbahy <40001427+nlbahy@users.noreply.github.com>
2022-02-03 15:45:06 -0500

    final decision invitations (#1231)
    * final decision invitations
    
    * Update venues/aclweb.org/ACL/post_decision_invitations.py
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>
    
    * Update venues/aclweb.org/ACL/post_decision_invitations.py
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>
    
    * Ethics Review
    
    * Reviewers can submit, acs cannot?
    
    * added as readers of group
    
    * removed members
    
    * Update venues/aclweb.org/ACL/post_ethics_review_invitation.py
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>
    
    * Implemented Melisa's changes
    
    * Removed Ethics chairs from editing reviews
    
    * added reviewers as readers
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

---
+++ venues/aclweb.org/ACL/post_ethics_review_invitation.py
@@ -0,0 +1,98 @@
+import argparse
+from re import sub
+import openreview
+from tqdm import tqdm
+import csv
+import tracks
+
+"""
+OPTIONAL SCRIPT ARGUMENTS
+
+    baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+    username - the email address of the logging in user
+    password - the user's password
+
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+sac_name_dictionary = tracks.sac_name_dictionary
+program_chairs_id = 'aclweb.org/ACL/2022/Conference/Program_Chairs'
+
+    
+ethics_review_super = openreview.Invitation(
+    id = "aclweb.org/ACL/2022/Conference/-/Ethics_Review",
+    readers = ["everyone"],
+    writers = ["aclweb.org/ACL/2022/Conference"],
+    signatures = ["aclweb.org/ACL/2022/Conference"],
+    reply = {
+        "writers":{
+            "values-copied":[
+                "aclweb.org/ACL/2022/Conference",
+                "{signatures}"
+            ]
+        },
+        "signatures":{
+            "values-regex":"~.*"
+        },
+        "content": {
+            "recommendation": {
+                "order": 1,
+                "value-radio": [
+                    "1 - No serious ethical issues",
+                    "2 - Serious ethical issues that need to be addressed in the final version",
+                    "3 - Paper should be rejected due to ethical issues"
+                ],
+                "description": "Please select your ethical recommendation",
+                "required": True
+                },
+            "ethics_review": {
+                "order": 3,
+                "value-regex": "[\\S\\s]{1,200000}",
+                "description": "Provide justification for your suggested ethics issues. Add formatting using Markdown and formulas using LaTeX. For more information see https://openreview.net/faq.",
+                "required": True,
+                "markdown": True
+                }
+        }
+    }
+)
+client.post_invitation(ethics_review_super)
+submissions_forum_list = ['Ofoi1kAeB5o']
+# For Each submission, create reviewer group, add reviewer group and AC group to readers 
+for submission_forum in tqdm(submissions_forum_list): 
+    acl_blind_submission = client.get_note(submission_forum)
+    paper_track = sac_name_dictionary[acl_blind_submission.content["track"]]
+    track_sac_id = f'aclweb.org/ACL/2022/Conference/{paper_track}/Senior_Area_Chairs'
+    conflict_id = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Conflicts'
+    ethics_reviewer_id = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Ethics_Reviewers'
+    ethics_ac_id = f'aclweb.org/ACL/2022/Conference/Ethics_Chairs'
+    
+    ethics_review = client.post_invitation(openreview.Invitation(
+        id = f"aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/-/Ethics_Review",
+        super = "aclweb.org/ACL/2022/Conference/-/Ethics_Review",
+        invitees = [ethics_reviewer_id, program_chairs_id], 
+        signatures = ["aclweb.org/ACL/2022/Conference"],
+        multiReply= False,
+        process = './ethics_review_process.py',
+        reply = {
+            "forum": acl_blind_submission.forum,
+            "replyto": acl_blind_submission.forum,
+            "signatures": {
+                "values-regex": f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Ethics_Reviewer_.*|aclweb.org/ACL/2022/Conference/Program_Chairs', 
+                "description": "How your identity will be displayed."
+            },
+            "readers": {
+                "values-copied": [program_chairs_id, ethics_ac_id, ethics_reviewer_id]
+            },
+            "writers": {
+                "values": [program_chairs_id, ethics_reviewer_id]
+            },
+            "nonreaders": {
+                "values": [conflict_id]
+            }
+        }
+    )
+)
---
+++ venues/aclweb.org/ACL/reviewer_ac_groups.py
@@ -0,0 +1,131 @@
+import argparse
+from re import sub
+import openreview
+from tqdm import tqdm
+import csv
+import tracks
+
+"""
+OPTIONAL SCRIPT ARGUMENTS
+
+    baseurl -  the URL of the OpenReview server to connect to (live site: https://openreview.net)
+    username - the email address of the logging in user
+    password - the user's password
+
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseurl', help="base URL")
+parser.add_argument('--username')
+parser.add_argument('--password')
+args = parser.parse_args()
+client = openreview.Client(baseurl=args.baseurl, username=args.username, password=args.password)
+sac_name_dictionary = tracks.sac_name_dictionary
+
+
+# Create Ethics AC group 
+ethics = client.post_group(openreview.Group(
+    id = 'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+    signatures = [
+        'aclweb.org/ACL/2022/Conference'
+        ],
+    signatories=[
+        'aclweb.org/ACL/2022/Conference',
+        'aclweb.org/ACL/2022/Conference/Ethics_Chairs'
+        ],
+    readers = [
+        'aclweb.org/ACL/2022/Conference',
+        'aclweb.org/ACL/2022/Conference/Ethics_Chairs'
+    ],
+    writers = [
+            'aclweb.org/ACL/2022/Conference'
+            ],
+    members = [
+
+    ]
+))
+# Create Ethics Reviewers Group 
+ethics_reviewers = client.post_group(openreview.Group(
+        id = f'aclweb.org/ACL/2022/Conference/Ethics_Reviewers',
+        signatures = [
+            'aclweb.org/ACL/2022/Conference'
+            ],
+        signatories=[
+            'aclweb.org/ACL/2022/Conference'
+            ],
+        readers = [
+            'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+            'aclweb.org/ACL/2022/Conference/Ethics_Reviewers',
+            'aclweb.org/ACL/2022/Conference'
+            ],
+        writers = [
+            'aclweb.org/ACL/2022/Conference'
+             ]
+        ))
+# For submission in submissions, add paperx/Reviewers group and AC group as readers 
+# Currently assumes submissions_list is a list of Notes 
+submissions_forum_list = ['Ofoi1kAeB5o']
+# For Each submission, create reviewer group, add reviewer group and AC group to readers 
+for submission_forum in tqdm(submissions_forum_list): 
+    submission = client.get_note(submission_forum)
+    paper_track = sac_name_dictionary[submission.content["track"]]
+    track_sac_id = f'aclweb.org/ACL/2022/Conference/{paper_track}/Senior_Area_Chairs'
+    conflict_id = f'aclweb.org/ACL/2022/Conference/Paper{submission.number}/Conflicts'
+    # Create Ethics Reviewer group 
+    ethics_reviewers_paper = client.post_group(openreview.Group(
+        id = f'aclweb.org/ACL/2022/Conference/Paper{submission.number}/Ethics_Reviewers',
+        signatures = [
+            'aclweb.org/ACL/2022/Conference'
+            ],
+        signatories=[
+            'aclweb.org/ACL/2022/Conference'
+            ],
+        readers = [
+            'aclweb.org/ACL/2022/Conference',
+            'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+            f'aclweb.org/ACL/2022/Conference/Paper{submission.number}/Ethics_Reviewers'
+            ],
+        writers = [
+            'aclweb.org/ACL/2022/Conference'
+             ],
+        nonreaders= submission.nonreaders
+        ))
+    
+    submission.readers = [
+        "aclweb.org/ACL/2022/Conference",
+        "aclweb.org/ACL/2022/Conference/Program_Chairs",
+        track_sac_id,
+        'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+        ethics_reviewers_paper.id
+    ]
+    submission.nonreaders = [
+        conflict_id
+    ]
+    submission.content = {
+        'authors': ['Anonymous'],
+        'authorids': [f'aclweb.org/ACL/2022/Conference/Paper{submission.number}/Authors']
+    }
+    client.post_note(submission)
+    metareviews = openreview.tools.iterget_notes(client, invitation = "aclweb.org/ACL/2022/Conference/-/Meta_Review", forum = submission.forum)
+    for review in metareviews: 
+        review.readers = [
+            "aclweb.org/ACL/2022/Conference/Program_Chairs",
+            track_sac_id,
+            'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+            ethics_reviewers_paper.id
+        ]
+        review.nonreaders = [
+            conflict_id
+        ]
+        client.post_note(review)
+    reviews = openreview.tools.iterget_notes(client, invitation = "aclweb.org/ACL/2022/Conference/-/Official_Review", forum = submission.forum)
+    for review in reviews: 
+        review.readers = [
+            "aclweb.org/ACL/2022/Conference/Program_Chairs",
+            track_sac_id,
+            'aclweb.org/ACL/2022/Conference/Ethics_Chairs',
+            ethics_reviewers_paper.id
+        ]
+        review.nonreaders = [
+            conflict_id
+        ]
+        client.post_note(review)
