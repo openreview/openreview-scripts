12a46bdd94fdd15b9cd1bf0029569beae6861f34
Nadia L\'Bahy <nlbahy@umass.edu>
2022-02-23 14:10:23 -0500

    melisa's feedback

--- venues/aclweb.org/ACL/release_submissions_to_authors.py
+++ venues/aclweb.org/ACL/release_submissions_to_authors.py
@@ -33,28 +33,25 @@ acl_blind_submissions = list(openreview.tools.iterget_notes(client, invitation =
 
 # For each submission, add the authors of the original ARR submission to the readers of the blind submission 
 for acl_blind_submission in tqdm(acl_blind_submissions): 
-    
+    # Need to add authors to ethics review invitation as readers 
+    invitation = invitations_by_forum[acl_blind_submission.forum]
+    if author_group not in invitation.reply['readers']:
+        invitation.reply['readers'].append(author_group)
+        client.post_invitation(invitation)
+
     author_group = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Authors'
-    acl_blind_submission.readers.append(author_group)
-    acl_blind_submission.content = {
-                "authorids" : [f"aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Authors"],
-                "authors":["Anonymous"]
-            }
-    client.post_note(acl_blind_submission)
-    try: 
-        ethics_review = client.get_notes(forum = acl_blind_submission.forum, invitation = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/-/Ethics_Review')
+    if author_group not in acl_blind_submission.readers:
+        acl_blind_submission.readers.append(author_group)
+        acl_blind_submission.content = {
+                    "authorids" : [f"aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/Authors"],
+                    "authors":["Anonymous"]
+                }
+        client.post_note(acl_blind_submission)
+    
+    ethics_review = client.get_notes(forum = acl_blind_submission.forum, invitation = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/-/Ethics_Review')
+    if author_group not in ethics_review.readers:
         ethics_review.readers.append(author_group)
         client.post_note(ethics_review)
-    except: 
-        pass
-    try: 
-        final_decision = client.get_notes(forum = acl_blind_submission.forum, invitation = f'aclweb.org/ACL/2022/Conference/Paper{acl_blind_submission.number}/-/Decision')
-        final_decision.readers.append(author_group)
-        client.post_note(final_decision)
-    except: 
-        #print(f'no final decision for paper {acl_blind_submission.forum}')
-        pass
-    invitation = invitations_by_forum[acl_blind_submission.forum]
-    invitation.reply['readers'].append(author_group)
-    client.post_invitation(invitation)
+  
+    
         
