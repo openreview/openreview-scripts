70049be26689638b117269a9139c721a61c7828e
Nadia L\'Bahy <nlbahy@umass.edu>
2022-03-09 14:48:52 -0500

    takes into account if PC is author of submission

--- venues/aclweb.org/Workshops/add-readers-to-original.py
+++ venues/aclweb.org/Workshops/add-readers-to-original.py
@@ -15,6 +15,7 @@ OPTIONAL SCRIPT ARGUMENTS
     confid - the user's conference 
 
 """
+# Need to first replace members by ID for PC group 
 # For workshops using their original venue -- PCs only posting decisions and Camera Ready Revision Invitations to Papers 
 parser = argparse.ArgumentParser()
 parser.add_argument('--baseurl', help="base URL")
@@ -29,8 +30,28 @@ confid = args.confid
 #if openreview.tools.get_group(client, f'{confid}/Area_Chairs'):
  #   committee.append(f'{confid}/Area_Chairs')
 # Retrieve all commitment submissions 
+PC_group = [PC.id for PC in openreview.tools.get_profiles(client, client.get_group(f'{confid}/Program_Chairs').members)]
 commitment_notes = list(openreview.tools.iterget_notes(client, invitation = f'{confid}/-/Commitment_Submission'))
-for commitment_note in commitment_notes: 
+original_arr_subs = {}
+author_groups = {}
+print("loading commitment data")
+for commitment_note in tqdm(commitment_notes): 
+    arr_submission_forum = ((commitment_note.content['paper_link'].split('=')[1]).split('&')[0]).strip()
+    #try:
+    original_arr_sub = client.get_note(arr_submission_forum)
+    arr_conf_id = original_arr_sub.invitation.rsplit('/', 2)[0]
+    if(original_arr_sub.original):
+        original_arr_sub_id = original_arr_sub.original
+    else:
+        original_arr_sub_id = original_arr_sub.id
+    original_arr_sub = client.get_note(original_arr_sub_id)
+    original_arr_subs[commitment_note.forum] = original_arr_sub
+    author_group = client.get_group(f'{arr_conf_id}/Paper{original_arr_sub.number}/Authors')
+    author_groups[original_arr_sub.id] = [author.id for author in openreview.tools.get_profiles(client, author_group.members)]
+    #except:
+    #    print(f"Note {arr_submission_forum} does not exist")
+print("adding PCs as readers")    
+for commitment_note in tqdm(commitment_notes): 
     paper_group = openreview.Group(
         id = f'{confid}/Commitment{commitment_note.number}',
         signatures = [
@@ -71,19 +92,24 @@ for commitment_note in commitment_notes:
     authors_posted = client.post_group(authors)
     assert authors_posted, print('Failed to post author groups: ', commitment_note.id)
     
-    arr_submission_forum = (commitment_note.content['paper_link'].split('=')[1]).split('&')[0]
-    try:
-        original_arr_sub = client.get_note(arr_submission_forum)
-        if(original_arr_sub.original):
-            original_arr_sub_id = original_arr_sub.original
-        else:
-            original_arr_sub_id = original_arr_sub.id
-    except:
-        print(f"Note {arr_submission_forum} does not exist")
-
+    original_arr_sub = original_arr_subs.get(commitment_note.forum)
     if original_arr_sub: 
         arr_conf_id = original_arr_sub.invitation.rsplit('/', 2)[0]
-        original_arr_sub.invitation.split('/')
         arr_reviewers = client.get_group(f'{arr_conf_id}/Paper{original_arr_sub.number}/Reviewers')
-        client.add_members_to_group(arr_reviewers, f'{confid}/Program_Chairs')
+        #arr_reviewers_submitted = client.get_group(f'{arr_conf_id}/Paper{original_arr_sub.number}/Reviewers_Submitted')
+        print(arr_reviewers.id)
+        group_with_PCs = client.add_members_to_group(arr_reviewers, f'{confid}/Program_Chairs')
+        # Check if PC is author of submission, and if so, add them as nonreaders of group 
+        for PC in PC_group: 
+            if PC in author_groups[original_arr_sub.id]: # Author groups dictionary of original arr sub: author group.members 
+                group_with_PCs.nonreaders.append(PC)
+        client.post_group(group_with_PCs)
+        if client.get_group(f'{arr_conf_id}/Paper{original_arr_sub.number}/Reviewers/Submitted'):
+            group_with_PCs = client.add_members_to_group(f'{arr_conf_id}/Paper{original_arr_sub.number}/Reviewers/Submitted', f'{confid}/Program_Chairs')
+            # Check if PC is author of submission, and if so, add them as nonreaders of group 
+            for PC in PC_group: 
+                if PC in author_groups[original_arr_sub.id]: # Author groups dictionary of original arr sub: author group.members 
+                    group_with_PCs.nonreaders.append(PC)
+            client.post_group(group_with_PCs)
+        
 
