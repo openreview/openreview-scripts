834452a117a5ff0cb4055fadef4c26e1328e78a8
Nadia L\'Bahy <nlbahy@umass.edu>
2022-03-11 12:49:42 -0500

    adds everyone as readers

--- venues/aclweb.org/NAACL/2022/post_preprints.py
+++ venues/aclweb.org/NAACL/2022/post_preprints.py
@@ -40,7 +40,8 @@ client.post_invitation(blind_submission_invitation)
 blind_submissions = list(openreview.tools.iterget_notes(client, invitation = 'aclweb.org/NAACL/2022/Conference/-/Blind_Submission'))
 
 for blind_submission in tqdm(blind_submissions):
-    if blind_submission.content['naacl_preprint'] == 'yes':
+    original = client.get_note(blind_submission.original)
+    if original.content['naacl_preprint'] == 'yes':
         content = {}
         keep_keys = ['title', 'pdf', 'abstract','paper_link','country_of_affiliation_of_corresponding_author', 'track','paper_type']
         for key in blind_submission.content: 
@@ -50,5 +51,6 @@ for blind_submission in tqdm(blind_submissions):
         content['authorids'] = [f"aclweb.org/NAACL/2022/Conference/Commitment{blind_submission.number}/Authors"]
         content['authors'] = ["Anonymous"]
         blind_submission.content = content
+        blind_submission.readers = ['everyone']
         client.post_note(blind_submission)
         
