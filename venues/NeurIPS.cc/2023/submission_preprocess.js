b066052a921abc0035c54f4e9d551d36db8a4fb5
Celeste Martinez <celeste@openreview.net>
2023-05-01 14:09:12 -0400

    add preprocess that checks registration was completed by revs and acs

---
+++ venues/NeurIPS.cc/2023/submission_preprocess.js
@@ -0,0 +1,94 @@
+async function process(client, edit, invitation) {
+    client.throwErrors = true
+    
+    const { note } = edit
+
+    if (note.ddate) { 
+      return
+    }
+
+    const profiles = await client.tools.getProfiles(note.content.authorids.value)
+    const profileIds = profiles.map(profile => profile.id)
+
+    const primaryAuthorField = note.content.corresponding_author.value
+    const primaryAuthorProfile = await client.tools.getProfiles([primaryAuthorField])
+    const primaryAuthor = primaryAuthorProfile ? primaryAuthorProfile[0].id : primaryAuthorField
+    if (!profileIds.includes(primaryAuthor)) {
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Select a paper co-author as corresponding author, ' + primaryAuthorField + ' does not appear in the author list' }))
+    }
+
+    if (note.content.financial_support) {
+      const financialSupportField = note.content.financial_support.value
+      const financialSupportProfile = await client.tools.getProfiles([financialSupportField])
+      const financialSupport = financialSupportProfile ? financialSupportProfile[0].id : financialSupportField
+      if (!profileIds.includes(financialSupport)) {
+        return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Select a paper co-author for financial support, ' + financialSupportField + ' does not appear in the author list' }))
+      }
+    }
+
+    const reviewerNominationField = note.content.reviewer_nomination.value
+    const reviewerNominationProfile = await client.tools.getProfiles([reviewerNominationField])
+    const reviewerNomination = reviewerNominationProfile ? reviewerNominationProfile[0].id : reviewerNominationField
+    if (!profileIds.includes(reviewerNomination)) {
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Select a paper co-author to nominate as reviewer, ' + reviewerNominationField + ' does not appear in the author list' }))
+    }
+
+    const { groups: revGroups } = await client.getGroups({ id: invitation.domain + '/Reviewers' })
+    const reviewers = revGroups[0].members;
+    
+    const reviewerMembers = {}
+    for (const reviewer of reviewers) {
+      reviewerMembers[reviewer] = true
+    }
+
+    const { groups: acGroups } = await client.getGroups({ id: invitation.domain + '/Area_Chairs' })
+    const acs = acGroups[0].members;
+    
+    const acMembers = {}
+    for (const ac of acs) {
+      acMembers[ac] = true
+    }
+
+    // var allIds; 
+    // profiles.forEach(async function(profile) {
+    //   const emails = profile.content.emails
+    //   const usernames = profile.content.names.map(name => name.username)
+    //   allIds = emails.concat(usernames)
+    //   for (const username of allIds) {
+    //     if (reviewerMembers[username]) {
+    //       const { count: noteCount } = await client.getNotes({ invitation: 'NeurIPS.cc/2023/Conference/Reviewers/-/Registration', signatures: [allIds] })
+    //       if (noteCount === 0) {
+    //         return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Reviewer ' + username + ' has not completed the Reviewer Registration.' }))
+    //       }
+    //     }
+    //     if (acMembers[username]) {
+    //       const { count: noteCount } = await client.getNotes({ invitation: 'NeurIPS.cc/2023/Conference/Area_Chairs/-/Registration', signatures: [allIds] })
+    //       if (noteCount === 0) {
+    //         return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Area Chair ' + username + ' has not completed the Area Chair Registration.' }))
+    //       }
+    //     }
+    //   }
+    // })
+
+    var allIds; 
+    for(var i=0;i<profiles.length;i++) {
+      const profile = profiles[i]
+      const emails = profile.content.emails
+      const usernames = profile.content.names.map(name => name.username)
+      allIds = emails.concat(usernames)
+      for (const username of allIds) {
+        if (reviewerMembers[username]) {
+          const { count: noteCount } = await client.getNotes({ invitation: 'NeurIPS.cc/2023/Conference/Reviewers/-/Registration', signatures: [allIds] })
+          if (noteCount === 0) {
+            return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Reviewer ' + username + ' has not completed the Reviewer Registration.' }))
+          }
+        }
+        if (acMembers[username]) {
+          const { count: noteCount } = await client.getNotes({ invitation: 'NeurIPS.cc/2023/Conference/Area_Chairs/-/Registration', signatures: [allIds] })
+          if (noteCount === 0) {
+            return Promise.reject(new OpenReviewError({ name: 'Error', message: 'Area Chair ' + username + ' has not completed the Area Chair Registration.' }))
+          }
+        }
+      }
+    }
+}
