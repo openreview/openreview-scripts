e5bac5fd62b898b7248e6d0832ac7ec6ad23375d
haroldrubio <harrywill.rubio@gmail.com>
2023-11-03 11:06:20 -0400

    Merge blind and original submission posting

--- venues/aclweb.org/Workshops/migrate_submissions_workshops_one.py
+++ venues/aclweb.org/Workshops/migrate_submissions_workshops_one.py
@@ -26,7 +26,7 @@ confid = args.confid
 # Post acl submission (calls post_blind_submission)
 def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_output_dict):
     # Depends on the workshop
-    eligible_arr_invitations = [
+    eligible_arr_invitations = [ ## Should move this to a regex match?
         'aclweb.org/ACL/ARR/2021/May/',
         'aclweb.org/ACL/ARR/2021/Jun/',
         'aclweb.org/ACL/ARR/2021/July/',
@@ -51,6 +51,7 @@ def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_ou
         'aclweb.org/ACL/ARR/2023/October/',
         ]
     original_arr_sub_id = None
+    note_exists = False
     #submission_output_dict['acl_commitment_forum'] = acl_commitment_note.forum
     try:
         original_arr_sub = client.get_note(arr_submission_forum)
@@ -58,25 +59,32 @@ def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_ou
             original_arr_sub_id = original_arr_sub.original
         else:
             original_arr_sub_id = original_arr_sub.id
+        note_exists = True
     except:
         print(f"Note {arr_submission_forum} does not exist")
-    if original_arr_sub_id and ((client.get_note(original_arr_sub_id)).invitation.startswith(tuple(eligible_arr_invitations))):
+    if note_exists and original_arr_sub_id and ((client.get_note(original_arr_sub_id)).invitation.startswith(tuple(eligible_arr_invitations))):
         #print(original_arr_sub_id)
         original_arr_sub = client.get_note(original_arr_sub_id)
         submission_output_dict[acl_commitment_note.forum]['arr_submission_forum'] = original_arr_sub.forum
-        if (openreview.tools.get_profiles(client,[acl_commitment_note.signatures[0]])[0].id) in [profile.id for profile in openreview.tools.get_profiles(client, original_arr_sub.content['authorids'])]:
+        print(acl_commitment_note.id)
+        first_author = openreview.tools.get_profiles(client,[acl_commitment_note.content['authorids']['value'][0]])[0].id
+        authorids_ids = [profile.id for profile in openreview.tools.get_profiles(client, original_arr_sub.content['authorids'])]
+        if first_author in authorids_ids:
             # Create new note to submit to ACL
             content = {}
-            conf_submission_invitation = client.get_invitation(f"{confid}/-/Migrated_Submission")
+            conf_submission_invitation = client_v2.get_invitation(f"{confid}/-/Migrated_Submission")
             for key in acl_commitment_note.content.keys():
-                content[key] = acl_commitment_note.content[key]
+                content[key]= acl_commitment_note.content[key]
             for key in original_arr_sub.content.keys():
-                if key in conf_submission_invitation.reply['content']:
-                    content[key] = original_arr_sub.content[key]
-            content['paper_link'] =f'https://openreview.net/forum?id={arr_submission_forum}'
-            content['commitment_note'] = f"https://openreview.net/forum?id={acl_commitment_note.forum}"
-            acl_sub = openreview.Note(
-                invitation=f"{confid}/-/Migrated_Submission",
+                if key in conf_submission_invitation.edit['note']['content']:
+                    if key not in content.keys():
+                        content[key] = {}
+                    content[key]['value'] = original_arr_sub.content[key]
+            content['paper_link'] = {}
+            content['paper_link']['value'] =f'https://openreview.net/forum?id={arr_submission_forum}'
+            content['commitment_note'] = {}
+            content['commitment_note']['value'] = f"https://openreview.net/forum?id={acl_commitment_note.forum}"
+            acl_sub = openreview.api.Note(
                 readers = [
                     confid
                     ],
@@ -88,16 +96,180 @@ def post_acl_submission(arr_submission_forum, acl_commitment_note, submission_ou
                     ],
                 content = content
             )
-            acl_submitted = client.post_note(acl_sub)
-            if(acl_submitted):
+            arr_submission = original_arr_sub
+
+            # Create PaperX group -- Submission groups automatically create
+
+            # Create paperX/Conflicts
+            conf_id = arr_submission.invitation.rsplit('/', 2)[0]
+            arr_number = arr_submission.number
+            conflict_members = [
+                    '{conf_id}/Paper{number}/Reviewers'.format(conf_id = conf_id, number = arr_number),
+                    '{conf_id}/Paper{number}/Area_Chairs'.format(conf_id = conf_id, number = arr_number),
+                    '{conf_id}/Paper{number}/Authors'.format(conf_id = conf_id, number = arr_number)
+                    ]
+            # Find all previous & future submissions and for each one add the reviewers and ACs to the conflict group
+            def get_reviewer_AC_conflicts(current_submission):
+                previous_url = current_submission.content.get('previous_URL')
+                if previous_url and ("openreview.net" in previous_url):
+                    #print(current_submission.forum)
+                    previous_forum = (current_submission.content.get('previous_URL').split('=')[1]).split('&')[0]
+                    previous_submission = client.get_note(previous_forum)
+                    conf_id = previous_submission.invitation.rsplit('/', 2)[0]
+                    arr_number = previous_submission.number
+                    reviewers = '{conf_id}/Paper{number}/Reviewers'.format(conf_id = conf_id, number = arr_number)
+                    area_chairs = '{conf_id}/Paper{number}/Area_Chairs'.format(conf_id = conf_id, number = arr_number)
+                    conflict_members.append(reviewers)
+                    conflict_members.append(area_chairs)
+                    if (previous_submission.forum != current_submission.forum) and (previous_submission.forum != current_submission.id):
+                        get_reviewer_AC_conflicts(previous_submission)
+
+            get_reviewer_AC_conflicts(arr_submission)
+            later_versions = submission_output_dict[acl_commitment_note.forum]['later_versions?']
+            if later_versions:
+                for version in later_versions:
+                    later_submission = client.get_note(version)
+                    arr_conf_id = later_submission.invitation.rsplit('/', 2)[0]
+                    arr_number = later_submission.number
+                    reviewers = '{arr_conf_id}/Paper{number}/Reviewers'.format(arr_conf_id = arr_conf_id, number = arr_number)
+                    area_chairs = '{arr_conf_id}/Paper{number}/Area_Chairs'.format(arr_conf_id = arr_conf_id, number = arr_number)
+                    conflict_members.append(reviewers)
+                    conflict_members.append(area_chairs)
+
+            # CHANGE: Move to group edit
+            conflicts = openreview.api.Group(
+                id=f'{confid}/Submission{acl_commitment_note.number}/Conflicts',
+                signatures = [
+                    confid
+                ],
+                signatories = [
+                    confid
+                ],
+                readers = [
+                    confid
+                ],
+                writers = [
+                    confid
+                ],
+                members=list(set(conflict_members))
+            )
+            conflicts_posted = client_v2.post_group_edit(
+                invitation = f"{confid}/-/Edit",
+                readers = [confid],
+                writers = [confid],
+                signatures = [confid],
+                group = conflicts
+            )
+            
+            assert conflicts_posted, print('Failed to post conflict group: ', f'{confid}/Submission{acl_commitment_note.number}/Conflicts')
+
+            # post blinded note
+            # CHANGE: move to note edit
+            content['authors']['readers'] = [
+                confid,
+                f"{confid}/Submission{acl_commitment_note.number}/Authors"
+            ]
+            content['authorids']['readers'] = [
+                confid,
+                f"{confid}/Submission{acl_commitment_note.number}/Authors"
+            ]
+            blinded_note = openreview.api.Note(
+                readers = [
+                    f"{confid}/Program_Chairs",
+                    confid,
+                    f"{confid}/Submission{acl_commitment_note.number}/Reviewers", f"{confid}/Submission{acl_commitment_note.number}/Area_Chairs", f"{confid}/Submission{acl_commitment_note.number}/Senior_Area_Chairs"
+                    #"aclweb.org/ACL/2022/Conference/{sac_track}/Senior_Area_Chairs".format(sac_track = sac_name_dictionary[acl_submission.content['track']])
+                ],
+                nonreaders = [
+                    f"{confid}/Submission{acl_commitment_note.number}/Conflicts"
+                ],
+                writers = [
+                    confid
+                ],
+                signatures = [
+                    confid
+                ],
+                content = content
+            )
+            blinded_note_posted = client_v2.post_note_edit( # CHANGE: Move to note edit
+                invitation = f"{confid}/-/Migrated_Submission",
+                signatures=[confid],
+                readers = [
+                    f"{confid}/Program_Chairs",
+                    confid,
+                    f"{confid}/Submission{acl_commitment_note.number}/Reviewers", f"{confid}/Submission{acl_commitment_note.number}/Area_Chairs", f"{confid}/Submission{acl_commitment_note.number}/Senior_Area_Chairs"
+                    #"aclweb.org/ACL/2022/Conference/{sac_track}/Senior_Area_Chairs".format(sac_track = sac_name_dictionary[acl_submission.content['track']])
+                ],
+                nonreaders = [
+                    f"{confid}/Submission{acl_commitment_note.number}/Conflicts"
+                ],
+                writers = [
+                    confid
+                ],
+                note=blinded_note
+            )
+            acl_submitted = blinded_note_posted
+
+            if blinded_note_posted:
+                # Repost the commitment note with a link to the blind submission
+                #print(acl_commitment_note)
+
+                # Post review and meta-review invitation edits
+                paper_official_review_edit = client_v2.post_invitation_edit(
+                    invitations=f"{confid}/-/ARR_Official_Review",
+                    readers=[confid],
+                    writers=[confid],
+                    signatures=[confid],
+                    content={
+                        'noteId': {
+                            'value': blinded_note_posted['note']['id']
+                        },
+                        'noteNumber': {
+                            'value': blinded_note_posted['note']['number']
+                        }
+                    },
+                    invitation=openreview.api.Invitation()
+                )
+                paper_meta_review_edit = client_v2.post_invitation_edit(
+                    invitations=f"{confid}/-/ARR_Meta_Review",
+                    readers=[confid],
+                    writers=[confid],
+                    signatures=[confid],
+                    content={
+                        'noteId': {
+                            'value': blinded_note_posted['note']['id']
+                        },
+                        'noteNumber': {
+                            'value': blinded_note_posted['note']['number']
+                        }
+                    },
+                    invitation=openreview.api.Invitation()
+                )
+
+                client_v2.post_note_edit( # CHANGE: Move to note edit
+                    invitation = f"{confid}/-/Edit",
+                    signatures=[confid],
+                    note=openreview.api.Note(
+                        id=acl_commitment_note.id,
+                        content={
+                            'migrated_paper_link': {
+                                'value': f"https://openreview.net/forum?id={blinded_note_posted['note']['forum']}"
+                            }
+                        }
+                    )
+                )
+
                 submission_output_dict[acl_commitment_note.forum]['was_migrated'] = True
-                acl_submission_dict[acl_submitted.content['paper_link'].split('=')[1]]=acl_submitted
-                post_blind_submission(acl_submitted.id, acl_submitted, original_arr_sub, submission_output_dict, acl_commitment_note)
+                submission_output_dict[acl_commitment_note.forum]['acl_blind_submission_forum'] = blinded_note_posted['note']['forum']
+                acl_submission_dict[acl_submitted['note']['content']['paper_link']['value'].split('=')[1]]=acl_submitted
+                blind_submissions[blinded_note_posted['note']['id']] = blinded_note_posted['note']
+                post_reviews(blinded_note_posted['note']['id'], blinded_note_posted['note'], arr_submission, submission_output_dict, acl_commitment_note)
             else:
                 print(f'Nothing posted for ARR Submission {arr_submission_forum}')
-                return(submission_output_dict)
+                return submission_output_dict
         else:
             print(f"signature of acl commitment note {acl_commitment_note.id} not in arr submission {arr_submission_forum} authors")
+            print(f"first_author={first_author} not in authorids={authorids_ids}")
             return submission_output_dict
     else:
         print(f"acl commitment note {acl_commitment_note.id} links to arr submission {arr_submission_forum} with invalid invitation")
